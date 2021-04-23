abe470a1304e3409dc93cc90ae5486d88ea5ee60
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-04-23 02:29:15 -0300

    refactor(api-client): fixed customerCart API typings

--- packages/api-client/src/api/customerCart/index.ts
+++ packages/api-client/src/api/customerCart/index.ts
@@ -1,10 +1,10 @@
 import { ApolloQueryResult } from 'apollo-client';
-import { CartQuery } from '../../types/GraphQL';
+import { CustomerCartQuery } from '../../types/GraphQL';
 import query from './query.graphql';
 import { Context } from '../../types/context';
 
-export default async ({ client }: Context): Promise<ApolloQueryResult<CartQuery>> => client
-  .query<CartQuery>({
+export default async ({ client }: Context): Promise<ApolloQueryResult<CustomerCartQuery>> => client
+  .query<CustomerCartQuery>({
   query,
   fetchPolicy: 'no-cache',
 });
