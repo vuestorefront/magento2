ab819a8652f75d417f747ffd6aeaa2b4d47955d5
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-04-23 02:29:45 -0300

    refactor(api-client): fixed wishlist API

--- packages/api-client/src/api/wishlist/index.ts
+++ packages/api-client/src/api/wishlist/index.ts
@@ -1,10 +1,14 @@
 import { ApolloQueryResult } from 'apollo-client';
-import { WishlistQuery } from '../../types/GraphQL';
+import { WishlistQuery, WishlistQueryVariables } from '../../types/GraphQL';
 import query from './query.graphql';
 import { Context } from '../../types/context';
 
-export default async ({ client }: Context): Promise<ApolloQueryResult<WishlistQuery>> => client
-  .query<WishlistQuery>({
+export default async (
+  { client }: Context,
+  queryParams: WishlistQueryVariables,
+): Promise<ApolloQueryResult<WishlistQuery>> => client
+  .query<WishlistQuery, WishlistQueryVariables>({
   query,
   fetchPolicy: 'no-cache',
+  variables: queryParams,
 });
