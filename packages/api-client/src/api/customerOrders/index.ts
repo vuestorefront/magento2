23a17694afef822119f76c6b21d9f135f92263f2
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-04-23 02:28:58 -0300

    refactor(api-client): fixed customerOrders API

--- packages/api-client/src/api/customerOrders/index.ts
+++ packages/api-client/src/api/customerOrders/index.ts
@@ -1,10 +1,14 @@
 import { ApolloQueryResult } from 'apollo-client';
-import { CustomerOrdersQuery } from '../../types/GraphQL';
+import { CustomerOrdersQuery, CustomerOrdersQueryVariables } from '../../types/GraphQL';
 import query from './query.graphql';
 import { Context } from '../../types/context';
 
-export default async ({ client }: Context): Promise<ApolloQueryResult<CustomerOrdersQuery>> => client
-  .query<CustomerOrdersQuery>({
+export default async (
+  { client }: Context,
+  orderParams: CustomerOrdersQueryVariables,
+): Promise<ApolloQueryResult<CustomerOrdersQuery>> => client
+  .query<CustomerOrdersQuery, CustomerOrdersQueryVariables>({
   query,
+  variables: orderParams,
   fetchPolicy: 'no-cache',
 });
