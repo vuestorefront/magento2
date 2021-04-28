4fdbe6e806e68df87346f7936e0fc00832cb1275
Alef Barbeli <alefbarbeli@gmail.com>
2021-04-28 01:42:35 -0300

    fix(composables): userGetters wrong property access

--- packages/composables/src/composables/getters/userGetters.ts
+++ packages/composables/src/composables/getters/userGetters.ts
@@ -3,14 +3,13 @@ import { UserGetters } from '@vue-storefront/core';
 import { User } from '../../types';
 
 export const getUserFirstName = (user: User): string => {
-  console.log(user);
   return user ? user.firstname : '';
 };
 
 export const getUserLastName = (user: User): string => (user ? user.lastname : '');
 export const getUserEmail = (user: User): string => (user ? user.email : '');
 
-export const getUserFullName = (user: User): string => (user ? `${user.firstName} ${user.lastName}` : '');
+export const getUserFullName = (user: User): string => (user ? `${user.firstname} ${user.lastname}` : '');
 
 const userGetters: UserGetters<User> = {
   getFirstName: getUserFirstName,
