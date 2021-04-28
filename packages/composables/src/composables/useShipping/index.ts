4bc919444345a2868f6a0126886e85f7457c7dc9
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-04-28 17:45:58 -0300

    refactor(composeables): fixed useShipping typings

--- packages/composables/src/composables/useShipping/index.ts
+++ packages/composables/src/composables/useShipping/index.ts
@@ -1,11 +1,12 @@
 import { Context, Logger } from '@vue-storefront/core';
 import {
-  AddressOnCart, CartAddressInput, SetShippingAddressesOnCartInput, ShippingAddressInput,
+  CartAddressInput,
+  SetShippingAddressesOnCartInput,
 } from '@vue-storefront/magento-api';
 import { useShippingFactory, UseShippingParams } from '../../factories/useShippingFactory';
 import useCart from '../useCart';
 
-const factoryParams: UseShippingParams<AddressOnCart, any> = {
+const factoryParams: UseShippingParams<any, any> = {
   provide() {
     return {
       cart: useCart(),
@@ -14,16 +15,23 @@ const factoryParams: UseShippingParams<AddressOnCart, any> = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   load: async (context: Context, { customQuery }) => {
     Logger.debug('[Magento] loadShipping');
+
     if (!context.cart.cart?.value?.shipping_addresses) {
       await context.cart.load({ customQuery });
     }
-    return context.cart.cart.value.shipping_addresses[0];
+
+    return context
+      .cart
+      .cart
+      .value
+      .shipping_addresses[0];
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   save: async (context: Context, saveParams) => {
     Logger.debug('[Magento] setShippingAddress');
     Logger.debug(context);
+
     const { id } = context.cart.cart.value;
     const address = saveParams.shippingDetails as CartAddressInput;
 
@@ -36,10 +44,17 @@ const factoryParams: UseShippingParams<AddressOnCart, any> = {
       ],
     };
 
-    const setShippingAddressesOnCartResponse = await context.$magento.api.setShippingAddressesOnCart(shippingAddressInput);
+    const setShippingAddressesOnCartResponse = await context
+      .$magento
+      .api
+      .setShippingAddressesOnCart(shippingAddressInput);
 
-    return setShippingAddressesOnCartResponse.data.setShippingAddressesOnCart.cart.shipping_addresses[0];
+    return setShippingAddressesOnCartResponse
+      .data
+      .setShippingAddressesOnCart
+      .cart
+      .shipping_addresses[0];
   },
 };
 
-export default useShippingFactory<AddressOnCart, any>(factoryParams);
+export default useShippingFactory<any, any>(factoryParams);
