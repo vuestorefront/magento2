558f48f529f123e23428d26d3dba18e2954e5a8e
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-05-02 03:01:40 -0300

    refactor(composeables): initial fixes for useReview

--- packages/composables/src/composables/useReview/index.ts
+++ packages/composables/src/composables/useReview/index.ts
@@ -1,16 +1,22 @@
 /* istanbul ignore file */
 import {
-  Context, Logger,
-  UseReview,
+  Context,
+  Logger,
   useReviewFactory,
   UseReviewFactoryParams,
 } from '@vue-storefront/core';
+import { GetProductSearchParams } from '@vue-storefront/magento-api';
 
 const factoryParams: UseReviewFactoryParams<any, any, any> = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  searchReviews: async (context: Context, params) => {
-    Logger.debug('Mocked: searchReviews');
-    return {};
+  searchReviews: async (context: Context, params: GetProductSearchParams) => {
+    Logger.debug('[Magento] searchReviews');
+
+    const { data } = await context.$magento.api.productReview(params as GetProductSearchParams);
+
+    Logger.debug(JSON.stringify(data, null, 2));
+
+    return data.products.items;
   },
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   addReview: async (context: Context, params) => {
@@ -19,6 +25,4 @@ const factoryParams: UseReviewFactoryParams<any, any, any> = {
   },
 };
 
-const useReview: (cacheId: string) => UseReview<any, any, any> = useReviewFactory<any, any, any>(factoryParams);
-
-export default useReview;
+export default useReviewFactory<any, any, any>(factoryParams);
