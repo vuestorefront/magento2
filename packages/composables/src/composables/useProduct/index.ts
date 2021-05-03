e8594dfd30d1e0f443a252a853519c9a4894ee99
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-05-03 13:44:57 -0300

    refactor(composeables): fixed useProduct implementation

--- packages/composables/src/composables/useProduct/index.ts
+++ packages/composables/src/composables/useProduct/index.ts
@@ -19,6 +19,25 @@ const factoryParams: UseProductFactoryParams<ProductsListQuery['products'], Prod
     switch (queryType) {
       case ProductsQueryType.Detail:
         const productDetailsResults = await context.$magento.api.productDetail(searchParams as GetProductSearchParams);
+        const upsellProduct = await context.$magento.api.upsellProduct(searchParams as GetProductSearchParams);
+        const relatedProduct = await context.$magento.api.relatedProduct(searchParams as GetProductSearchParams);
+
+        productDetailsResults.data.products.items[0] = {
+          ...productDetailsResults.data.products.items[0],
+          ...upsellProduct.data.products.items[0],
+          ...relatedProduct.data.products.items[0],
+        };
+
+        // eslint-disable-next-line no-underscore-dangle
+        if (productDetailsResults.data.products.items[0].__typename === 'ConfigurableProduct') {
+          const configurableProduct = await context.$magento.api.configurableProductDetail(searchParams as GetProductSearchParams);
+
+          productDetailsResults.data.products.items[0] = {
+            ...productDetailsResults.data.products.items[0],
+            ...configurableProduct.data.products.items[0],
+          };
+        }
+
         return productDetailsResults.data.products;
 
       case ProductsQueryType.List:
