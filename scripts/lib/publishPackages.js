f00e91485c39087e5b55adc480d1665497dc1e90
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-05-10 13:44:20 -0300

    refactor: fixed composeables naming and api-clients api typings (#47)
    * chore(composeables): renamed composeables
    
    * chore(theme): renamed composeables to new structure
    
    * chore: added files for auto publishing on NPM
    
    * refactor(api-client): fixed customer creation typings

---
+++ scripts/lib/publishPackages.js
@@ -0,0 +1,41 @@
+const npmPublish = require('@jsdevtools/npm-publish');
+const path = require('path');
+const labelsToRemove = ['release'];
+const npmLabelBase = 'NPM:';
+
+const publishPackages = async (labels, token) => {
+  const npmTagBase = [...labels]
+  .filter((l) => !labelsToRemove.includes(l))
+  .find((l) => l.startsWith(npmLabelBase));
+
+  if(npmTagBase){
+    const npmTag = npmTagBase.replace(new RegExp(`${npmLabelBase}`), '');
+
+    const baseApiClientPath = path.join(process.cwd(), 'packages', 'api-client');
+    const baseComposablesPath = path.join(process.cwd(), 'packages', 'composables');
+
+    const basePublishOptions = {
+      token: token,
+      tag: npmTag,
+      access: 'public',
+      checkVersion: true,
+    };
+
+    const apiReturn = await npmPublish({
+      package: path.resolve(baseApiClientPath, './package.json'),
+      ...basePublishOptions,
+    });
+
+    const composerReturn = await npmPublish({
+      package: path.resolve(baseComposablesPath, './package.json'),
+      ...basePublishOptions,
+    });
+
+    console.log(JSON.stringify(apiReturn, null, 2));
+    console.log(JSON.stringify(composerReturn, null, 2));
+  }
+}
+
+module.exports = {
+  publishPackages,
+}
