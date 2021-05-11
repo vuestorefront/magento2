2271dd6d49c431a90a248ac4f04d10bde27bfd97
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-05-11 17:36:49 -0300

    chore: release version 1.0.0-beta.7

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,15 +1,14 @@
 const { publishPackages } = require('./lib/publishPackages');
 
-(async () => {
-  const myArgs = process.argv.slice(2);
-  const token = myArgs[1];
-  const labels = myArgs[0].split('|');
+const myArgs = process.argv.slice(2);
+const token = process.env.NPM_TOKEN || myArgs[1];
+const labels = myArgs[0].split('|');
 
-  if(token || labels.length <= 0) return false;
+if (token || labels.length <= 0) return false;
 
-  try {
-    await publishPackages(labels, token);
-  } catch (e) {
+return publishPackages(labels, token)
+  .then((result) => {
+    console.log(result);
+  }).catch((e) => {
     throw e;
-  }
-})();
+  });
