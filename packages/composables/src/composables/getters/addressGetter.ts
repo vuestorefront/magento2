44a5f603c6da8dbb977a210bc3251252c2baa5ee
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-05-14 01:56:37 -0300

    refactor(composeables): added sort and filter to countriesList getter

--- packages/composables/src/composables/getters/addressGetter.ts
+++ packages/composables/src/composables/getters/addressGetter.ts
@@ -1,12 +1,15 @@
 import { Countries } from '@vue-storefront/magento-api';
 import { AddressGetter } from '../../types';
 
-const countriesList: AddressGetter['countriesList'] = (countries: Countries[]) => countries.map((c) => ({
-  id: c.id,
-  label: c.full_name_locale,
-  englishLabel: c.full_name_english,
-  abbreviation: c.two_letter_abbreviation,
-}));
+const countriesList: AddressGetter['countriesList'] = (countries: Countries[]) => countries
+  .filter((c) => c.id && c.full_name_english && c.full_name_locale)
+  .map((c) => ({
+    id: c.id,
+    label: c.full_name_locale,
+    englishLabel: c.full_name_english,
+    abbreviation: c.two_letter_abbreviation,
+  }))
+  .sort((a, b) => a.label.localeCompare(b.label));
 
 export default {
   countriesList,
