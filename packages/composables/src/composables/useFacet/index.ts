8bdbf27c513920a6ec3e08bc68f5c87d2aba63cf
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-05-17 12:56:10 -0300

    refactor(composeables): updated useFacet and category getter to display categories correctly

--- packages/composables/src/composables/useFacet/index.ts
+++ packages/composables/src/composables/useFacet/index.ts
@@ -64,7 +64,13 @@ const factoryParams = {
   search: async (context: Context, params: FacetSearchResult<any>) => {
     const itemsPerPage = (params.input.itemsPerPage) ? params.input.itemsPerPage : 20;
     const inputFilters = (params.input.filters) ? params.input.filters : {};
-    const categoryId = (params.input.categoryId) ? { category_uid: { eq: params.input.categoryId } } : {};
+    const categoryId = (params.input.categoryId) ? {
+      category_uid: {
+        ...(Array.isArray(params.input.categoryId)
+          ? { in: params.input.categoryId }
+          : { eq: params.input.categoryId }),
+      },
+    } : {};
     const productParams: ProductsSearchParams = {
       filter: {
         ...categoryId,
