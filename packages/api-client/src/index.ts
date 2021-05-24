40632883c7d44794ad1cc8e3420c60331ecef9ed
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-05-24 17:29:34 -0300

    refactor(api-client): added missing typing export

--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -1,6 +1,7 @@
 export * from './types/setup';
 export * from './types/API';
 export * from './types/context';
+export { SetPaymentMethodOnCartInputs } from './api/setPaymentMethodOnCart';
 export {
   AddBundleProductsToCartInput,
   AddBundleProductsToCartOutput,
