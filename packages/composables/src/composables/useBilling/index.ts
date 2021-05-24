7627c8f3836107e4351b51ceca99c673e6e9cf9f
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-05-24 16:15:22 -0300

    refactor(composeables): fixed magento shipping bug

--- packages/composables/src/composables/useBilling/index.ts
+++ packages/composables/src/composables/useBilling/index.ts
@@ -8,12 +8,14 @@ import {
   SetBillingAddressOnCartInput,
 } from '@vue-storefront/magento-api';
 import useCart from '../useCart';
+import useShippingProvider from '../useShippingProvider';
 
 const factoryParams: UseBillingParams<any, any> = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   provide() {
     return {
       cart: useCart(),
+      useShippingProvider: useShippingProvider(),
     };
   },
 
@@ -53,6 +55,22 @@ const factoryParams: UseBillingParams<any, any> = {
     };
 
     const { data } = await context.$magento.api.setBillingAddressOnCart(setBillingAddressOnCartInput);
+
+    /**
+     * This is a workaround needed due to Magento GraphQL API
+     * cleaning the Shipping method after defining the billing address
+     */
+    const shippingMethod = context.useShippingProvider.state.value;
+
+    await context.useShippingProvider.save({
+      shippingMethod: {
+        carrier_code: shippingMethod.carrier_code,
+        method_code: shippingMethod.method_code,
+      },
+    });
+    /**
+     * End of GraphQL Workaround
+     */
 
     return data.setBillingAddressOnCart.cart.billing_address;
   },
