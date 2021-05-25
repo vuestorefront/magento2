4dae76dd6f83647a2f77bc2421fd495e1db9318a
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-05-25 01:18:49 -0300

    refactor(theme): added login for magento coupon

--- packages/theme/components/Checkout/CartPreview.vue
+++ packages/theme/components/Checkout/CartPreview.vue
@@ -44,9 +44,9 @@
       />
       <SfButton
         class="promo-code__button"
-        @click="() => applyCoupon({ couponCode: promoCode })"
+        @click="handleCoupon"
       >
-        {{ $t('Apply') }}
+        {{ promoIsApplied ? $t('Remove') : $t('Apply') }}
       </SfButton>
     </div>
     <div class="highlighted">
@@ -65,15 +65,34 @@
 import {
   SfHeading,
   SfButton,
-  SfCollectedProduct,
   SfProperty,
   SfCharacteristic,
   SfInput,
-  SfCircleIcon,
 } from '@storefront-ui/vue';
-import { computed, ref } from '@vue/composition-api';
+import {
+  computed, onMounted, watch, ref,
+} from '@vue/composition-api';
 import { useCart, cartGetters } from '@vue-storefront/magento';
 import getShippingMethodPrice from '~/helpers/checkout/getShippingMethodPrice';
+
+const CHARACTERISTICS = [
+  {
+    title: 'Safety',
+    description: 'It carefully packaged with a personal touch',
+    icon: 'safety',
+  },
+  {
+    title: 'Easy shipping',
+    description:
+      'You’ll receive dispatch confirmation and an arrival date',
+    icon: 'shipping',
+  },
+  {
+    title: 'Changed your mind?',
+    description: 'Rest assured, we offer free returns within 30 days',
+    icon: 'return',
+  },
+];
 
 export default {
   name: 'CartPreview',
@@ -90,17 +109,36 @@ export default {
       removeItem,
       updateItemQty,
       applyCoupon,
+      removeCoupon,
     } = useCart();
 
     const listIsHidden = ref(false);
     const promoCode = ref('');
-    const showPromoCode = ref(false);
+
+    const promoIsApplied = computed(() => cartGetters.getAppliedCoupon(cart.value)?.code);
 
     const products = computed(() => cartGetters.getItems(cart.value));
     const totalItems = computed(() => cartGetters.getTotalItems(cart.value));
     const totals = computed(() => cartGetters.getTotals(cart.value));
     const discounts = computed(() => cartGetters.getDiscounts(cart.value));
     const hasSpecialPrice = computed(() => totals.value.special > 0 && totals.value.special < totals.value.subtotal);
+    const selectedShippingMethod = computed(() => cartGetters.getSelectedShippingMethod(cart.value));
+
+    const setCartCoupon = () => {
+      promoCode.value = promoIsApplied.value;
+    };
+
+    onMounted(setCartCoupon);
+
+    watch(promoIsApplied, setCartCoupon);
+
+    const handleCoupon = async () => {
+      await (
+        promoIsApplied.value
+          ? removeCoupon({ currentCart: cart.value })
+          : applyCoupon({ couponCode: promoCode.value })
+      );
+    };
 
     return {
       cart,
@@ -110,31 +148,14 @@ export default {
       products,
       totals,
       promoCode,
-      showPromoCode,
       removeItem,
       updateItemQty,
       cartGetters,
       getShippingMethodPrice,
-      applyCoupon,
-      characteristics: [
-        {
-          title: 'Safety',
-          description: 'It carefully packaged with a personal touch',
-          icon: 'safety',
-        },
-        {
-          title: 'Easy shipping',
-          description:
-            'You’ll receive dispatch confirmation and an arrival date',
-          icon: 'shipping',
-        },
-        {
-          title: 'Changed your mind?',
-          description: 'Rest assured, we offer free returns within 30 days',
-          icon: 'return',
-        },
-      ],
-      selectedShippingMethod: computed(() => cartGetters.getSelectedShippingMethod(cart.value)),
+      promoIsApplied,
+      handleCoupon,
+      characteristics: CHARACTERISTICS,
+      selectedShippingMethod,
       hasSpecialPrice,
     };
   },
@@ -147,19 +168,23 @@ export default {
   width: 100%;
   background-color: var(--c-light);
   padding: var(--spacer-xl) var(--spacer-xl) 0;
+
   &:last-child {
     padding-bottom: var(--spacer-xl);
   }
 }
+
 .total-items {
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin-bottom: var(--spacer-xl);
 }
+
 .property {
   margin-bottom: var(--spacer-base);
 }
+
 .property-total {
   margin-top: var(--spacer-xl);
   padding-top: var(--spacer-lg);
@@ -174,13 +199,16 @@ export default {
     margin-bottom: var(--spacer-base);
   }
 }
+
 .promo-code {
   display: flex;
   align-items: flex-start;
+
   &__button {
     --button-width: 6.3125rem;
     --button-height: var(--spacer-lg);
   }
+
   &__input {
     --input-background: var(--c-white);
     flex: 1;
