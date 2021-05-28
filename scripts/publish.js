91ca18955e41ceb2bfe529ad7d8213b322632f00
Renan Oliveira <renan.oliveira0@gmail.com>
2021-05-27 22:36:21 -0300

    chore: adjust and fix on publish scripts (#82)
    * chore: little adjust and fix on publish scripts
    
    * fix: fix var name
    
    * Delete .npmrc
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Heitor Ramon Ribeiro <heitor.ramon@gmail.com>

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,12 +1,14 @@
+/* eslint-disable unicorn/no-process-exit */
 const { publishPackages } = require('./lib/publishPackages');
 
 const myArgs = process.argv.slice(2);
-const token = myArgs[1];
 const labels = myArgs[0].split('|');
 
-return publishPackages(labels, token)
-  .then((result) => {
-    console.log(result);
-  }).catch((e) => {
-    throw e;
+console.log(publishPackages);
+
+publishPackages(labels)
+  .then(console.log)
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
   });
