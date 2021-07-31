a5847fae76ab00f6a76105fe6528fa5b1ba7874a
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-07-30 23:28:00 -0300

    chore(api-client): updated rollup.config.js

--- packages/api-client/rollup.config.js
+++ packages/api-client/rollup.config.js
@@ -1,11 +1,13 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript2';
+/* eslint-disable import/no-extraneous-dependencies */
+import commonjs from '@rollup/plugin-commonjs';
 import graphql from '@rollup/plugin-graphql';
 import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 import { generateBaseConfig } from '../../rollup.base.config';
 
-const extensions = ['.ts', '.graphql', '.js'];
+const extensions = ['.js', '.jsx', '.es6', '.es', '.mjs', '.ts', '.graphql'];
 
 const server = {
   input: 'src/index.server.ts',
@@ -25,9 +27,17 @@ const server = {
     nodeResolve({
       extensions,
     }),
-    typescript({ useTsconfigDeclarationDir: true }),
     graphql(),
     json(),
+    typescript({
+      rollupCommonJSResolveHack: false,
+      useTsconfigDeclarationDir: true,
+      // eslint-disable-next-line unicorn/prefer-module
+      tslib: require.resolve('typescript'),
+    }),
+    commonjs({
+      extensions,
+    }),
   ],
 };
 
