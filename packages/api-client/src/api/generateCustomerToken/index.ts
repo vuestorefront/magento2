c21f14a6ca27214ef4871b5dce0bf760f2357ec8
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-08-06 23:06:17 -0300

    refactor(api-client): added error manipulation

--- packages/api-client/src/api/generateCustomerToken/index.ts
+++ packages/api-client/src/api/generateCustomerToken/index.ts
@@ -1,18 +1,30 @@
 import { FetchResult } from '@apollo/client';
 import generateCustomerToken from './generateCustomerToken';
-import { GenerateCustomerTokenMutation, GenerateCustomerTokenMutationVariables } from '../../types/GraphQL';
+import {
+  CreateCustomerMutation,
+  CreateCustomerMutationVariables,
+  GenerateCustomerTokenMutation,
+  GenerateCustomerTokenMutationVariables,
+} from '../../types/GraphQL';
 import { Context } from '../../types/context';
+import createCustomer from '../createCustomer/createCustomer';
 
 export default async (
   { client }: Context,
   email: string,
   password: string,
-): Promise<FetchResult<GenerateCustomerTokenMutation>> => client
-  .mutate<GenerateCustomerTokenMutation, GenerateCustomerTokenMutationVariables>({
-  mutation: generateCustomerToken,
-  variables: {
-    email,
-    password,
-  },
-  fetchPolicy: 'no-cache',
-});
+): Promise<FetchResult<GenerateCustomerTokenMutation>> => {
+  try {
+    return await client
+      .mutate<GenerateCustomerTokenMutation, GenerateCustomerTokenMutationVariables>({
+      mutation: generateCustomerToken,
+      variables: {
+        email,
+        password,
+      },
+      fetchPolicy: 'no-cache',
+    });
+  } catch (error) {
+    throw error.graphQLErrors?.[0].message || error.networkError?.result || error;
+  }
+};
