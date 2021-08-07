fadcf22f00e5d14c3f4bf7e0b3ea6d7d8f9705ab
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-08-07 15:48:11 -0300

    feat(composables): added CmsBlocks loading

--- packages/composables/src/composables/useContent/index.ts
+++ packages/composables/src/composables/useContent/index.ts
@@ -1,12 +1,16 @@
 import { Context } from '@vue-storefront/core';
-import { Page } from '@vue-storefront/magento-api';
+import { Page, CmsBlock } from '@vue-storefront/magento-api';
 import { useContentFactory, UseContentFactoryParams } from '../../factories/useContentFactory';
 
-const factoryParams: UseContentFactoryParams<Page> = {
+const factoryParams: UseContentFactoryParams<Page, CmsBlock> = {
   loadContent: async (context: Context, identifier: string) => {
     const result = await context.$magento.api.cmsPage(identifier);
     return result.data.cmsPage;
   },
+  loadBlocks: async (context: Context, identifiers: string[]) => {
+    const result = await context.$magento.api.cmsBlocks(identifiers);
+    return result.data.cmsBlocks.items;
+  },
 };
 
-export default useContentFactory<Page>(factoryParams);
+export default useContentFactory<Page, CmsBlock>(factoryParams);
