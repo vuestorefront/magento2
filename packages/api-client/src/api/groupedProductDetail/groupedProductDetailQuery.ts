1f0372f0b2914e2172a4e2a16c00bbc77150d5b9
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-08-13 00:24:05 -0300

    refactor(api-client): fixed graphql queries

--- packages/api-client/src/api/groupedProductDetail/groupedProductDetailQuery.ts
+++ packages/api-client/src/api/groupedProductDetail/groupedProductDetailQuery.ts
@@ -1,14 +1,59 @@
 import gql from 'graphql-tag';
-import GroupedProductOptionsData from '../../fragments/groupedProductOptionsFragment';
 
 export default gql`
-  ${GroupedProductOptionsData}
-  
-query groupedProductDetail($search: String = "", $filter: ProductAttributeFilterInput, $pageSize: Int = 20, $currentPage: Int = 1, $sort: ProductAttributeSortInput) {
-  products(search: $search, filter: $filter, sort: $sort, pageSize: $pageSize, currentPage: $currentPage) {
-    items {
-      uid
-      ...GroupedProductOptionsData
+  query groupedProductDetail($search: String = "", $filter: ProductAttributeFilterInput, $pageSize: Int = 20, $currentPage: Int = 1, $sort: ProductAttributeSortInput) {
+    products(
+      search: $search
+      filter: $filter
+      sort: $sort
+      pageSize: $pageSize
+      currentPage: $currentPage
+    ) {
+      items {
+        uid
+        ... on GroupedProduct {
+          items {
+            position
+            qty
+            product {
+              uid
+              __typename
+              sku
+              name
+              stock_status
+              only_x_left_in_stock
+              price_range {
+                maximum_price {
+                  final_price {
+                    currency
+                    value
+                  }
+                  regular_price {
+                    currency
+                    value
+                  }
+                }
+                minimum_price {
+                  final_price {
+                    currency
+                    value
+                  }
+                  regular_price {
+                    currency
+                    value
+                  }
+                }
+              }
+              thumbnail {
+                url
+                position
+                disabled
+                label
+              }
+            }
+          }
+        }
+      }
     }
   }
-}`;
+`;
