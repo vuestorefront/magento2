4824971249c21cb50d82bb28da9d10100d08608d
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-08-19 00:21:57 -0300

    refactor(api-client): updated wishlist queries

--- packages/api-client/src/api/addProductToWishList/addProductsToWishlist.ts
+++ packages/api-client/src/api/addProductToWishList/addProductsToWishlist.ts
@@ -3,6 +3,7 @@ import WishlistData from '../../fragments/wishlistDataFragment';
 
 export default gql`
   ${WishlistData}
+  
 mutation addProductsToWishlist($id: ID!, $items: [WishlistItemInput!]!) {
   addProductsToWishlist(wishlistId: $id, wishlistItems: $items) {
     wishlist {
--- packages/api-client/src/api/removeProductsFromWishlist/removeProductsFromWishlist.ts
+++ packages/api-client/src/api/removeProductsFromWishlist/removeProductsFromWishlist.ts
@@ -3,6 +3,7 @@ import WishlistData from '../../fragments/wishlistDataFragment';
 
 export default gql`
   ${WishlistData}
+  
 mutation removeProductsFromWishlist($id: ID!, $items: [ID!]!) {
   removeProductsFromWishlist(wishlistId: $id, wishlistItemsIds: $items) {
     wishlist {
