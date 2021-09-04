58b5a8fb5329b2536a82bffceefc05be014df99b
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-09-04 17:40:27 -0300

    refactor(composables): add initial logic for resetting the password

--- packages/composables/src/composables/useForgotPassword/index.ts
+++ packages/composables/src/composables/useForgotPassword/index.ts
@@ -1,21 +1,31 @@
 import {
   Context,
   useForgotPasswordFactory,
-  UseForgotPasswordFactoryParams,
 } from '@vue-storefront/core';
+import { UseForgotPasswordFactoryParams } from '../../factories/useForgotPasswordFactory';
 
 const factoryParams: UseForgotPasswordFactoryParams<any> = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   resetPassword: async (context: Context, { email, customQuery }) => {
-    const { data } = await context.$magento.api.requestPasswordResetEmail({ email });
+    const { data } = await context.$magento.api.requestPasswordResetEmail({ email }, customQuery);
 
     return data;
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  setNewPassword: async (context: Context, { tokenValue, newPassword, customQuery }) => {
-    console.log('Mocked: setNewPassword');
-    return {};
+  setNewPassword: async (context: Context, {
+    tokenValue,
+    newPassword,
+    email,
+    customQuery,
+  }) => {
+    const { data } = await context.$magento.resetPassword({
+      email,
+      newPassword,
+      resetPasswordToken: tokenValue,
+    }, customQuery);
+
+    return data;
   },
 };
 
