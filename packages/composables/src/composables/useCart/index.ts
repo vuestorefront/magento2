2ff2b5042ce30ac9fa2abeb784b8afd6a26789c0
Bogdan <bogdan.podlesnii@gmail.com>
2021-09-05 23:23:06 +0300

    fix: set cart id for logged in customer

--- packages/composables/src/composables/useCart/index.ts
+++ packages/composables/src/composables/useCart/index.ts
@@ -66,6 +66,7 @@ const factoryParams: UseCartFactoryParams<Cart, CartItem, Product> = {
     if (customerToken) {
       try {
         const result = await context.$magento.api.customerCart();
+        apiState.setCartId(result.data.customerCart.id);
 
         return result.data.customerCart as unknown as Cart;
       } catch {
