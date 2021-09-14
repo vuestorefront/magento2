7ecff1eff5129188d1eefe4b14a573f9778023c8
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-09-14 00:14:55 -0300

    docs: update base information in docs (#176)
    * docs: added api-extraction
    
    * docs: fixed api-extraction
    
    * docs: fixed api-extraction
    
    * docs: updated about page

---
+++ packages/api-client/src/api-extractor-data.ts
@@ -0,0 +1,15 @@
+/**
+ * `api-client` for Magento 2 integration for Vue Storefront 2.
+ *
+ * @remarks
+ * The `@vue-storefront/magento2` library includes everything needed to fetch data from the
+ * Magento 2 eCommerce platform. This includes API client configuration, API endpoints, and
+ * GraphQL types and fragments.
+ *
+ * @packageDocumentation
+ */
+
+import * as apiMethods from './api';
+
+export * from './index';
+export { apiMethods };
