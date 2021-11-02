32b018e3b50016037b271e30cd4836618b69805b
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-11-02 17:52:27 -0300

    refactor(api-client): added parameters for user wishlist pagination

--- packages/api-client/src/api/wishlist/index.ts
+++ packages/api-client/src/api/wishlist/index.ts
@@ -1,14 +1,49 @@
 import { ApolloQueryResult } from 'apollo-client';
-import { WishlistQuery, WishlistQueryVariables } from '../../types/GraphQL';
-import wishlist from './wishlist';
+import { CustomQuery } from '@vue-storefront/core';
+import gql from 'graphql-tag';
+import {
+  WishlistQuery,
+  WishlistQueryVariables,
+} from '../../types/GraphQL';
+import wishlistQuery from './wishlist';
 import { Context } from '../../types/context';
 
+type Variables = {
+  pageSize: number;
+  currentPage: number;
+};
+
 export default async (
-  { client }: Context,
-  queryParams: WishlistQueryVariables,
-): Promise<ApolloQueryResult<WishlistQuery>> => client
-  .query<WishlistQuery, WishlistQueryVariables>({
-  query: wishlist,
-  fetchPolicy: 'no-cache',
-  variables: queryParams,
-});
+  context: Context,
+  searchParams: WishlistQueryVariables,
+  customQuery?: CustomQuery,
+): Promise<ApolloQueryResult<WishlistQuery>> => {
+  const defaultParams = {
+    pageSize: 10,
+    currentPage: 1,
+    ...searchParams,
+  };
+
+  const variables: Variables = {
+    pageSize: defaultParams.pageSize <= 0 ? 10 : defaultParams.pageSize,
+    currentPage: defaultParams.currentPage <= 0 ? 1 : defaultParams.currentPage,
+  };
+
+  const { wishlist } = context.extendQuery(
+    customQuery, {
+      wishlist: {
+        query: wishlistQuery,
+        variables,
+      },
+    },
+  );
+  try {
+    return await context.client
+      .query<WishlistQuery, WishlistQueryVariables>({
+      query: gql`${wishlist.query}`,
+      variables: wishlist.variables,
+    });
+  } catch (error) {
+    throw error.graphQLErrors?.[0].message || error.networkError?.result || error;
+  }
+};
