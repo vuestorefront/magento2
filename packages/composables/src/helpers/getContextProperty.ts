9afe23ea179a65c66a26179c1be4820430d1f956
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-11-29 19:14:09 -0300

    refactor(composables): changed to anom function

--- packages/composables/src/helpers/getContextProperty.ts
+++ packages/composables/src/helpers/getContextProperty.ts
@@ -1,6 +1,3 @@
-export function getContextProperty<RETURN_TYPE>(
-  object: { [key: string]: unknown },
-  name: string
-) {
-  return (object?.[`$${name}`] || object?.[name]) as RETURN_TYPE;
-}
+export const getContextProperty = <RETURN_TYPE>(object: Record<string, any>, name: string) => (
+  object?.[`$${name}`]
+  || object?.[name]) as RETURN_TYPE;
