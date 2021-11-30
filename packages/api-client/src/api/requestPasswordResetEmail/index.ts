4ae7d9fc993b09423f1da0cbf3c439064ff6eedc
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-11-30 20:58:00 -0300

    refactor(api-client): fixed wishlist and reset password api

--- packages/api-client/src/api/requestPasswordResetEmail/index.ts
+++ packages/api-client/src/api/requestPasswordResetEmail/index.ts
@@ -13,37 +13,21 @@ export default async (
   input: RequestPasswordResetEmailMutationVariables,
   customQuery?: CustomQuery,
 ): Promise<FetchResult<RequestPasswordResetEmailMutation>> => {
-  const { requestPasswordResetEmail } = context.extendQuery(
-    customQuery, {
-      requestPasswordResetEmail: {
-        query: requestPasswordResetEmailMutation,
-        variables: input,
-      },
+  const { requestPasswordResetEmail } = context.extendQuery(customQuery, {
+    requestPasswordResetEmail: {
+      query: requestPasswordResetEmailMutation,
+      variables: input,
     },
-  );
+  });
 
-  try {
-    Logger.debug('[VSF: Magento] requestPasswordResetEmail', JSON.stringify(input, null, 2));
-    const result = await context.client
-      .mutate<RequestPasswordResetEmailMutation, RequestPasswordResetEmailMutationVariables>({
-      mutation: gql`${requestPasswordResetEmail.query}`,
-      variables: requestPasswordResetEmail.variables,
-    });
+  Logger.debug('[VSF: Magento] requestPasswordResetEmail', JSON.stringify(input, null, 2));
+  const result = await context.client
+    .mutate<RequestPasswordResetEmailMutation, RequestPasswordResetEmailMutationVariables>({
+    mutation: gql`${requestPasswordResetEmail.query}`,
+    variables: requestPasswordResetEmail.variables,
+  });
 
-    if (!result.data.requestPasswordResetEmail) throw new Error('Email was not found, or not available.');
+  if (!result.data.requestPasswordResetEmail) throw new Error('Email was not found, or not available.');
 
-    return result;
-  } catch (error) {
-    // For error in data we don't throw 500, because it's not server error
-    if (error.graphQLErrors) {
-      Logger.debug(error);
-
-      return {
-        errors: error.graphQLErrors,
-        data: null,
-      };
-    }
-    Logger.error(error);
-    throw error.networkError?.result || error;
-  }
+  return result;
 };
