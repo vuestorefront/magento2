e59ffc47b1d7c1d5d1369d37975903fe83e27604
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-11-30 16:28:06 -0300

    refactor(composables): added new getters for storeConfigGetters

--- packages/composables/src/getters/storeConfigGetters.ts
+++ packages/composables/src/getters/storeConfigGetters.ts
@@ -2,9 +2,10 @@ const getCode = (config) => config.store_code;
 const getTitle = (config) => config.default_title;
 const getName = (config) => config.store_name;
 const getCurrency = (config) => config.default_display_currency_code;
-const getLocale = (config) => config.locale;
+const getLocale = (config) => config.locale.replace('_', '-');
 const allowGuestProductReview = (config) => config.allow_guests_to_write_product_reviews;
 const enabledWishlist = (config) => config.magento_wishlist_general_is_enabled;
+const isSelectedStore = (config) => (store) => config.store_code === store.store_code;
 
 const storeConfigGetters = {
   getCode,
@@ -14,6 +15,7 @@ const storeConfigGetters = {
   getLocale,
   allowGuestProductReview,
   enabledWishlist,
+  isSelectedStore,
 };
 
 export default storeConfigGetters;
