83527fd4a784d96be3d28850ef0d3382386f667a
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-11-30 16:27:45 -0300

    refactor(theme): improved store selection component

--- packages/theme/components/LocaleSelector.vue
+++ packages/theme/components/LocaleSelector.vue
@@ -12,23 +12,33 @@
     </SfButton>
     <SfBottomModal
       :is-open="isLangModalOpen"
-      :title="availableStores.length > 0 ? 'Change language': ''"
+      :title="availableStores.length > 0 ? 'Change Store': ''"
       @click:close="isLangModalOpen = !isLangModalOpen"
     >
-      <SfList>
-        <SfListItem v-for="store in availableStores" :key="store.id">
+      <SfList
+        v-if="availableStores.length > 1"
+      >
+        <SfListItem
+          v-for="store in availableStores"
+          :key="store.id"
+        >
           <a
             href="/"
             class="container__store--link"
-            :class="isStoreSelected(store) ? 'container__store--selected' : ''"
-            @click="handleChangeStore(store)"
+            :class="storeConfigGetters.isSelectedStore(storeConfig)(store) ? 'container__store--selected' : ''"
+            @click="handleChanges(() => changeStore(store))"
           >
             <SfCharacteristic class="language">
               <template #title>
-                <span>{{ store.store_name }} - {{ store.locale }}</span>
+                <span>{{ storeConfigGetters.getName(store) }}</span>
               </template>
               <template #icon>
-                <SfImage :src="`/icons/langs/${getStoreLocale(store)}.webp`" width="20" alt="Flag" class="language__flag" />
+                <SfImage
+                  :src="`/icons/langs/${storeConfigGetters.getLocale(store)}.webp`"
+                  width="20"
+                  alt="Flag"
+                  class="language__flag"
+                />
               </template>
             </SfCharacteristic>
           </a>
@@ -41,19 +51,55 @@
         class="container__lang--title"
       />
 
-      <SfList>
+      <SfList
+        v-if="availableCurrencies.length > 1"
+      >
         <SfListItem
           v-for="currency in availableCurrencies"
           :key="currency"
         >
-          <a href="/"
-             @click.prevent="changeCurrency(currency)">
+          <a
+            href="/"
+            :class="selectedCurrency === currency ? 'container__currency--selected' : ''"
+            @click.prevent="handleChanges(() => changeCurrency(currency), false, true)"
+          >
             <SfCharacteristic class="currency">
               <template #title>
                 <span>{{ currency }}</span>
               </template>
             </SfCharacteristic>
           </a>
+        </SfListItem>
+      </SfList>
+
+      <SfHeading
+        v-if="availableLocales.length > 1"
+        :level="3"
+        title="Choose language"
+        class="container__lang--title"
+      />
+      <SfList
+        v-if="availableLocales.length > 1"
+      >
+        <SfListItem
+          v-for="lang in availableLocales"
+          :key="lang.code"
+        >
+          <nuxt-link :to="switchLocalePath(lang.code)">
+            <SfCharacteristic class="language">
+              <template #title>
+                <span>{{ lang.label }}</span>
+              </template>
+              <template #icon>
+                <SfImage
+                  :src="`/icons/langs/${lang.code}.webp`"
+                  width="20"
+                  alt="Flag"
+                  class="language__flag"
+                />
+              </template>
+            </SfCharacteristic>
+          </nuxt-link>
         </SfListItem>
       </SfList>
     </SfBottomModal>
@@ -66,6 +112,7 @@ import {
   useStore,
   useCurrency,
   useCart,
+  storeConfigGetters,
 } from '@vue-storefront/magento';
 import {
   SfImage,
@@ -75,9 +122,15 @@ import {
   SfBottomModal,
   SfCharacteristic,
 } from '@storefront-ui/vue';
-import { ref, computed, defineComponent } from '@nuxtjs/composition-api';
+import {
+  ref,
+  computed,
+  defineComponent,
+  useRoute,
+  useRouter,
+} from '@nuxtjs/composition-api';
 import { useI18n } from '~/helpers/hooks/usei18n';
-import { onSSR } from '@vue-storefront/core';
+import { useMagentoConfiguration } from '~/composables/useMagentoConfiguration';
 
 export default defineComponent({
   components: {
@@ -90,60 +143,67 @@ export default defineComponent({
   },
   setup() {
     const {
-      loadConfig,
+      selectedCurrency,
+      selectedLocale,
+    } = useMagentoConfiguration();
+
+    const {
       config,
     } = useConfig();
 
     const {
-      load: loadStores,
       stores,
       change: changeStore,
     } = useStore();
 
     const {
-      load: loadCurrencies,
       currencies,
       change: changeCurrency,
     } = useCurrency();
 
-    const { clear, cart } = useCart();
+    const route = useRoute();
+    const router = useRouter();
 
-    const { locales, locale } = useI18n();
+    const {
+      path,
+      fullPath,
+    } = route.value;
+    const {
+      locales,
+      locale,
+    } = useI18n();
     const isLangModalOpen = ref(false);
+
     const availableLocales = computed(() => [...locales].filter((i) => (Object.keys(i).length > 0 && typeof i === 'object' ? i.code !== locale : i !== locale)));
-
-    onSSR(async () => {
-      await Promise.all([
-        loadConfig(),
-        loadStores(),
-        loadCurrencies(),
-      ]);
-    });
-
     const availableStores = computed(() => stores.value ?? []);
-    const selectedStore = computed(() => config.value?.store_code);
-    const availableCurrencies = computed(() => currencies.value?.available_currency_codes);
 
-    const handleChangeStore = async (store) => {
-      // isLangModalOpen.value = false;
-      if (cart?.value) await clear(cart);
-      changeStore(store);
+    const availableCurrencies = computed(() => currencies.value?.available_currency_codes);
+    const handleChanges = async (cb, redirect = true, refresh = false) => {
+      if (cb) {
+        await cb();
+      }
+      if (redirect) {
+        await router.replace(path);
+      }
+
+      if (refresh) {
+        router.go(fullPath);
+      }
     };
 
-    const isStoreSelected = (store) => selectedStore.value?.id === store.id;
-    const getStoreLocale = (store) => store?.locale ?? locale;
-
     return {
-      availableStores,
-      selectedStore,
-      availableLocales,
-      locale,
-      isLangModalOpen,
-      handleChangeStore,
-      isStoreSelected,
-      getStoreLocale,
-      changeCurrency,
       availableCurrencies,
+      availableLocales,
+      availableStores,
+      changeCurrency,
+      changeStore,
+      handleChanges,
+      isLangModalOpen,
+      locale,
+      selectedCurrency,
+      selectedLocale,
+      storeConfig: config,
+      storeConfigGetters,
     };
   },
 });
@@ -156,6 +216,7 @@ export default defineComponent({
   flex-wrap: nowrap;
   align-items: center;
   position: relative;
+
   .sf-bottom-modal {
     z-index: 2;
     left: 0;
@@ -163,17 +224,38 @@ export default defineComponent({
       --bottom-modal-height: 100vh;
     }
   }
+
+  .sf-bottom-modal::v-deep .sf-bottom-modal__close {
+    position: var(--circle-icon-position, absolute);
+    top: var(--spacer-xs);
+    right: var(--spacer-xs);
+  }
+
   .sf-list {
     .language {
       padding: var(--spacer-sm);
+
       &__flag {
         margin-right: var(--spacer-sm);
       }
     }
+
     @include for-desktop {
       display: flex;
     }
   }
+
+  &__store {
+    &--selected {
+      font-weight: bold;
+    }
+  }
+  &__currency {
+    &--selected {
+      font-weight: bold;
+    }
+  }
+
   &__lang {
     width: 20px;
     --button-box-shadow: none;
@@ -183,9 +265,24 @@ export default defineComponent({
     align-items: center;
     opacity: 0.5;
     border: none;
+
     &:hover,
     &--selected {
       opacity: 1;
+    }
+
+    &--title {
+      --heading-title-font-weight: var(--font-weight--normal);
+      padding: var(
+              --bottom-modal-title-padding,
+              var(--spacer-sm) var(--spacer-lg)
+      );
+      color: var(--bottom-modal-title-color, var(--c-text));
+      text-align: var(--bottom-modal-title-text-align, center);
+      @include for-mobile {
+        --heading-title-font-size: var(--font-size--xs);
+        --heading-title-font-weight: var(--font-weight--bold);
+      }
     }
   }
 }
