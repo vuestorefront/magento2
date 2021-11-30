9e82451dfe491d027e8dab7a4d6efca175350978
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-11-30 20:58:44 -0300

    refactor(composables): improved facet prices filter

--- packages/composables/src/composables/useFacet/index.ts
+++ packages/composables/src/composables/useFacet/index.ts
@@ -34,7 +34,7 @@ const constructFilterObject = (inputFilters: Object) => {
   Object.keys(inputFilters).forEach((key) => {
     if (key === 'price') {
       const price = { from: 0, to: 0 };
-      const flatPrices = inputFilters[key].flatMap((inputFilter) => inputFilter.split('_')).sort();
+      const flatPrices = inputFilters[key].flatMap((inputFilter) => inputFilter.split('_').map((str) => Number.parseFloat(str))).sort((a, b) => a - b);
 
       [price.from] = flatPrices;
       price.to = flatPrices[flatPrices.length - 1];
