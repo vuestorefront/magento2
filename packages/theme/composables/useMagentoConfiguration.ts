892aeac96ace5ee485661c41a6bbe495db795a87
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-11-30 16:28:22 -0300

    refactor(theme): added new props for useMagentoConfiguration

--- packages/theme/composables/useMagentoConfiguration.ts
+++ packages/theme/composables/useMagentoConfiguration.ts
@@ -4,7 +4,7 @@ import {
   useStore,
   storeConfigGetters,
 } from '@vue-storefront/magento';
-import { useContext } from '@nuxtjs/composition-api';
+import { computed, useContext } from '@nuxtjs/composition-api';
 import cookieNames from '~/enums/cookieNameEnum';
 
 export const useMagentoConfiguration = () => {
@@ -24,6 +24,12 @@ export const useMagentoConfiguration = () => {
     load: loadCurrencies,
   } = useCurrency();
 
+  const selectedCurrency = computed(() => app.$cookies.get(cookieNames.currencyCookieName));
+
+  const selectedLocale = computed(() => app.$cookies.get(cookieNames.localeCookieName));
+
+  const selectedStore = computed(() => app.$cookies.get(cookieNames.storeCookieName));
+
   const loadConfiguration = async () => {
     await Promise.all([
       loadConfig(),
@@ -32,29 +38,25 @@ export const useMagentoConfiguration = () => {
     ]);
 
     if (!app.$cookies.get(cookieNames.storeCookieName)) {
-      console.log('defining store');
       app.$cookies.set(cookieNames.storeCookieName, storeConfigGetters.getCode(storeConfig.value));
     }
 
     if (!app.$cookies.get(cookieNames.localeCookieName)) {
-      console.log('defining locale');
       app.$cookies.set(cookieNames.localeCookieName, storeConfigGetters.getLocale(storeConfig.value));
     }
 
     if (!app.$cookies.get(cookieNames.currencyCookieName)) {
-      console.log('defining currency');
       app.$cookies.set(cookieNames.currencyCookieName, storeConfigGetters.getCurrency(storeConfig.value));
     }
-
-    /*    if (!app.$cookies.get(cookieNames.countryCookieName)) {
-      app.$cookies.set(cookieNames.countryCookieName, storeConfigGetters.(storeConfig.value));
-    } */
   };
 
   return {
     currencies,
     stores,
     storeConfig,
+    selectedCurrency,
+    selectedLocale,
+    selectedStore,
     loadConfiguration,
   };
 };
