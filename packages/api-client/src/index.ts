265e982816fec4fe8d3e30627dd789bd9439ad77
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-12-16 14:01:46 -0300

    bugfix(api-client): removed braintree leftover

--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -52,8 +52,6 @@ export {
   AvailableStoresQueryVariables,
   BillingAddressInput,
   BillingCartAddress,
-  BraintreeCcVaultInput,
-  BraintreeInput,
   Breadcrumb,
   BundleCartItem,
   BundleCreditMemoItem,
