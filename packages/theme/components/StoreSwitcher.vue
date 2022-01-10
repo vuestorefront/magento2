adf3ca24a4c41b98e1260b6138b99dd719d046fe
Bartosz Herba <bartoszherba@gmail.com>
2022-01-10 07:42:10 +0100

    fix(theme): refactor and fix store switcher (#383)
    - update nuxt.config.js to handle de translation
    - fix issue with flag icon #362
    - refactor store switcher component
    - add store name next to the language icon flag
    - add translation info in docs
    
    Co-authored-by: Bartosz Herba <bherba@vuestorefront.io>

--- packages/theme/components/StoreSwitcher.vue
+++ packages/theme/components/StoreSwitcher.vue
@@ -6,63 +6,109 @@
     >
       <SfCharacteristic class="store-switcher">
         <template #title>
-          <span class="desktop-only">{{ storeConfig.store_name }}</span>
+          <span>{{ storeConfig.store_name }}</span>
         </template>
-        <template #icon v-if="storeConfigGetters.getLocale(storeConfig)">
-          <nuxt-img
+        <template #icon>
+          <SfImage
             :src="`/icons/langs/${storeConfigGetters.getLocale(storeConfig)}.webp`"
             width="20"
-            height="20"
             alt="Flag"
             class="language__flag"
           />
         </template>
       </SfCharacteristic>
     </SfButton>
-    <LazyHydrate when-visible>
-      <StoresModal
-        v-if="isLangModalOpen"
-        :store-config="storeConfig"
-        :is-lang-modal-open="isLangModalOpen"
-        @closeModal="isLangModalOpen = false"
-      />
-    </LazyHydrate>
+    <SfBottomModal
+      :is-open="isLangModalOpen"
+      :title="availableStores.length > 0 ? 'Change Store': ''"
+      @click:close="isLangModalOpen = !isLangModalOpen"
+    >
+      <SfList
+        v-if="availableStores.length > 1"
+      >
+        <SfListItem
+          v-for="store in availableStores"
+          :key="store.id"
+        >
+          <a
+            href="/"
+            class="container__store--link"
+            :class="storeGetters.getSelected(storeConfig, store) ? 'container__store--selected' : ''"
+            @click="handleChanges({
+              callback: () => changeStore(store),
+              redirect: false,
+              windowRefresh: true,
+            })"
+          >
+            <SfCharacteristic class="language">
+              <template #title>
+                <span>{{ storeConfigGetters.getName(store) }}</span>
+              </template>
+              <template #icon>
+                <SfImage
+                  :src="`/icons/langs/${storeConfigGetters.getLocale(store)}.webp`"
+                  width="20"
+                  alt="Flag"
+                  class="language__flag"
+                />
+              </template>
+            </SfCharacteristic>
+          </a>
+        </SfListItem>
+      </SfList>
+    </SfBottomModal>
   </div>
 </template>
 
 <script>
-import LazyHydrate from 'vue-lazy-hydration';
 import {
   useConfig,
+  useStore,
   storeConfigGetters,
   storeGetters,
 } from '@vue-storefront/magento';
 import {
+  SfImage,
   SfButton,
+  SfList,
+  SfBottomModal,
   SfCharacteristic,
 } from '@storefront-ui/vue';
 import {
   ref,
+  computed,
   defineComponent,
 } from '@nuxtjs/composition-api';
-import StoresModal from '~/components/StoreSwitcher/StoresModal.vue';
+import { useHandleChanges } from '~/helpers/magentoConfig/handleChanges';
 
 export default defineComponent({
   name: 'StoreSwitcher',
   components: {
-    StoresModal,
+    SfImage,
     SfButton,
+    SfList,
+    SfBottomModal,
     SfCharacteristic,
-    LazyHydrate,
   },
   setup() {
     const {
       config,
     } = useConfig();
 
+    const {
+      stores,
+      change: changeStore,
+    } = useStore();
+
+    const { handleChanges } = useHandleChanges();
     const isLangModalOpen = ref(false);
 
+    const availableStores = computed(() => stores.value ?? []);
+
     return {
+      availableStores,
+      changeStore,
+      handleChanges,
       isLangModalOpen,
       storeConfig: config,
       storeConfigGetters,
@@ -73,30 +119,84 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.language {
-  &__flag {
-    margin-right: var(--spacer-sm);
-  }
-}
 
-.container__lang {
-  --button-box-shadow: none;
-  background: none;
-  padding: 0 5px;
+.container {
   display: flex;
+  flex-wrap: nowrap;
   align-items: center;
-  opacity: 0.5;
-  border: none;
+  position: relative;
 
-  &:hover,
-  &--selected {
-    opacity: 1;
+  .sf-bottom-modal {
+    z-index: 2;
+    left: 0;
+    @include for-desktop {
+      --bottom-modal-height: 100vh;
+    }
   }
-}
 
-.store-switcher {
-  text-transform: capitalize;
-  color: var(--bottom-modal-title-color, var(--c-link));
-  font-size: var(--font-size--sm);
+  .sf-bottom-modal::v-deep .sf-bottom-modal__close {
+    position: var(--circle-icon-position, absolute);
+    top: var(--spacer-xs);
+    right: var(--spacer-xs);
+  }
+
+  .language {
+    padding: var(--spacer-sm);
+
+    &__flag {
+      margin-right: var(--spacer-sm);
+    }
+  }
+
+  .sf-list {
+    @include for-desktop {
+      display: flex;
+    }
+  }
+
+  &__store {
+    &--selected {
+      font-weight: bold;
+    }
+  }
+
+  &__lang {
+    --button-box-shadow: none;
+    background: none;
+    padding: 0 5px;
+    display: flex;
+    align-items: center;
+    opacity: 0.5;
+    border: none;
+
+    &:hover,
+    &--selected {
+      opacity: 1;
+    }
+
+    &--title {
+      --heading-title-font-weight: var(--font-weight--normal);
+      padding: var(
+              --bottom-modal-title-padding,
+              var(--spacer-sm) var(--spacer-lg)
+      );
+      color: var(--bottom-modal-title-color, var(--c-text));
+      text-align: var(--bottom-modal-title-text-align, center);
+      @include for-mobile {
+        --heading-title-font-size: var(--font-size--xs);
+        --heading-title-font-weight: var(--font-weight--bold);
+      }
+    }
+  }
+
+  .sf-characteristic__text {
+    color: var(--bottom-modal-title-color, var(--c-text));
+  }
+
+  .store-switcher {
+    text-transform: capitalize;
+    color: var(--bottom-modal-title-color, var(--c-link));
+    font-size: var(--font-size--sm);
+  }
 }
 </style>
