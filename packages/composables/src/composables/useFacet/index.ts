624bdc7ec15c7805fe1c3208e978261cee209b6e
Aliaaaam <88658555+Aliaaaam@users.noreply.github.com>
2022-01-13 20:16:06 +0330

    Fix useFacet(...) customQuery implemention (#413)

--- packages/composables/src/composables/useFacet/index.ts
+++ packages/composables/src/composables/useFacet/index.ts
@@ -94,7 +94,7 @@ const factoryParams = {
       currentPage: productParams.page,
     };
 
-    const { data } = await context.$magento.api.products(productSearchParams);
+    const { data } = await context.$magento.api.products(productSearchParams, params?.input.customQuery || {});
 
     Logger.debug('[Result]:', { data });
 
