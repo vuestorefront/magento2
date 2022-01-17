3243f7b3e822923bfcf6f78919ec6dfa0af7a06b
Kevin Gorjan <kevingorjan@gmail.com>
2022-01-17 09:46:45 +0100

    test(composables): reverted back and removed jest.base.config.json

--- packages/composables/jest.config.js
+++ packages/composables/jest.config.js
@@ -1,20 +1,70 @@
-const baseConfig = require('./../../jest.base.config');
-
 /* eslint-disable unicorn/prefer-module */
 module.exports = {
-  ...baseConfig,
-  
-  collectCoverageFrom: [
-    '<rootDir>/composables/**/*.ts'
+  globals: {
+    __DEV__: true,
+  },
+  coverageReporters: [
+    'lcov',
   ],
-
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  
+  coverageThreshold: {
+    global: {},
+  },
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '.d.ts$',
+    '/__mocks__/',
+  ],
+  coverageDirectory: './coverage/',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+  },
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(ts)$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
   },
-
-  testEnvironment: 'jsdom',
-
+  transformIgnorePatterns: [
+    'node_modules',
+    '<rootDir>/node_modules',
+  ],
+  testMatch: [
+    '<rootDir>/**/__tests__/**/*spec.[jt]s?(x)',
+  ],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+    [
+      'jest-watch-toggle-config',
+      {
+        setting: 'verbose',
+      },
+    ],
+    [
+      'jest-watch-toggle-config',
+      {
+        setting: 'collectCoverage',
+      },
+    ],
+    [
+      'jest-watch-toggle-config',
+      {
+        setting: 'notify',
+      },
+    ],
+    [
+      'jest-watch-toggle-config',
+      {
+        setting: 'bail',
+      },
+    ],
+  ],
+  collectCoverageFrom: [
+    '<rootDir>/composables/**/*.ts',
+  ],
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'ts',
+  ],
 };
