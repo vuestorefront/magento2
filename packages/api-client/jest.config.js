8f3986b8b091921d0827aafd5d1fb12dc0ef9779
Kevin Gorjan <kevingorjan@gmail.com>
2022-01-17 10:18:36 +0100

    test(api-client): reverted back and removed jest.base.config.json

--- packages/api-client/jest.config.js
+++ packages/api-client/jest.config.js
@@ -1,22 +1,71 @@
-const baseConfig = require('./../../jest.base.config');
-
 /* eslint-disable unicorn/prefer-module */
 module.exports = {
-  ...baseConfig,
-
+  globals: {
+    __DEV__: true,
+  },
+  coverageReporters: [
+    'lcov',
+  ],
+  coverageThreshold: {
+    global: {},
+  },
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '.d.ts$',
+    '/__mocks__/',
+  ],
+  coverageDirectory: './coverage/',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    'api-client(.*)$': '<rootDir>$1',
+  },
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts)$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules',
+    '<rootDir>/node_modules',
+  ],
+  testMatch: [
+    '<rootDir>/**/__tests__/**/*spec.[jt]s?(x)',
+  ],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+    [
+      'jest-watch-toggle-config',
+      {
+        setting: 'verbose',
+      },
+    ],
+    [
+      'jest-watch-toggle-config',
+      {
+        setting: 'collectCoverage',
+      },
+    ],
+    [
+      'jest-watch-toggle-config',
+      {
+        setting: 'notify',
+      },
+    ],
+    [
+      'jest-watch-toggle-config',
+      {
+        setting: 'bail',
+      },
+    ],
+  ],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
   ],
-
-  moduleNameMapper: {
-    ...baseConfig.moduleNameMapper,
-    'api-client(.*)$': '<rootDir>$1',
-  },
-
-  moduleFileExtensions: ['js', 'json', 'ts'],
-
-  testEnvironment: 'jsdom',
-
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'ts',
+  ],
 };
