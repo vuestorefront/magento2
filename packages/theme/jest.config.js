f60013fb20e441552bc4dfaf41f0b2c7b690e6f5
Kevin Gorjan <kevingorjan@gmail.com>
2022-01-17 09:44:22 +0100

    tests(theme): reverted back and removed jest.base.config.json

--- packages/theme/jest.config.js
+++ packages/theme/jest.config.js
@@ -1,41 +1,54 @@
-const baseConfig = require('./../../jest.base.config');
-
 /* eslint-disable unicorn/prefer-module */
 module.exports = {
-  ...baseConfig,
+  globals: { __DEV__: true },
 
-  collectCoverage: false,
+  coverageReporters: ['lcov'],
 
-  testEnvironment: 'jsdom',
+  coverageThreshold: { global: {} },
+
+  coveragePathIgnorePatterns: ['/node_modules/', '.d.ts$', '/__mocks__/'],
+
+  coverageDirectory: './coverage/',
 
   moduleNameMapper: {
-    ...baseConfig.moduleNameMapper,
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js',
   },
 
-  moduleFileExtensions: ['js', 'vue', 'json', 'ts'],
-  
+  testEnvironment: 'jsdom',
+
   transform: {
-    ...baseConfig.transform,
+    '^.+\\.(ts)$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
     '^.+\\.vue$': 'vue-jest',
     '^.+\\.(css|svg)': 'jest-transform-stub',
   },
 
+  transformIgnorePatterns: ['node_modules/(?!(@storefront-ui)|vee-validate/dist/rules|nouislider)'],
+
+  testMatch: ['<rootDir>/**/__tests__/**/*spec.[jt]s?(x)'],
+
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+    ['jest-watch-toggle-config', { setting: 'verbose' }],
+    ['jest-watch-toggle-config', { setting: 'collectCoverage' }],
+    ['jest-watch-toggle-config', { setting: 'notify' }],
+    ['jest-watch-toggle-config', { setting: 'bail' }],
+  ],
+
+  collectCoverage: false,
+
+  moduleFileExtensions: ['js', 'vue', 'json', 'ts'],
+
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue',
-  ],
+    '<rootDir>/pages/**/*.vue'],
 
-  setupFiles: [
-    'jest-date-mock',
-    'jest-localstorage-mock',
-  ],
+  setupFiles: ['jest-date-mock', 'jest-localstorage-mock'],
 
   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
-
-  transformIgnorePatterns: [
-    'node_modules/(?!(@storefront-ui)|vee-validate/dist/rules|nouislider)',
-  ],
 
   modulePathIgnorePatterns: ['_theme', 'tests/e2e'],
 };
