469cecb5627fdf0ae5a9045a80e7495db49cf7d7
Kevin Gorjan <kevingorjan@gmail.com>
2022-01-17 11:13:11 +0100

    test: scaffold unit tests in composables and api client package (#419)
    * test(composables): added jest config
    
    * tests(composables): rewritten test + fix in productGetter
    
    * tests(composables): updated configuration in jest.config.js
    
    * tests(api-client): added tests for api-client + move general config to jest.base.config.js
    
    * tests(api-client): added jest configuration + simple test
    
    * chore: removed jest-environment-jsdom-sixteen
    
    * tests(theme): reverted back and removed jest.base.config.json
    
    * test(composables): reverted back and removed jest.base.config.json
    
    * test(api-client): reverted back and removed jest.base.config.json
    
    * chore: removed jest.base.config.json
    
    Co-authored-by: Marcin Kwiatkowski <marcin.kwiatkowski@vuestorefront.io>

--- packages/composables/jest.config.js
+++ packages/composables/jest.config.js
@@ -1,9 +1,70 @@
-const baseConfig = require('./../../jest.base.config');
-
+/* eslint-disable unicorn/prefer-module */
 module.exports = {
-  ...baseConfig,
-  coverageReporters: ['html', 'lcov', 'text'],
-  rootDir: __dirname,
-  setupFilesAfterEnv: ['./__tests__/setup.ts'],
-  watchPathIgnorePatterns: ['/node_modules/']
+  globals: {
+    __DEV__: true,
+  },
+  coverageReporters: [
+    'lcov',
+  ],
+  coverageThreshold: {
+    global: {},
+  },
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '.d.ts$',
+    '/__mocks__/',
+  ],
+  coverageDirectory: './coverage/',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+  },
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts)$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules',
+    '<rootDir>/node_modules',
+  ],
+  testMatch: [
+    '<rootDir>/**/__tests__/**/*spec.[jt]s?(x)',
+  ],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+    [
+      'jest-watch-toggle-config',
+      {
+        setting: 'verbose',
+      },
+    ],
+    [
+      'jest-watch-toggle-config',
+      {
+        setting: 'collectCoverage',
+      },
+    ],
+    [
+      'jest-watch-toggle-config',
+      {
+        setting: 'notify',
+      },
+    ],
+    [
+      'jest-watch-toggle-config',
+      {
+        setting: 'bail',
+      },
+    ],
+  ],
+  collectCoverageFrom: [
+    '<rootDir>/composables/**/*.ts',
+  ],
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'ts',
+  ],
 };
