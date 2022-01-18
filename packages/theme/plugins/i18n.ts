46db4a279c116c41439fd5437126258d70129f97
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2022-01-18 00:20:09 -0300

    refactor(theme): updated i18n middleware to fix URL redirect

--- packages/theme/plugins/i18n.ts
+++ packages/theme/plugins/i18n.ts
@@ -1,3 +1,6 @@
+import { ConfigState } from '@vue-storefront/magento-api';
+import { Context, NuxtAppOptions } from '@nuxt/types';
+import { LocaleObject } from 'nuxt-i18n';
 import cookieNames from '~/enums/cookieNameEnum';
 
 /**
@@ -6,7 +9,7 @@ import cookieNames from '~/enums/cookieNameEnum';
  * @param app {NuxtAppOptions} - Nuxt App options object
  * @returns {string} - vsf-store cookie value
  */
-const readStoreCookie = (app) => app.$cookies.get(cookieNames.storeCookieName);
+const readStoreCookie = (app: NuxtAppOptions) => app.$cookies.get(cookieNames.storeCookieName);
 
 /**
  * Find locale code based on magento store code
@@ -14,7 +17,13 @@ const readStoreCookie = (app) => app.$cookies.get(cookieNames.storeCookieName);
  * @param locales {array} - array with locales
  * @returns boolean
  */
-const findLocaleBasedOnStoreCode = (storeCode, locales) => locales.find((locale) => locale.code === storeCode);
+const findLocaleBasedOnStoreCode = (storeCode: string, locales: string[] | LocaleObject[]) => {
+  if (locales.some((l) => typeof l !== 'string')) {
+    return !!(locales as LocaleObject[]).find((locale) => locale.code === storeCode);
+  }
+
+  return (locales as string[]).includes(storeCode);
+};
 
 /**
  * Set default locale
@@ -29,22 +38,21 @@ const setDefaultLocale = async (i18n) => {
  * Prepare new cookie string based on app state.
  *
  * @param apiState {ConfigState}
+ * @param newStoreCode {string}
  * @returns {string}
  */
-const prepareNewCookieString = (apiState, newStoreCode) => {
-  let cookie = `vsf-store=${newStoreCode}; `;
+const prepareNewCookieString = (apiState: ConfigState, newStoreCode: string) => {
+  const customerTokenCookie = apiState.getCustomerToken();
+  const cartIdCookie = apiState.getCartId();
 
+  let cookie = `vsf-store=${newStoreCode}; `;
   cookie += `vsf-locale=${newStoreCode}; `;
   cookie += `vsf-currency=${apiState.getCurrency()}; `;
   cookie += `vsf-country=${apiState.getCountry()}; `;
 
-  const customerTokenCookie = apiState.getCustomerToken();
-
   if (customerTokenCookie) {
     cookie += `vsf-customer=${customerTokenCookie}; `;
   }
-
-  const cartIdCookie = apiState.getCartId();
 
   if (cartIdCookie) {
     cookie += `vsf-cart=${cartIdCookie} `;
@@ -53,9 +61,8 @@ const prepareNewCookieString = (apiState, newStoreCode) => {
   return cookie;
 };
 
-export default ({ app }) => {
-  app.$vsf.$magento.client.interceptors.request.use(async (request) => {
-
+export default async ({ app }: Context) => {
+  await app.$vsf.$magento.client.interceptors.request.use(async (request) => {
     const { i18n } = app;
     const currentStoreCode = readStoreCookie(app);
 
@@ -63,14 +70,14 @@ export default ({ app }) => {
     if (!currentStoreCode || !findLocaleBasedOnStoreCode(currentStoreCode, i18n.locales)) {
       await setDefaultLocale(i18n);
 
-      return;
+      return request;
     }
 
     const i18nCurrentLocaleCode = i18n.locale;
     const localeCookie = app.$cookies.get(cookieNames.localeCookieName);
 
     if (i18nCurrentLocaleCode !== localeCookie) {
-      const apiState = app.$vsf.$magento.config.state;
+      const apiState = app.$vsf.$magento.config.state as ConfigState;
 
       apiState.setStore(i18nCurrentLocaleCode);
       apiState.setLocale(i18nCurrentLocaleCode);
