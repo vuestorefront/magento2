24ddc54b887f6f8cac07136b80b019c244640b7d
Bartosz Herba <bherba@vuestorefront.io>
2022-01-19 09:15:43 +0100

    docs(theme): add currency formatting plugin information

--- docs/plugins/index.md
+++ docs/plugins/index.md
@@ -26,3 +26,24 @@ client and server side.
 
 
 ![i18n flow](./i18n-plugin-diagram.png)
+
+## Currency
+
+We expose `$fc` (format currency) function as a plugin to allow you to convert any number or string into a formatted currency string. Under the hood plugins uses Intl.NumberFormat class to handle formatting and therefore exposes the same options configuration.
+Here are few examples of how you can use it in templates.
+```javascript
+$fc(productGetters.getPrice(product).regular) // by default vsf-currency cookie is used to define target currency
+$fc(productGetters.getPrice(product).regular, { currency: 'USD' }) // you can easily override this behaviour by passing configuration object
+```
+
+Configuration object is an interface Intl.NumberFormatOptions, check it for all available options.
+
+---
+***NOTE***
+
+Why we are not using built-in i18n package currency configuration and $n function to cover currency displaying? The answer is because we must be able to handle multiple currencies within one store but in i18n package, currency and locale are in 1:1 relation when in Magento one store can have multiple languages and currencies set. Currency and locale are necessarily disjointed which is not true for i18n plugin.
+
+You can still use all i18n native functions to perform any formatting though.
+
+---
+
--- packages/theme/plugins/formatCurrency.ts
+++ packages/theme/plugins/formatCurrency.ts
@@ -2,17 +2,22 @@ import cookieNames from '~/enums/cookieNameEnum';
 
 declare module 'vue/types/vue' {
   interface Vue {
-    $fc(value: number): string;
-    $fc(value: number, options?: Intl.NumberFormatOptions): string;
-    $fc(value: number, locale?: string, options?: Intl.NumberFormatOptions): string;
+    $fc(value: number | string): string;
+    $fc(value: number | string, options?: Intl.NumberFormatOptions): string;
+    $fc(value: number | string, locale?: string, options?: Intl.NumberFormatOptions): string;
   }
 }
 
 export default ({ app }, inject) => {
-  inject('fc', (value: number, locale: string = '', options = {}): string => {
-    const localeToUse: string = locale || app.$cookies.get(cookieNames.localeCookieName);
+  inject('fc', (value: number | string, locale: string = '', options = {}): string => {
+    if (typeof value === 'string') {
+      // eslint-disable-next-line no-param-reassign
+      value = Number(value);
+    }
+    // eslint-disable-next-line no-param-reassign
+    locale = locale || app.$cookies.get(cookieNames.localeCookieName);
     const currency: string = app.$cookies.get(cookieNames.currencyCookieName);
 
-    return new Intl.NumberFormat(localeToUse, { style: 'currency', currency, ...options }).format(value);
+    return new Intl.NumberFormat(locale, { style: 'currency', currency, ...options }).format(value);
   });
 };
