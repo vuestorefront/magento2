635114e5da91f5feb5e1c610138bef029c844a0f
Kevin Gorjan <kevingorjan@gmail.com>
2022-01-24 09:25:57 +0100

    test: added tests for asyncLocalStorage and the steps helper

---
+++ packages/theme/helpers/checkout/__tests__/steps.spec.js
@@ -0,0 +1,26 @@
+import { isPreviousStepValid } from '~/helpers/checkout/steps'
+
+const CHECKOUT_DATA = {
+  'my-account': {a: 'b', c: 'd'}
+}
+
+describe('steps :: steps helper for the checkout', () => {
+
+  beforeEach(async () => {
+    localStorage.clear();
+
+    localStorage.setItem('vsf-checkout', JSON.stringify(CHECKOUT_DATA))
+  });
+
+  test('user can continue', async () => {
+    const isValid = await isPreviousStepValid('my-account')
+
+    expect(isValid).toBeTruthy();
+  });
+
+  test('user can\'t continue', async () => {
+    const isValid = await isPreviousStepValid('billing')
+
+    expect(isValid).toBeFalsy();
+  });
+});
