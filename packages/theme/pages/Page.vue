39c410fd75ac52cdfb4867f8e03979bd10eaf68f
Marcin Kwiatkowski <marcin.kwiatkowski@vuestorefront.io>
2022-01-28 08:47:05 +0100

    feat: added support for @vue-storefront/cache module (#482)
    * chore(theme): added nuxt-image
    
    * feat(theme): added nuxt-img to category page
    
    * feat: added nuxt-img to category pages, added newe configs, and useImage compoosable
    
    * feat(theme): added nuxt-img to products carousel, added image enums
    
    * feat(theme): added nuxt-img for search results
    
    * feat(theme): added nuxt-img to my account wishlist
    
    * feat(theme): changed log to use nuxt-img
    
    * feat(theme): changed empty cart icon to use nuxt-img
    
    * feat(theme): added nuxt-img to instagram feed, updated image names
    
    * feat(theme): changed error image on search results to use nuxt-img
    
    * feat(theme): update store switcher to use nuxt-img instead of SfImage
    
    * feat(theme): added nuxt-img to wishlist sidebar
    
    * feat(theme): added nuxt-img to MobileStoreBanner
    
    * feat(theme): added nuxt-img to grouoped product selector
    
    * feat(theme): added nuxt-img to payment component
    
    * docs: added docs for external images providers
    
    * build(cloud): updated vuestorefront cloud docker file and added new args
    
    * build: updated deploy gh action to add image env vatiables
    
    * docs(configuration): updated docs for image providers
    
    * build: added missed buildargs
    
    * build: fixed dockerfile
    
    * build: fixed typo in docker file
    
    * build: updated config
    
    * test: add unit tests for the useImage composable
    
    * chore(theme): added @vue-storefront-cache module
    
    * chore(theme): added config for @vue-storefront/cache to nuxt.config.js
    
    * feat(theme): added cache tags to home and category page
    
    * chore(theme): added redis-cache module, fixed cache tags for category page
    
    * feat(theme): added cache tags to product page, added body parser server middleware
    
    * build: added config for redis, modified deployment config for tests
    
    * build: removed unnecessary redis_password env variable
    
    * build: fixed redis env variables names
    
    * feat(theme): added cache tags for cms pages
    
    * chore(theme): removed console.logs, added comments to nuxt.config.js
    
    * build: removed redis-cache module from package.json and addet it to dockerfile
    
    * build: updated dockerfile
    
    * build(updzted docker file): updated dovkerfile
    
    * build: added REDIS__ENABLED env variable
    
    * docs: added docs for redis caching
    
    * build: reverted test changes in deploy action
    
    * fix(theme): removed typo from Category template
    
    n
    
    * build: fixed GH deploy action

--- packages/theme/pages/Page.vue
+++ packages/theme/pages/Page.vue
@@ -23,6 +23,7 @@ import { onSSR } from '@vue-storefront/core';
 import { defineComponent, useContext, useRoute } from '@nuxtjs/composition-api';
 import { useCache, CacheTagPrefix } from '@vue-storefront/cache';
 import HTMLContent from '~/components/HTMLContent';
+import { useCache, CacheTagPrefix } from '@vue-storefront/cache';
 
 export default defineComponent({
   components: {
