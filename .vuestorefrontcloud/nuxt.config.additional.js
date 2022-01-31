d6e6791f253c2aa08d1b16f8838617cb09c0dd5b
Marcin Kwiatkowski <marcin.kwiatkowski@vuestorefront.io>
2022-01-31 15:38:18 +0100

    build: introduced nuxt.config.additional.js

---
+++ .vuestorefrontcloud/nuxt.config.additional.js
@@ -0,0 +1,23 @@
+import config from '@vue-storefront/magento-theme/config.js';
+import baseNuxtConfig from '@vue-storefront/magento-theme/nuxt.config';
+
+export default {
+  ...baseNuxtConfig,
+  modules: [
+    ...baseNuxtConfig.modules,
+    '@nuxtjs/recaptcha',
+  ],
+  recaptcha: {
+    hideBadge: config.get('recaptchaHideBadge'), // Hide badge element (v3 & v2 via size=invisible)
+    siteKey: config.get('recaptchaSiteKey'), // Site key for requests
+    version: config.get('recaptchaVersion'), // Version 2 or 3
+    size: config.get('recaptchaSize'), // Size: 'compact', 'normal', 'invisible' (v2)
+  },
+  publicRuntimeConfig: {
+    isRecaptcha: config.get('recaptchaEnabled'),
+  },
+  sentry: {
+    dsn: process.env.SENTRY_DSN,
+    tracing: true,
+  },
+};
