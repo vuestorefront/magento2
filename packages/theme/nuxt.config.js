8ef5da7c9f37b23f63a7f93e13b42bc7a5d3e18a
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2022-02-03 04:36:36 -0300

    chore: updated dependencies & configuration management (#538)
    * docs: updated docs to match missing elements
    
    * chore: added missing Redis password env
    
    * refactor(theme): added missing configuration envs
    
    Added missing configuration envs for Redis and Sentry
    
    * chore(eslint): updated dependencies
    
    - updated `eslint-plugin-unicorn` to `40.1.0`
    
    * chore(eslint-jest): updated dependencies
    
    - updated `eslint-plugin-jest` to `26.0.0`
    
    * chore(eslint-typescript): updated dependencies
    
    - updated `@typescript-eslint/eslint-plugin` to `5.10.1`
    - updated `@typescript-eslint/parser` to `5.10.1`
    
    * chore(eslint-vue): updated dependencies
    
    - updated `eslint-plugin-vue` to `8.4.0`
    
    * chore(api-client): updated dependencies
    
    - updated `@apollo/client` to `3.5.8`
    - updated `graphql` to `16.3.0`
    - updated `cross-fetch` to `3.1.5`
    - updated `dotenv` to `14.3.2`
    - updated `msw` to `0.36.7`
    - updated `typescript` to `4.5.5`
    
    * chore(theme): updated dependencies
    
    - updated `@sentry/tracing` to `6.17.3`
    - updated `@storefront-ui/vue` to `0.12.0`
    - updated `isomorphic-dompurify` to `0.18.0`
    - updated `@babel/core` to `7.16.12`
    - updated `cypress` to `9.3.1`
    - updated `dotenv` to `14.3.2`
    - updated `lint-staged` to `12.3.2`
    - updated `mocha` to `9.2.0`
    - updated `npm-check-updates` to `12.2.1`
    - updated `typescript` to `4.5.5`
    
    * chore: updated dependencies
    
    - updated `@babel/core` to `7.16.12`
    - updated `@babel/plugin-proposal-private-methods` to `7.16.11`
    - updated `@commitlint/cli` to `16.1.0`
    - updated `@graphql-codegen/cli` to `2.4.0`
    - updated `@types/node` to `17.0.13`
    - updated `@typescript-eslint/parser` to `5.10.1`
    - updated `eslint` to `8.8.0`
    - updated `lint-staged` to `12.3.2`
    - updated `npm-check-updates` to `12.2.1`
    - updated `rollup` to `2.66.1`
    - updated `vue-eslint-parser` to `8.2.0`
    
    * chore(eslint-typescript): updated dependencies
    
    - `@typescript-eslint/eslint-plugin` to `5.10.2`
    - `@typescript-eslint/parser` to `5.10.2`
    
    * chore(api-client): updated dependencies
    
    - `dotenv` to `15.0.0`
    - `msw` to `0.36.8`
    
    * chore(theme): rollback storefront-ui update
    
    * chore: updated dependencies
    
    - `@graphql-codegen/typescript-operations` to `2.2.3`
    - `@types/node` to `17.0.14`
    - `@typescript-eslint/parser` to `5.10.2`
    - `lint-staged` to `12.3.3`
    
    * chore: moved sentry package to Dockerfile
    
    * chore(theme): added dynamic nuxt.config.js generation
    
    * chore: removed REDIS__PASSWORD
    
    * chore: removed REDIS__PASSWORD from workflow

--- packages/theme/nuxt.config.js
+++ packages/theme/nuxt.config.js
@@ -21,67 +21,124 @@ const {
   },
 } = middleware;
 
-export default {
-  ssr: true,
-  dev: config.get('nuxtAppEnvironment') !== 'production',
-  server: {
-    port: config.get('nuxtAppPort'),
-    host: '0.0.0.0',
-  },
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
-      },
-    ],
-    link: [
-      {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico',
-      },
-    ],
-  },
-  loading: { color: '#fff' },
-  buildModules: [
-    // to core
-    '@nuxtjs/composition-api/module',
-    '@nuxt/typescript-build',
-    '@nuxtjs/google-fonts',
-    '@nuxtjs/pwa',
-    '@nuxtjs/style-resources',
-    ['@vue-storefront/nuxt', {
+export default () => {
+  const baseConfig = {
+    ssr: true,
+    dev: config.get('nuxtAppEnvironment') !== 'production',
+    server: {
+      port: config.get('nuxtAppPort'),
+      host: '0.0.0.0',
+    },
+    head: {
+      title: process.env.npm_package_name || '',
+      meta: [
+        { charset: 'utf-8' },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: process.env.npm_package_description || '',
+        },
+      ],
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico',
+        },
+      ],
+    },
+    loading: { color: '#fff' },
+    buildModules: [
+      // to core
+      '@nuxtjs/composition-api/module',
+      '@nuxt/typescript-build',
+      '@nuxtjs/google-fonts',
+      '@nuxtjs/pwa',
+      '@nuxtjs/style-resources',
+      ['@vue-storefront/nuxt', {
+        // @core-development-only-start
+        coreDevelopment: true,
+        logger: {
+          verbosity: 'debug',
+        },
+        // @core-development-only-end
+        useRawSource: {
+          dev: [
+            '@vue-storefront/magento',
+            '@vue-storefront/core',
+          ],
+          prod: [
+            '@vue-storefront/magento',
+            '@vue-storefront/core',
+          ],
+        },
+      }],
       // @core-development-only-start
-      coreDevelopment: true,
-      logger: {
-        verbosity: 'debug',
-      },
+      ['@vue-storefront/nuxt-theme', {
+        generate: {
+          replace: {
+            apiClient: '@vue-storefront/magento-api',
+            composables: '@vue-storefront/magento',
+          },
+        },
+        routes: false,
+      }],
       // @core-development-only-end
-      useRawSource: {
-        dev: [
-          '@vue-storefront/magento',
-          '@vue-storefront/core',
-        ],
-        prod: [
-          '@vue-storefront/magento',
-          '@vue-storefront/core',
-        ],
-      },
-    }],
-    // @core-development-only-start
-    ['@vue-storefront/nuxt-theme', {
-      generate: {
-        replace: {
-          apiClient: '@vue-storefront/magento-api',
-          composables: '@vue-storefront/magento',
+      /* project-only-start
+      ['@vue-storefront/nuxt-theme', {
+        routes: false,
+      }],
+      project-only-end */
+      ['@vue-storefront/magento/nuxt', {
+        i18n: {
+          useNuxtI18nConfig: true,
+        },
+        cookies,
+        externalCheckout,
+        defaultStore,
+        facets,
+        magentoBaseUrl,
+        imageProvider,
+      }],
+      '@nuxt/image',
+    ],
+    modules: [
+      ['nuxt-i18n', {
+        baseUrl: process.env.BASE_URL || 'http://localhost:3000',
+      }],
+      'cookie-universal-nuxt',
+      'vue-scrollto/nuxt',
+      '@vue-storefront/middleware/nuxt',
+      '@nuxt/image',
+      ['@vue-storefront/cache/nuxt', {
+        enabled: false,
+        invalidation: {
+          handlers: [
+            '@vue-storefront/cache/defaultHandler',
+          ],
+        },
+        driver: [],
+      }],
+    ],
+    i18n: {
+      country: 'US',
+      strategy: 'prefix',
+      locales: [
+        {
+          code: 'default',
+          file: 'en.js',
+          iso: 'en_US',
+          defaultCurrency: 'USD',
+        },
+        {
+          code: 'german',
+          file: 'de.js',
+          iso: 'de_DE',
+          defaultCurrency: 'EUR',
         },
       },
       routes: false,
@@ -131,17 +188,11 @@ export default {
           },
         },
       ],
-    }],
-  ],
-  i18n: {
-    country: 'US',
-    strategy: 'prefix',
-    locales: [
-      {
-        code: 'default',
-        file: 'en.js',
-        iso: 'en_US',
-        defaultCurrency: 'USD',
+      defaultLocale: 'default',
+      autoChangeCookie: {
+        currency: false,
+        locale: false,
+        country: false,
       },
       {
         code: 'german',
@@ -174,59 +225,73 @@ export default {
             currency: 'USD',
             currencyDisplay: 'symbol',
           },
-        },
-        german: {
-          currency: {
-            style: 'currency',
-            currency: 'EUR',
-            currencyDisplay: 'symbol',
+          german: {
+            currency: {
+              style: 'currency',
+              currency: 'EUR',
+              currencyDisplay: 'symbol',
+            },
           },
         },
       },
+      detectBrowserLanguage: false,
     },
-    detectBrowserLanguage: false,
-  },
-  pwa: {
-    meta: {
-      theme_color: '#5ECE7B',
-    },
-  },
-  googleFonts: {
-    families: {
-      Raleway: {
-        wght: [300, 400, 500, 600, 700],
-        ital: [400],
-      },
-      Roboto: {
-        wght: [300, 400, 500, 700],
-        ital: [300, 400],
+    pwa: {
+      meta: {
+        theme_color: '#5ECE7B',
       },
     },
-    display: 'swap',
-  },
-  styleResources: {
-    scss: [require.resolve('@storefront-ui/shared/styles/_helpers.scss', { paths: [process.cwd()] })],
-  },
-  build: {
-    babel: {
-      plugins: [
-        ['@babel/plugin-proposal-private-methods', { loose: true }],
+    googleFonts: {
+      families: {
+        Raleway: {
+          wght: [300, 400, 500, 600, 700],
+          ital: [400],
+        },
+        Roboto: {
+          wght: [300, 400, 500, 700],
+          ital: [300, 400],
+        },
+      },
+      display: 'swap',
+    },
+    styleResources: {
+      scss: [require.resolve('@storefront-ui/shared/styles/_helpers.scss', { paths: [process.cwd()] })],
+    },
+    build: {
+      babel: {
+        plugins: [
+          ['@babel/plugin-proposal-private-methods', { loose: true }],
+        ],
+      },
+      transpile: [
+        'vee-validate/dist/rules',
       ],
-    },
-    transpile: [
-      'vee-validate/dist/rules',
-    ],
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.VERSION': JSON.stringify({
-          // eslint-disable-next-line global-require
-          version: require('./package.json').version,
-          lastCommit: process.env.LAST_COMMIT || '',
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.VERSION': JSON.stringify({
+            // eslint-disable-next-line global-require
+            version: require('./package.json').version,
+            lastCommit: process.env.LAST_COMMIT || '',
+          }),
         }),
-      }),
+      ],
+      extractCSS: {
+        allChunks: true,
+      },
+    },
+    plugins: [
+      '~/plugins/token-expired',
+      '~/plugins/i18n',
+      '~/plugins/fcPlugin',
     ],
-    extractCSS: {
-      allChunks: true,
+    serverMiddleware: [
+      '~/serverMiddleware/body-parser.js',
+    ],
+    router: {
+      extendRoutes(routes) {
+        getRoutes(`${__dirname}/_theme`)
+          .forEach((route) => routes.unshift(route));
+      },
     },
     extractCSS: {
       allChunks: true,
@@ -245,11 +310,29 @@ export default {
       getRoutes(`${__dirname}/_theme`)
         .forEach((route) => routes.unshift(route));
     },
-  },
-  image: {
-    provider: config.get('imageProvider'),
-    cloudinary: {
+  };
+
+  if (config.get('imageProvider') === 'cloudinary') {
+    baseConfig.image.cloudinary = {
       baseURL: config.get('imageProviderBaseUrl'),
-    },
-  },
+    };
+  }
+
+  if (config.get('recaptchaEnabled')) {
+    baseConfig.modules.push('@nuxtjs/recaptcha');
+
+    baseConfig.recaptcha = {
+      hideBadge: config.get('recaptchaHideBadge'), // Hide badge element (v3 & v2 via size=invisible)
+      siteKey: config.get('recaptchaSiteKey'), // Site key for requests
+      version: config.get('recaptchaVersion'), // Version 2 or 3
+      size: config.get('recaptchaSize'), // Size: 'compact', 'normal', 'invisible' (v2)
+    };
+
+    baseConfig.publicRuntimeConfig = {
+      ...baseConfig.publicRuntimeConfig,
+      isRecaptcha: config.get('recaptchaEnabled'),
+    };
+  }
+
+  return baseConfig;
 };
