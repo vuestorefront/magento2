eb9b6974c84c7eda282e78d856b5356eb133c80d
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2022-02-03 04:36:36 -0300

    chore: updated dependencies & configuration management (#538)
    * docs: updated docs to match missing elements
    
    * chore: added missing Redis password env
    
    * refactor(theme): added missing configuration envs
    
    Added missing configuration envs for Redis and Sentry
    
    * chore(eslint): updated dependencies
    
    - updated `eslint-plugin-unicorn` to `40.1.0`
    
    * chore(eslint-jest): updated dependencies
    
    - updated `eslint-plugin-jest` to `26.0.0`
    
    * chore(eslint-typescript): updated dependencies
    
    - updated `@typescript-eslint/eslint-plugin` to `5.10.1`
    - updated `@typescript-eslint/parser` to `5.10.1`
    
    * chore(eslint-vue): updated dependencies
    
    - updated `eslint-plugin-vue` to `8.4.0`
    
    * chore(api-client): updated dependencies
    
    - updated `@apollo/client` to `3.5.8`
    - updated `graphql` to `16.3.0`
    - updated `cross-fetch` to `3.1.5`
    - updated `dotenv` to `14.3.2`
    - updated `msw` to `0.36.7`
    - updated `typescript` to `4.5.5`
    
    * chore(theme): updated dependencies
    
    - updated `@sentry/tracing` to `6.17.3`
    - updated `@storefront-ui/vue` to `0.12.0`
    - updated `isomorphic-dompurify` to `0.18.0`
    - updated `@babel/core` to `7.16.12`
    - updated `cypress` to `9.3.1`
    - updated `dotenv` to `14.3.2`
    - updated `lint-staged` to `12.3.2`
    - updated `mocha` to `9.2.0`
    - updated `npm-check-updates` to `12.2.1`
    - updated `typescript` to `4.5.5`
    
    * chore: updated dependencies
    
    - updated `@babel/core` to `7.16.12`
    - updated `@babel/plugin-proposal-private-methods` to `7.16.11`
    - updated `@commitlint/cli` to `16.1.0`
    - updated `@graphql-codegen/cli` to `2.4.0`
    - updated `@types/node` to `17.0.13`
    - updated `@typescript-eslint/parser` to `5.10.1`
    - updated `eslint` to `8.8.0`
    - updated `lint-staged` to `12.3.2`
    - updated `npm-check-updates` to `12.2.1`
    - updated `rollup` to `2.66.1`
    - updated `vue-eslint-parser` to `8.2.0`
    
    * chore(eslint-typescript): updated dependencies
    
    - `@typescript-eslint/eslint-plugin` to `5.10.2`
    - `@typescript-eslint/parser` to `5.10.2`
    
    * chore(api-client): updated dependencies
    
    - `dotenv` to `15.0.0`
    - `msw` to `0.36.8`
    
    * chore(theme): rollback storefront-ui update
    
    * chore: updated dependencies
    
    - `@graphql-codegen/typescript-operations` to `2.2.3`
    - `@types/node` to `17.0.14`
    - `@typescript-eslint/parser` to `5.10.2`
    - `lint-staged` to `12.3.3`
    
    * chore: moved sentry package to Dockerfile
    
    * chore(theme): added dynamic nuxt.config.js generation
    
    * chore: removed REDIS__PASSWORD
    
    * chore: removed REDIS__PASSWORD from workflow

--- .vuestorefrontcloud/docker/nuxt.config.additional.js
+++ .vuestorefrontcloud/docker/nuxt.config.additional.js
@@ -1,14 +1,17 @@
 import config from '@vue-storefront/magento-theme/config';
 import baseNuxtConfig from '@vue-storefront/magento-theme/base.nuxt.config';
 
-export default {
-  ...baseNuxtConfig,
-  modules: [
-    ...baseNuxtConfig.modules,
-    '@nuxtjs/sentry'
-  ],
-  sentry: {
-    dsn: process.env.SENTRY_DSN,
-    tracing: true,
-  },
-};
+export default () => {
+  const baseDefaults = baseNuxtConfig();
+
+  return {
+    ...baseDefaults,
+    modules: [
+      ...baseDefaults.modules,
+    ],
+    sentry: {
+      dsn: process.env.SENTRY_DSN,
+      tracing: true,
+    },
+  };
+}
