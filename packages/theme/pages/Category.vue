4719fe1d24f74427b3d329bd0427a81b9d4b383e
Bartosz Herba <bartoszherba@gmail.com>
2022-02-03 07:45:37 +0100

    fix(theme): fix add to wishlist button on catgory listing view for a mobile devices (#554)
    Co-authored-by: Bartosz Herba <bherba@vuestorefront.io>

--- packages/theme/pages/Category.vue
+++ packages/theme/pages/Category.vue
@@ -253,8 +253,7 @@
               </template>
               <template #actions>
                 <SfButton
-                  class="sf-button--text desktop-only"
-                  style="margin: 0 0 1rem auto; display: block"
+                  class="sf-button--text products__product-card-horizontal__add-to-wishlist"
                   @click="addItemToWishlist(product)"
                 >
                   {{ isInWishlist({product}) ? $t('Remove from Wishlist') : $t('Save for later') }}
@@ -438,7 +437,6 @@ import { useUiHelpers, useUiState, useImage } from '~/composables';
 import cacheControl from '~/helpers/cacheControl';
 import { useAddToCart } from '~/helpers/cart/addToCart';
 import CategorySidebarMenu from '~/components/Category/CategorySidebarMenu';
-
 
 // TODO(addToCart qty, horizontal): https://github.com/vuestorefront/storefront-ui/issues/1606
 export default defineComponent({
@@ -647,16 +645,12 @@ export default defineComponent({
 
       const tags = [{ prefix: CacheTagPrefix.View, value: 'category' }];
       // eslint-disable-next-line no-underscore-dangle
-      const productTags = products.value.map((product) => {
-        return { prefix: CacheTagPrefix.Product, value: product.uid };
-      });
+      const productTags = products.value.map((product) => ({ prefix: CacheTagPrefix.Product, value: product.uid }));
 
-      const categoriesTags = categoryTree.value.items.map((category) => {
-        return { prefix: CacheTagPrefix.Category, value: category.slug };
-      });
+      const categoriesTags = categoryTree.value.items.map((category) => ({ prefix: CacheTagPrefix.Category, value: category.slug }));
 
       // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      addTags(tags.concat(productTags, categoriesTags));
+      addTags([...tags, ...productTags, ...categoriesTags]);
     });
 
     const { getMagentoImage, imageSizes } = useImage();
