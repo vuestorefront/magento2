160acda8e3dd926a47d5caf4408426223fad1fb4
Marcin Kwiatkowski <marcin.kwiatkowski@vuestorefront.io>
2022-02-10 16:27:09 +0100

    refactor: disabled composables for test

--- packages/theme/composables/useUrlResolver.ts
+++ packages/theme/composables/useUrlResolver.ts
@@ -1,26 +1,38 @@
-import {
-  useUrlResolver as urlResolver,
-} from '@vue-storefront/magento';
-import { useRoute, useContext } from '@nuxtjs/composition-api';
+// import {
+//   useUrlResolver as urlResolver,
+// } from '@vue-storefront/magento';
+import { useRoute, useContext, ref } from '@nuxtjs/composition-api';
+import {Logger} from '@vue-storefront/core';
 
 export const useUrlResolver = () => {
   const route = useRoute();
-  const { error } = useContext();
+  const { error, app } = useContext();
   const { path } = route.value;
 
-  const {
-    search,
-    result,
-    loading,
-  } = urlResolver(`router:${path}`);
+  // const {
+  //   search,
+  //   result,
+  //   loading,
+  // } = urlResolver(`router:${path}`);
+
+  const result = ref(null);
 
   return {
     path,
     search: async () => {
-      await search({ url: path });
+      // await search({ url: path });
+      // console.log
+      // if (!result?.value) error({ statusCode: 404 });
+
+      const { data } = await app.context.$vsf.$magento.api.urlResolver({ url: path });
+
+      Logger.debug('[Result]:', { data });
+
+      result.value = data.urlResolver;
+
       if (!result?.value) error({ statusCode: 404 });
     },
     result,
-    loading,
+    loading: false,
   };
 };
