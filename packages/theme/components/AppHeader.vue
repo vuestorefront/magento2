506fd5b8705d560cd78ac8075a8fc260f8af6076
Bartosz Herba <bherba@vuestorefront.io>
2022-02-15 09:50:03 +0100

    refactor(general): refactor how and where the cart is loaded - add loadTotalQuantity method on the cart composable as sometimes this is the only information needed - add lazy load for a minicart - header will pull only total quantity data instead of loading whole cart

--- packages/theme/components/AppHeader.vue
+++ packages/theme/components/AppHeader.vue
@@ -203,7 +203,7 @@ export default defineComponent({
 
     return {
       accountIcon,
-      cartTotalItems,
+      cartTotalItems: totalQuantity,
       categoryTree,
       getAgnosticCatLink,
       handleAccountClick,
