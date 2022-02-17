b64566acf792179e0ab5348f00345715e1c74c08
Bartosz Herba <bherba@vuestorefront.io>
2022-02-17 09:39:57 +0100

    feat(theme): add LoadWhenVisible component and lazyload footer
    - update social icons to reduce weight and increase quality
    - load footer only when visible

---
+++ packages/theme/components/utils/LoadWhenVisible.vue
@@ -0,0 +1,53 @@
+<template>
+  <div ref="root">
+    <slot v-if="isVisible" />
+  </div>
+</template>
+<script>
+import {
+  defineComponent, onMounted, onUnmounted, ref,
+} from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  name: 'LoadWhenVisible',
+  props: {
+    options: {
+      type: Object,
+      default: () => ({
+        rootMargin: '0px 0px 200px 0px',
+      }),
+    },
+
+  },
+  setup({ options }) {
+    const isVisible = ref(false);
+    const root = ref(null);
+
+    let observer = null;
+    if (process.browser && ('IntersectionObserver' in window)) {
+      observer = new IntersectionObserver((entries) => {
+        if (entries[0].intersectionRatio <= 0) return;
+        observer.unobserve(root.value);
+        isVisible.value = true;
+      }, options);
+    }
+
+    onMounted(() => {
+      if (observer) {
+        observer.observe(root.value);
+      }
+    });
+
+    onUnmounted(() => {
+      if (observer) {
+        observer.unobserve(root.value);
+      }
+    });
+
+    return {
+      root,
+      isVisible,
+    };
+  },
+});
+</script>
