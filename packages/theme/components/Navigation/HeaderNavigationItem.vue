854ad76dd9cddf4c5896578edf901bc7ee8d48e0
Bartosz Herba <bherba@vuestorefront.io>
2022-02-17 17:33:07 +0100

    perf(theme): appHeader navigation improvement - add @nuxtjs/device package to recognize used device - remove mobile observer from header and replave it with device recognition tool
    - navigation is now lazy loaded, categories required for a navigation are not loaded at all on
    mobile devices

---
+++ packages/theme/components/Navigation/HeaderNavigationItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="sf-header-navigation-item">
+    <div class="sf-header-navigation-item__item sf-header-navigation-item__item--desktop">
+      <slot name="desktop-navigation-item">
+        <SfLink
+          class="sf-header-navigation-item__link"
+          :link="link"
+        >
+          {{
+            label
+          }}
+        </SfLink>
+      </slot>
+      <slot />
+    </div>
+  </div>
+</template>
+<script>
+import { SfLink } from '@storefront-ui/vue';
+
+export default {
+  name: 'HeaderNavigationItem',
+  components: {
+    SfLink,
+  },
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    link: {
+      type: [String, Object],
+      default: '',
+    },
+  },
+};
+</script>
