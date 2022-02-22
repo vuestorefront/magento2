0e9026379358a719d5cc4fcff4f8ca254889ffc1
Bartosz Herba <bherba@vuestorefront.io>
2022-02-22 16:20:37 +0100

    fix(api): fix cart_id graphql error

--- packages/api-client/src/api/cartTotalQty/cartTotalQty.ts
+++ packages/api-client/src/api/cartTotalQty/cartTotalQty.ts
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
 export default gql`
-  query cart($cartId: String!) {
+  query cartTotalQty($cartId: String!) {
     cart(cart_id:$cartId) {
       total_quantity
     }
