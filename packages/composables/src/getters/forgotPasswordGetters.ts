cae48a7caa5498c6a6627f9e3fea910517d10b49
Bartosz Herba <bartoszherba@gmail.com>
2022-03-16 10:53:23 +0100

    fix(composables): revert getters (#739)
    - copy getters back to the legacy composables for a backward compatibility package and mark all as deprecated
    
    Co-authored-by: Marcin Kwiatkowski <marcin.kwiatkowski@vuestorefront.io>

---
+++ packages/composables/src/getters/forgotPasswordGetters.ts
@@ -0,0 +1,18 @@
+/**
+ * @deprecated since version 1.0.0
+ */
+import { ForgotPasswordGetters } from '@vue-storefront/core';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function getResetPasswordToken(result: any): string {
+  return '';
+}
+
+const isPasswordChanged = (result: any): boolean => result?.resetPassword;
+
+export const forgotPasswordGetters: ForgotPasswordGetters<any> = {
+  getResetPasswordToken,
+  isPasswordChanged,
+};
+
+export default forgotPasswordGetters;
