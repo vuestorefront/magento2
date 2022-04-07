a7934761a5c8d2502b0786cd16f04b15dbf92ac8
Roz <roz@rjmunhoz.me>
2022-04-07 17:39:59 -0300

    refactor: removeItemFromCart JSDoc and notation
    - add JSDoc comment
    - refactor from arrow function to function notation

--- packages/api-client/src/api/removeItemFromCart/removeItemFromCart.ts
+++ packages/api-client/src/api/removeItemFromCart/removeItemFromCart.ts
@@ -1,5 +1,6 @@
 import gql from 'graphql-tag';
 
+/** GraphQL Mutation to remove an item from a cart */
 export default gql`
   mutation removeItemFromCart($input: RemoveItemFromCartInput) {
     removeItemFromCart(input: $input) {
