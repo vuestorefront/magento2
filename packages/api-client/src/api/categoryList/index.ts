e04824463925d60b3c1b6d046c79752550ea3c14
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-04-07 18:30:01 -0300

    chore: transform categoryList API method into function declaration

--- packages/api-client/src/api/categoryList/index.ts
+++ packages/api-client/src/api/categoryList/index.ts
@@ -1,25 +1,23 @@
 import { ApolloQueryResult } from '@apollo/client/core';
 import { CustomQuery } from '@vue-storefront/core';
 import { CategoryListQuery, CategoryListQueryVariables } from '../../types/GraphQL';
-import categoryList from './categoryList';
+import categoryListQuery from './categoryList';
 import { Context } from '../../types/context';
 
-export default async (
+export default async function categoryList(
   context: Context,
   params: CategoryListQueryVariables,
   customQuery: CustomQuery = { categoryList: 'categoryList' },
-): Promise<ApolloQueryResult<CategoryListQuery>> => {
-  const { categoryList: categoryListGQL } = context.extendQuery(
-    customQuery,
-    {
-      categoryList: {
-        query: categoryList,
-        variables: { ...params },
-      },
+): Promise<ApolloQueryResult<CategoryListQuery>> {
+  const { categoryList: categoryListGQL } = context.extendQuery(customQuery, {
+    categoryList: {
+      query: categoryListQuery,
+      variables: { ...params },
     },
-  );
+  });
+
   return context.client.query<CategoryListQuery, CategoryListQueryVariables>({
     query: categoryListGQL.query,
     variables: categoryListGQL.variables,
   });
-};
+}
