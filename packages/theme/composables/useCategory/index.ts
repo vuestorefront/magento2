5e4460d4c2f60a1cf7cc9b8d3d655f041e4621bf
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-04-07 23:13:59 -0300

    docs: add TSDocs to useCategory and its types

--- packages/theme/composables/useCategory/index.ts
+++ packages/theme/composables/useCategory/index.ts
@@ -7,6 +7,63 @@ import type {
   UseCategoryParamsInput,
 } from '~/composables/useCategory/useCategory';
 
+/**
+ * @public
+ *
+ * The `useCategory` composable allows searching categories from Magento API. It
+ * provides the search function and the refs for categories, loading and error.
+ *
+ * @remarks
+ *
+ * Under the hood, it calls the following Server Middleware API method:
+ *
+ * - {@link @vue-storefront/magento-api#categoryList} for loading category list;
+ *
+ * It is currently used in:
+ *
+ * - `components/AppHeader.vue`
+ *
+ * - `components/MobileMenuSidebar.vue`
+ *
+ * @example
+ *
+ * Search categories on client side using the `onMounted` Composition API hook:
+ *
+ * ```vue
+ * <template>
+ *   <div v-if="loading">
+ *     Loading categories…
+ *   </div>
+ *   <div v-else-if="error.search">
+ *     Error: {{ error.search.message }}
+ *   </div>
+ *   <div v-else>
+ *     <!-- Display 'categories' -->
+ *   </div>
+ * </template>
+ *
+ * <script>
+ * import { onMounted } from '@nuxtjs/composition-api';
+ * import { useCategory } from '~/composables';
+ *
+ * export default {
+ *   setup() {
+ *     const { categories, error, loading, search } = useCategory();
+ *
+ *     onMounted(async () => {
+ *       await search({ pageSize: 10 });
+ *     });
+ *
+ *     return {
+ *       error,
+ *       loading,
+ *       categories,
+ *     };
+ *   },
+ * };
+ * </script>
+ * ```
+ */
 export function useCategory(): UseCategoryInterface {
   const { app } = useContext();
   const loading: Ref<boolean> = ref(false);
--- packages/theme/composables/useCategory/useCategory.ts
+++ packages/theme/composables/useCategory/useCategory.ts
@@ -1,17 +1,90 @@
-import { Ref } from '@nuxtjs/composition-api';
-import { ComposableFunctionArgs } from '~/composables/types';
+import type { Ref } from '@nuxtjs/composition-api';
+import type { ComposableFunctionArgs } from '~/composables/types';
 
+/**
+ * @public
+ *
+ * The {@link useCategory} error object. Its properties values' are the errors
+ * thrown by composable methods.
+ *
+ * @example
+ *
+ * Use `error` to check if searching fails:
+ *
+ * ```typescript
+ * import { useFetch } from '@nuxtjs/composition-api';
+ * import { useCategory } from '~/composables';
+ *
+ * export default {
+ *   setup() {
+ *     const { categories, error, loading, search } = useCategory();
+ *
+ *     useFetch(async () => {
+ *       await search({ pageSize: 10 });
+ *
+ *       // It's an error when searching fails
+ *       if (error.value.search) {
+ *         // Handle search errors
+ *       }
+ *     });
+ *
+ *     return { categories, loading };
+ *   },
+ * };
+ * ```
+ */
 export interface UseCategoryErrors {
-  search: Error;
+  /** Error when searching categories fails, otherwise is `null`. */
+  search: Error | null;
 }
 
+/**
+ * @public
+ *
+ * The {@link useCategory} params object received by `search` function.
+ */
 export interface UseCategoryParamsInput {
   pageSize: number;
 }
 
+/**
+ * @public
+ *
+ * The {@link useCategory} interface with the refs and the search function.
+ */
 export interface UseCategoryInterface {
-  categories: any;
-  search: (searchParams: ComposableFunctionArgs<UseCategoryParamsInput>) => Promise<void>;
-  loading: Ref<boolean>;
+  /** The searched category list/tree. */
+  categories: Ref<any>;
+
+  /** The error object */
   error: Ref<UseCategoryErrors>;
+
+  /**
+   * The loading state.
+   *
+   * It's `true` when searching and `false` otherwise.
+   */
+  loading: Ref<boolean>;
+
+  /**
+   * Searches for the category list.
+   *
+   * @example
+   * ```typescript
+   * import { useFetch } from '@nuxtjs/composition-api';
+   * import { useCategory } from '~/composables';
+   *
+   * export default {
+   *   setup() {
+   *     const { categories, error, loading, search } = useCategory();
+   *
+   *     useFetch(async () => {
+   *       await search({ pageSize: 10 });
+   *     });
+   *
+   *     return { categories, loading };
+   *   },
+   * };
+   */
+  search: (searchParams: ComposableFunctionArgs<UseCategoryParamsInput>) => Promise<void>;
 }
