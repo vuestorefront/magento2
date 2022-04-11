e1a8c945f06eea1270afccf3f2c8202efb587072
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-04-11 16:36:35 -0300

    docs: add TSDocs to UseBillingError

--- packages/theme/composables/useBilling/useBilling.ts
+++ packages/theme/composables/useBilling/useBilling.ts
@@ -2,8 +2,39 @@ import { Ref } from '@nuxtjs/composition-api';
 import { BillingCartAddress } from '~/modules/GraphQL/types';
 import { ComposableFunctionArgs } from '~/composables/types';
 
+/**
+ * The {@link useBilling} error object. The properties values' are the errors
+ * thrown by its methods.
+ *
+ * @example
+ *
+ * Check if saving a new billing address was failed:
+ *
+ * ```typescript
+ * import { useBilling, UseBillingSaveParams } from '~/composables';
+ *
+ * export default {
+ *   setup() {
+ *     const { error, save } = useBilling();
+ *
+ *     const onSave = async (params: UseBillingSaveParams) => {
+ *       const newBilling = await save(params);
+ *
+ *       if (error.value.save) {
+ *         // Handle save error
+ *       }
+ *     };
+ *
+ *     return { onSave };
+ *   },
+ * };
+ * ```
+ */
 export interface UseBillingError {
+  /** Error when loading billing address fails, otherwise is `null`. */
   load: Error | null;
+
+  /** Error when saving new billing address fails, otherwise is `null`. */
   save: Error | null;
 }
 
