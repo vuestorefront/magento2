f8468bed70f53f0c75eb405a0aaf6be2ac878318
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-04-11 17:51:43 -0300

    docs: add TSDocs for UseBillingInterface and its methods' params

--- packages/theme/composables/useBilling/useBilling.ts
+++ packages/theme/composables/useBilling/useBilling.ts
@@ -39,15 +39,89 @@ export interface UseBillingError {
   save: Error | null;
 }
 
+/** The params required by {@link useBilling}'s `load` method. */
 export type UseBillingLoadParams = ComposableFunctionArgs<{}>;
 
+/** The params required by {@link useBilling}'s `save` method. */
 export interface UseBillingSaveParams {
   billingDetails: BillingDetails;
 }
 
+/** The object with refs and methods returned by {@link useBilling} composable. */
 export interface UseBillingInterface {
-  loading: Ref<boolean>;
+  /** The error object */
   error: Ref<UseBillingError>;
+
+  /**
+   * The loading state.
+   *
+   * It's `true` when loading or saving, and `false` otherwise.
+   */
+  loading: Ref<boolean>;
+
+  /**
+   * A method that loads the billing address.
+   *
+   * @example
+   *
+   * Load the billing address on server side using the `useFetch` composable:
+   *
+   * ```typescript
+   * import { ref, useFetch } from '@nuxtjs/composition-api';
+   * import { useBilling } from '~/composables';
+   * import type { BillingCartAddress } from '~/modules/GraphQL/types';
+   *
+   * export default {
+   *   setup() {
+   *     const { error, load, loading } = useBilling();
+   *
+   *     const billing = ref<BillingCartAddress | null>(null);
+   *
+   *     useFetch(async () => {
+   *       billing.value = await load();
+   *
+   *       if (error.value.load) {
+   *         // Handle load errors
+   *       }
+   *     });
+   *
+   *     return { billing, loading };
+   *   },
+   * };
+   * ```
+   */
   load: (params?: UseBillingLoadParams) => Promise<BillingCartAddress | null>;
+
+  /**
+   * A method that saves a new billing address.
+   *
+   * @example
+   *
+   * Check if saving a new billing address was failed:
+   *
+   * ```typescript
+   * import { ref } from '@nuxtjs/composition-api';
+   * import { useBilling, UseBillingSaveParams } from '~/composables';
+   * import type { BillingCartAddress } from '~/modules/GraphQL/types';
+   *
+   * export default {
+   *   setup() {
+   *     const { error, save, loading: saving } = useBilling();
+   *
+   *     const newBilling = ref<BillingCartAddress | null>(null);
+   *
+   *     const onSave = async (params: UseBillingSaveParams) => {
+   *       newBilling.value = await save(params);
+   *
+   *       if (error.value.save) {
+   *         // Handle save error
+   *       }
+   *     };
+   *
+   *     return { onSave, saving, newBilling };
+   *   },
+   * };
+   * ```
+   */
   save: (params: UseBillingSaveParams) => Promise<BillingCartAddress | null>;
 }
