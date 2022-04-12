053ebc43d7c718feb684dad247fca4a92a58e3fd
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-04-12 15:46:56 -0300

    chore: transform categorySearch to function declaration

--- packages/api-client/src/api/categorySearch/index.ts
+++ packages/api-client/src/api/categorySearch/index.ts
@@ -1,26 +1,23 @@
 import { ApolloQueryResult } from '@apollo/client/core';
 import { CustomQuery } from '@vue-storefront/core';
 import { CategorySearchQuery, CategorySearchQueryVariables } from '../../types/GraphQL';
-import categorySearch from './categorySearch';
+import categorySearchQuery from './categorySearch';
 import { Context } from '../../types/context';
 
-export default async (
+export default async function categorySearch(
   context: Context,
   filters: CategorySearchQueryVariables,
   customQuery: CustomQuery = { categorySearch: 'categorySearch' },
-): Promise<ApolloQueryResult<CategorySearchQuery>> => {
-  const { categorySearch: categorySearchGQL } = context.extendQuery(
-    customQuery,
-    {
-      categorySearch: {
-        query: categorySearch,
-        variables: { ...filters },
-      },
+): Promise<ApolloQueryResult<CategorySearchQuery>> {
+  const { categorySearch: categorySearchGQL } = context.extendQuery(customQuery, {
+    categorySearch: {
+      query: categorySearchQuery,
+      variables: { ...filters },
     },
-  );
+  });
 
   return context.client.query<CategorySearchQuery, CategorySearchQueryVariables>({
     query: categorySearchGQL.query,
     variables: categorySearchGQL.variables,
   });
-};
+}
