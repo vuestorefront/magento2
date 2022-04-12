341d3177acef6e89db067565f1203720b4263921
Roz <roz@rjmunhoz.me>
2022-04-12 11:32:22 -0300

    docs: simplify `useCart` JSDoc

--- packages/theme/composables/useCart/useCart.ts
+++ packages/theme/composables/useCart/useCart.ts
@@ -4,128 +4,21 @@ import { ComposableFunctionArgs } from '~/composables/types';
 import { CartItemInput } from '~/modules/GraphQL/types';
 
 /**
- * `UseCartInterface` is used by userCart composable, and it's responsible for manipulating cart data in Magento.
+ * Represents data and methods returned from the {@link useCart} composable
  */
 export interface UseCartInterface<CART, CART_ITEM, PRODUCT> {
-  /**
-   * Loads the current cart
-   *
-   * Returns a `Promise` that resolves when cart information is fetched
-   *
-   * @example
-   * Load cart information on the server side using the `onSsr` hook:
-   *
-   * ```typescript
-   * import { useCart } from '@vue-storefront/magento';
-   * import { onSsr } from '@vue-storefront/core';
-   * export default {
-   *   setup () {
-   *     const { load, cart } = useCart()
-   *
-   *     onSsr(async () => {
-   *       await load()
-   *     })
-   *
-   *     return {
-   *      cart
-   *     }
-   *   }
-   * }
-   * ```
-   */
+  /** Loads the current cart */
   load: (params: ComposableFunctionArgs<{ realCart?: boolean }>) => Promise<void>;
-  /**
-   * Updates the global application state with the current total quantity of the cart
-   *
-   * @example
-   *
-   * Load cart total on the server side using the `onSsr` hook:
-   *
-   * ```typescript
-   * import { useCart } from '@vue-storefront/magento';
-   * import { onSsr } from '@vue-storefront/core';
-   * export default {
-   *   setup () {
-   *     const { loadTotalQty, cart } = useCart()
-   *
-   *     onSsr(async () => {
-   *       await loadTotalQty()
-   *     })
-   *
-   *     return {
-   *      cart
-   *     }
-   *   }
-   * }
-   * ```
-   */
+  /** Updates the global application state with the current total quantity of the cart */
   loadTotalQty: (context: Context['app']) => Promise<void>;
-  /**
-   * Takes in a `product` and its `quantity` and adds it to the cart
-   *
-   * @example
-   *
-   * Adding a product to cart and handling possible errors:
-   *
-   * ```typescript
-   * import { useCart } from '~/composables';
-   *
-   * export default {
-   *   setup() {
-   *     const { addItem, error } = useCart();
-   *
-   *     const addItemToCart = async (
-   *       product: ProductInterface,
-   *       quantity: nubmer
-   *     ) => {
-   *       await addItem({ product, quantity });
-   *
-   *       if (error.value.addItem) {
-   *         // handle adding item to cart error
-   *       }
-   *     }
-   *
-   *     return { addItemToCart };
-   *   }
-   * }
-   * ```
-   */
+  /** Takes in a `product` and its `quantity` and adds it to the cart */
   addItem: (
     params: ComposableFunctionArgs<{
       product: PRODUCT;
       quantity: any;
     }>
   ) => Promise<void>;
-  /**
-   * Removes an `item` from a `cart`
-   *
-   * @example
-   *
-   * Removing an item from cart and handling possible errors:
-   *
-   * ```typescript
-   * import { useCart } from '~/composables';
-   *
-   * export default {
-   *   setup() {
-   *     const { removeItem, error } = useCart();
-   *
-   *     const removeItemFromCart = async (
-   *       product: ProductInterface,
-   *       quantity: nubmer
-   *     ) => {
-   *       await removeItem({ product, quantity });
-   *
-   *       if (error.value.removeItem) {
-   *         // handle adding item to cart error
-   *       }
-   *     }
-   *
-   *     return { removeItemToCart };
-   *   }
-   * }
-   * ```
-   */
+  /** Removes an `item` from a `cart` */
   removeItem: (params: ComposableFunctionArgs<{ currentCart: CART; product: CART_ITEM }>) => Promise<void>;
   /** Updates the `quantity` of an `item` in a cart */
   updateItemQty: (params: ComposableFunctionArgs<{ product: CART_ITEM; quantity: number }>) => Promise<void>;
@@ -141,41 +34,17 @@ export interface UseCartInterface<CART, CART_ITEM, PRODUCT> {
   setCart: (newCart: CART) => void;
   /** Returns the Items in the Cart as a `computed` property */
   cart: ComputedRef<CART>;
-  /** The loading state. It's `true` when loading, saving, updating, or removing customer addresses and `false` otherwise. */
+  /**
+   * The loading state.
+   *
+   * This is `true` when loading, saving, updating, or removing customer addresses and `false` otherwise.
+   */
   loading: Ref<boolean>;
   /** The error object */
   error: Ref<UseCartErrors>;
 }
 
-/**
- * The {@link useCart} error object. The properties values are the errors thrown by its methods
- *
- * @example
- * Check if adding an item to a cart failed:
- *
- * ```typescript
- * import { useCart } from '~/composables';
- *
- * export default {
- *   setup() {
- *     const { addItem, error } = useCart();
- *
- *     const addItemToCart = async (
- *       product: ProductInterface,
- *       quantity: nubmer
- *     ) => {
- *       await addItem({ product, quantity });
- *
- *       if (error.value.addItem) {
- *         // handle adding item to cart error
- *       }
- *     }
- *
- *     return { addItemToCart };
- *   }
- * }
- * ```
- */
+/** The {@link useCart} error object. The properties values are the errors thrown by its methods */
 export interface UseCartErrors {
   /** Error when adding item to cart fails, otherwise is `null` */
   addItem: Error | null;
