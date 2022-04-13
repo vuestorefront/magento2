15ae32fda1d5f8714034f84f4bd5e215c4a11466
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-04-13 16:56:08 -0300

    feat: add useLoading to useAddresses

--- packages/theme/composables/useAddresses/index.ts
+++ packages/theme/composables/useAddresses/index.ts
@@ -1,6 +1,7 @@
 import { Ref, ref, useContext } from '@nuxtjs/composition-api';
 import { Logger } from '~/helpers/logger';
 import { transformUserCreateAddressInput, transformUserUpdateAddressInput } from '~/helpers/userAddressManipulator';
+import useLoading from '~/composables/useLoading';
 import type { ComposableFunctionArgs } from '~/composables/types';
 import type { UseAddressesInterface, UseAddressesParamsInput, UseAddressesErrors } from './useAddresses';
 
@@ -91,16 +92,15 @@ export function useAddresses(): UseAddressesInterface {
     remove: null,
     update: null,
   });
-  const loading: Ref<boolean> = ref(false);
+  const { loading, withLoading } = useLoading();
   const { app } = useContext();
   const context = app.$vsf;
 
-  const load = async () => {
+  const load = withLoading(async () => {
     Logger.debug('[Magento] load user addresses');
     let results = null;
 
     try {
-      loading.value = true;
       const { data } = await context.$magento.api.getCustomerAddresses();
       results = data?.customer?.addresses ?? [];
       Logger.debug('[Magento] load user addresses results:', results);
@@ -108,19 +108,16 @@ export function useAddresses(): UseAddressesInterface {
     } catch (err) {
       error.value.load = err;
       Logger.error('Magento] load user addresses error', err);
-    } finally {
-      loading.value = false;
     }
 
     return results;
-  };
+  });
 
-  const save = async (saveParams: ComposableFunctionArgs<UseAddressesParamsInput>) => {
+  const save = withLoading(async (saveParams: ComposableFunctionArgs<UseAddressesParamsInput>) => {
     Logger.debug('[Magento] save user address:', saveParams.address);
     let results = null;
 
     try {
-      loading.value = true;
       const { data } = await context.$magento.api.createCustomerAddress(transformUserCreateAddressInput(saveParams));
       results = data?.createCustomerAddress ?? {};
       Logger.debug('[Magento] save user address results:', saveParams.address);
@@ -128,19 +125,16 @@ export function useAddresses(): UseAddressesInterface {
     } catch (err) {
       error.value.save = err;
       Logger.error('[Magento] save user address error:', err);
-    } finally {
-      loading.value = false;
     }
 
     return results;
-  };
+  });
 
-  const update = async (updateParams: ComposableFunctionArgs<UseAddressesParamsInput>) => {
+  const update = withLoading(async (updateParams: ComposableFunctionArgs<UseAddressesParamsInput>) => {
     Logger.debug('[Magento] update user address:', updateParams.address);
     let results = null;
 
     try {
-      loading.value = true;
       const { data } = await context.$magento.api.updateCustomerAddress(transformUserUpdateAddressInput(updateParams));
       results = data?.updateCustomerAddress ?? {};
       Logger.debug('[Magento] update user address results:', results);
@@ -148,19 +142,16 @@ export function useAddresses(): UseAddressesInterface {
     } catch (err) {
       error.value.update = err;
       Logger.error('[Magento] update user address error:', err);
-    } finally {
-      loading.value = false;
     }
 
     return results;
-  };
+  });
 
-  const remove = async (removeParams: ComposableFunctionArgs<UseAddressesParamsInput>) => {
+  const remove = withLoading(async (removeParams: ComposableFunctionArgs<UseAddressesParamsInput>) => {
     Logger.debug('[Magento] remove user address:', removeParams.address);
     let results = null;
 
     try {
-      loading.value = true;
       const { data } = await context.$magento.api.deleteCustomerAddress(removeParams.address.id);
       results = !!data.deleteCustomerAddress;
       Logger.debug('[Magento] remove user address results:', results);
@@ -168,12 +159,10 @@ export function useAddresses(): UseAddressesInterface {
     } catch (err) {
       error.value.remove = err;
       Logger.error('[Magento] remove user address error:', err);
-    } finally {
-      loading.value = false;
     }
 
     return results;
-  };
+  });
 
   return {
     error,
