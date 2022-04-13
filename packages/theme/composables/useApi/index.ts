a6181addca1d9b36788a5881bd41acf72eae379f
Artur Tagisow <atagisow@vuestorefront.io>
2022-04-13 07:49:02 +0200

    refactor: move graphqlclient from useApi to context
    M2-354
    
    this is so you can query graphql endpoints in plugins
    
    refactor: useApi to use client from context
    
    refactor: move dynamic headers to client instance
    
    I don't want to keep the append the headers in useApi because if someone
    wants to pull the graphql clients from the context - e.g. when
    requesting something in a plugin - they'll have to reimplement the
    cookie logic again
    
    this commit is a checkpoint commit - the '' fallback in graphqlClient
    isn't ideal because the header shouldn't be appended to the request if
    it's empty. Getters are limited int this way. I'll try something with Proxy
    
    refactor: proxy workaround for dynamic headers
    
    refactor: use proxies as workaround for dynamic headers
    
    refactor: add template types to useApi
    
    refactor: remove references to customApolloHttpLinkOptions
    
    fix: fix spread
    
    refactor: code review changes

--- packages/theme/composables/useApi/index.ts
+++ packages/theme/composables/useApi/index.ts
@@ -1,54 +1,14 @@
 import { useContext } from '@nuxtjs/composition-api';
-import { request as graphQLRequest, GraphQLClient } from 'graphql-request';
+import type { Variables } from 'graphql-request';
+
+type Request = <T = any, V = Variables>(document: string, variables?: V, requestHeaders?: HeadersInit) => Promise<T>;
 
 export const useApi = () => {
-  const { app } = useContext();
-  const customerToken = app.$vsf.$magento.config.state.getCustomerToken();
-  const storeCode = app.$vsf.$magento.config.state.getStore();
-  const currency = app.$vsf.$magento.config.state.getCurrency();
-  const magentoConfig = app.$vsf.$magento.config;
-  // TODO remove once we remove apollo client
-  const { useGETForQueries } = magentoConfig.customApolloHttpLinkOptions;
-  const defaultEndpoint = magentoConfig.magentoApiEndpoint;
-  const defaultHeaders: {
-    authorization?: string,
-    store?: string
-    'Content-Currency'?: string
-  } = {};
+  const context = useContext();
+  const query : Request = (document, variables, headers) => context.$graphql.query.request(document, variables, headers);
+  const mutate : Request = (document, variables, headers) => context.$graphql.mutation.request(document, variables, headers);
 
-  if (customerToken) {
-    defaultHeaders.authorization = `Bearer ${customerToken}`;
-  }
-
-  if (storeCode) {
-    defaultHeaders.store = storeCode;
-  }
-
-  if (currency) {
-    defaultHeaders['Content-Currency'] = currency;
-  }
-
-  const query = (
-    document: string,
-    variables = null,
-    endpoint = defaultEndpoint,
-    requestHeaders = defaultHeaders,
-  ) => new GraphQLClient(endpoint, {
-    method: useGETForQueries ? 'GET' : 'POST',
-    headers: requestHeaders,
-  }).request(document, variables);
-
-  const mutate = (
-    document: string,
-    variables = null,
-    endpoint = defaultEndpoint,
-    requestHeaders = defaultHeaders,
-  ) => graphQLRequest(endpoint, document, variables, requestHeaders);
-
-  return {
-    query,
-    mutate,
-  };
+  return { query, mutate };
 };
 
 export default useApi;
