39046a0afa542d3a19c51129299c107a1bea80f6
Roz <3948961+roziscoding@users.noreply.github.com>
2022-04-14 03:08:21 -0300

    feat!: make `loading` and `error` properties readonly in the `useCart` composable (#827)
    * refactor: change useCart declaration from const to function
    
    * refactor: addConfigurableProductstoCart docs and function notation
    
    - add JSDoc
    - refactor to use function instead of arrow function
    
    * refactor: addConfigurableProducts to cart JSDoc and function notation
    
    - add JSDoc
    - refactor from arrow function to function notation
    
    * refactor: addProductsToCart JSDoc and function notation
    
    - add JSDoc comments
    - refactor from arrow function to function notation
    
    * refactor: addVirtualProductsToCart JSDoc and function notation
    
    - add JSDoc comments
    - refactor from arrow function to function notation
    
    * refactor: applyCouponToCart JSDoc and notation
    
    - add JSDoc comment
    - refactor from arrow function to function notation
    
    * refactor: cart JSDoc and notation
    
    - add JSDoc comment
    - refactor from arrow function to function notation
    
    * refactor: customerCart JSDoc and notation
    
    - add JSDoc comment
    - refactor from arrow function to function notation
    
    * refactor: removeCouponFromCart JSDoc and notation
    
    - add JSDoc comment
    - refactor from arrow function to function notation
    
    * refactor: removeItemFromCart JSDoc and notation
    
    - add JSDoc comment
    - refactor from arrow function to function notation
    
    * refactor: updateCartItems JSDoc and notation
    
    - add JSDoc comment
    - refactor from arrow function to function notatio
    
    * feat: add link to useCart documentation
    
    * docs: add documentation for useCart composable
    
    * docs: add documentation for UseCartInterface#load
    
    * docs: add documentation for useCart interfaces
    
    * feat!: make UseCartErrors properties nullable
    
    BREAKING CHANGE: all properties of the UseCartErrors interface are
    converted to a union with null. The new type is `Error | null` for all
    properties
    
    * docs: add UseCartInterface's loadTotalQty JSDoc
    
    * docs: add UseCartInterface's addItem JSDoc
    
    * docs: simplify `useCart` JSDoc
    
    * refactor: cleanup useCart types and JSDoc
    
    * docs: simplify `useCart` composable docs
    
    * docs: update useCart JSDoc
    
    * refactor: replace imports with `import type`
    
    Co-authored-by: Marcin Kwiatkowski <marcin.kwiatkowski@vuestorefront.io>
    
    * docs: add API methods to sidebar
    
    Co-authored-by: Marcin Kwiatkowski <marcin.kwiatkowski@vuestorefront.io>

--- packages/theme/composables/useCart/useCart.ts
+++ packages/theme/composables/useCart/useCart.ts
@@ -1,43 +1,114 @@
 import { Context } from '@nuxt/types';
-import { ComputedRef, Ref } from '@nuxtjs/composition-api';
+import { ComputedRef, DeepReadonly, Ref } from '@nuxtjs/composition-api';
 import { ComposableFunctionArgs } from '~/composables/types';
-import { CartItemInput } from '~/modules/GraphQL/types';
 
-export interface UseCartInterface<CART, CART_ITEM, PRODUCT> {
-  load: (params: ComposableFunctionArgs<{ realCart?: boolean; }>) => Promise<void>;
-  loadTotalQty: (context: Context['app']) => Promise<void>;
-  addItem: (
-    params: ComposableFunctionArgs<{
-      product: PRODUCT;
-      quantity: any;
-    }>
-  ) => Promise<void>;
-  removeItem: (params: ComposableFunctionArgs<{ currentCart: CART; product: CART_ITEM; }>) => Promise<void>;
-  updateItemQty: (
-    params: ComposableFunctionArgs<{ product: CART_ITEM; quantity: number; }>
-  ) => Promise<void>;
-  clear: (params: ComposableFunctionArgs<{ realCart?: boolean; }>) => Promise<void>;
-  applyCoupon: (params: ComposableFunctionArgs<{ couponCode: string; }>) => Promise<void>;
-  removeCoupon: (params: ComposableFunctionArgs<{}>) => Promise<void>;
-  isInCart: (params: { currentCart: CART; product: PRODUCT }) => boolean;
-  setCart: (newCart: CART) => void;
-  cart: ComputedRef<CART>;
-  loading: Ref<boolean>;
-  error: Ref<UseCartErrors>;
-}
+/**
+ * Parameters accepted by the `addItem` method in the {@link useCart} composable
+ */
+export type UseCartAddItemParams<PRODUCT> = ComposableFunctionArgs<{
+  product: PRODUCT;
+  quantity: any; // TODO: Update type
+}>;
 
-export interface UseCartErrors {
-  addItem: Error;
-  removeItem: Error;
-  updateItemQty: Error;
-  load: Error;
-  clear: Error;
-  applyCoupon: Error;
-  removeCoupon: Error;
-  loadTotalQty: Error;
-}
+/**
+ * Parameters accepted by the `removeItem` method in the {@link useCart} composable
+ */
+type UseCartRemoveItemParams<CART, CART_ITEM> = ComposableFunctionArgs<{
+  currentCart: CART
+  product: CART_ITEM
+}>;
 
-export declare type AddProductsToCartInput = {
-  cartId: string;
-  cartItems: CartItemInput[];
+/**
+ * Parameters accepted by the `updateItemQty` method in the {@link useCart} composable
+ */
+type UseCartUpdateItemQtyParams<CART_ITEM> = ComposableFunctionArgs<{
+  product: CART_ITEM
+  quantity: number
+}>;
+
+/**
+ * Parameters accepted by the `clear` method in the {@link useCart} composable
+ */
+type UseCartClearParams = ComposableFunctionArgs<{
+  realCart?: boolean
+}>;
+
+/**
+ * Parameters accepted by the `applyCoupon` method in the {@link useCart} composable
+ */
+type UseCartApplyCouponParams = ComposableFunctionArgs<{
+  couponCode: string
+}>;
+
+/**
+ * Parameters accepted by the `isInCart` method in the {@link useCart} composable
+ */
+type UseCartIsInCartParams<CART, PRODUCT> = {
+  currentCart: CART
+  product: PRODUCT
 };
+
+/**
+ * Represents data and methods returned from the {@link useCart} composable
+ */
+export interface UseCartInterface<CART, CART_ITEM, PRODUCT> {
+  /** Loads the current cart */
+  load(params: ComposableFunctionArgs<{ realCart?: boolean }>): Promise<void>;
+  /** Updates the global application state with the current total quantity of the cart */
+  loadTotalQty(context: Context['app']): Promise<void>;
+  /** Takes in a `product` and its `quantity` and adds it to the cart */
+  addItem(
+    params: UseCartAddItemParams<PRODUCT>
+  ): Promise<void>;
+  /** Removes an `item` from a `cart` */
+  removeItem(params: UseCartRemoveItemParams<CART, CART_ITEM>): Promise<void>;
+  /** Updates the `quantity` of an `item` in a cart */
+  updateItemQty(params: UseCartUpdateItemQtyParams<CART_ITEM>): Promise<void>;
+  /** Removes all items from the cart */
+  clear(params: UseCartClearParams): Promise<void>;
+  /** Applies a coupon to the cart */
+  applyCoupon(params: UseCartApplyCouponParams): Promise<void>;
+  /** Removes applied coupon from the cart */
+  removeCoupon(params: ComposableFunctionArgs<{}>): Promise<void>;
+  /** Checks wheter a `product` is in the `cart` */
+  isInCart(params: UseCartIsInCartParams<CART, PRODUCT>): boolean;
+  /** Sets the contents of the cart */
+  setCart(newCart: CART): void;
+  /** Returns the Items in the Cart as a `computed` property */
+  cart: ComputedRef<CART>;
+  /**
+   * The loading state.
+   *
+   * This is `true` when loading, saving, updating, or removing customer addresses and `false` otherwise.
+   */
+  loading: DeepReadonly<Ref<boolean>>;
+  /** The error object */
+  error: DeepReadonly<Ref<UseCartErrors>>;
+}
+
+/** The {@link useCart} error object. The properties values are the errors thrown by its methods */
+export interface UseCartErrors {
+  /** Error when adding item to cart fails, otherwise is `null` */
+  addItem: Error | null;
+
+  /** Error when removing item from cart fails, otherwise is `null` */
+  removeItem: Error | null;
+
+  /** Error when updating item quantity fails, otherwise is `null` */
+  updateItemQty: Error | null;
+
+  /** Error when loading fails, otherwise is `null` */
+  load: Error | null;
+
+  /** Error when clearing cart fails, otherwise is `null` */
+  clear: Error | null;
+
+  /** Error when applying coupon to cart fails, otherwise is `null` */
+  applyCoupon: Error | null;
+
+  /** Error when removing coupon to cart fails, otherwise is `null` */
+  removeCoupon: Error | null;
+
+  /** Error when loading total quantity fails, otherwise is `null` */
+  loadTotalQty: Error | null;
+}
