5bb4b7f70d08becf83dbe49883282bf337812a4c
Roz <3948961+roziscoding@users.noreply.github.com>
2022-04-14 03:54:22 -0300

    feat!: make `error` and `loading` properties readonly on `useCountrySearch` composable (#847)
    * feat: create `UseCountrySearchInterface`
    
    * feat!: readonly properties on useCountrySearch
    
    BREAKING CHANGE: `loading` and `error` properties from the
    `UseCountrySearchInterface` are now readonly
    
    * docs: add `useCountrySearch` JSDoc
    
    * docs: add country and countries API JSDoc
    
    Co-authored-by: Marcin Kwiatkowski <marcin.kwiatkowski@vuestorefront.io>

--- packages/api-client/src/api/countries/index.ts
+++ packages/api-client/src/api/countries/index.ts
@@ -1,22 +1,27 @@
 import { ApolloQueryResult } from '@apollo/client/core';
 import { CustomQuery } from '@vue-storefront/core';
 import { CountriesListQuery } from '../../types/GraphQL';
-import countriesList from './countriesList';
+import countriesListQuery from './countriesList';
 import { Context } from '../../types/context';
 
-export default async (
+/**
+ * Loads the list of countries
+ * @param context VSF Context
+ * @param [customQuery] (optional) - custom GraphQL query that extends the default one
+ */
+export default async function countries(
   context: Context,
   customQuery: CustomQuery = { countries: 'countries' },
-): Promise<ApolloQueryResult<CountriesListQuery>> => {
+): Promise<ApolloQueryResult<CountriesListQuery>> {
   const { countries: countriesGQL } = context.extendQuery(
     customQuery,
     {
       countries: {
-        query: countriesList,
+        query: countriesListQuery,
       },
     },
   );
   return context.client.query<CountriesListQuery>({
     query: countriesGQL.query,
   });
-};
+}
