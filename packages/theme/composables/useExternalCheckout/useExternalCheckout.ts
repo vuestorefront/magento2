980aa94f93472a7197dc807c53d9ab5dbbebe0c4
Roz <3948961+roziscoding@users.noreply.github.com>
2022-04-14 05:05:51 -0300

    feat!: make `loading` and `error` readonly in useExternalCheckout composable (#856)
    * docs: add useExternalCheckout JSDoc
    
    * feat!: make `loading` and `error` readonly
    
    BREAKING CHANGE: useExternalCheckout `loading` and `error` properties
    are now readonly
    
    Co-authored-by: Marcin Kwiatkowski <marcin.kwiatkowski@vuestorefront.io>

--- packages/theme/composables/useExternalCheckout/useExternalCheckout.ts
+++ packages/theme/composables/useExternalCheckout/useExternalCheckout.ts
@@ -1,8 +1,14 @@
-import { Ref } from '@nuxtjs/composition-api';
+import { DeepReadonly, Ref } from '@nuxtjs/composition-api';
 import { ComposableFunctionArgs } from '~/composables/types';
 
-export type UseExternalCheckout = {
-  initializeCheckout: (params: ComposableFunctionArgs<{ baseUrl: string }>) => string
-  loading: Ref<boolean>,
-  error: Ref<string>,
-};
+export type UseExternalCheckoutInitializeParams = ComposableFunctionArgs<{ baseUrl: string }>;
+
+/** Represents the data and methods returned by the {@link useExternalCheckout} composable */
+export interface UseExternalCheckoutInterface {
+  /** Initializes the checkout provider with the `baseUrl` */
+  initializeCheckout(params: UseExternalCheckoutInitializeParams): string
+  /** Returns the loading state as a computed boolean */
+  loading: DeepReadonly<Ref<boolean>>,
+  /** Returns errors when intializing the checkout */
+  error: DeepReadonly<Ref<string>>,
+}
