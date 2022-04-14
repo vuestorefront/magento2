65df163af4267934aa765f6e0544975da509b14d
Roz <3948961+roziscoding@users.noreply.github.com>
2022-04-14 03:38:37 -0300

    feat!: make `loading` property readonly in the `useConfig` composable (#846)
    * refactor!: rename UseConfig to UseConfigInterface
    
    * refactor: make UseConfigInterface a typescript interface
    
    * feat!: make loading `useConfig#loading` readonly
    
    * docs: add useConfig composable JSDoc
    
    * docs: add storeConfig API JSDoc
    
    Co-authored-by: Marcin Kwiatkowski <marcin.kwiatkowski@vuestorefront.io>

--- packages/theme/composables/useConfig/useConfig.ts
+++ packages/theme/composables/useConfig/useConfig.ts
@@ -1,12 +1,20 @@
-import { ComputedRef, Ref } from '@nuxtjs/composition-api';
+import { ComputedRef, DeepReadonly, Ref } from '@nuxtjs/composition-api';
 import { StoreConfig } from '~/modules/GraphQL/types';
 
+/** Errors returned by the {@link useConfig} composable */
 export interface UseConfigErrors {
+  /** Error when loading configuration fails, otherwise is `null` */
   load: Error | null;
 }
 
-export type UseConfig = {
+/**
+ * Represents the data and methods returned by the {@link useConfig} composable
+ */
+export interface UseConfigInterface {
+  /** Returns the loaded config as computed property */
   config: ComputedRef<StoreConfig>,
-  loading: Ref<boolean>,
+  /** Return state of loadConfig Function as computed property */
+  loading: DeepReadonly<Ref<boolean>>,
+  /** Function to load the config */
   load (): Promise<void>
-};
+}
