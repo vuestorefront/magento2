5800fd23096a7e6790f9c5be1a36e16a168ae8cf
Bartosz Herba <bartoszherba@gmail.com>
2022-04-15 12:33:39 +0200

    refactor: store config query (#862)
    * refactor: store config query
    - move loading of the store config to the plugin
    - docs: add store config usage docs
    - remove usage of storeGetters and storeConfigGetters
    
    M2-414
    
    * Apply suggestions from code review
    
    Co-authored-by: Artur Tagisow <5359825+sethidden@users.noreply.github.com>
    
    Co-authored-by: Artur Tagisow <5359825+sethidden@users.noreply.github.com>

--- packages/theme/composables/useMagentoConfiguration.ts
+++ packages/theme/composables/useMagentoConfiguration.ts
@@ -1,7 +1,5 @@
 import { computed, ComputedRef, useContext } from '@nuxtjs/composition-api';
 import { StoreConfig } from '~/modules/GraphQL/types';
-import { storeConfigGetters } from '~/getters';
-
 import { useConfig } from '~/composables';
 
 type UseMagentoConfiguration = () => {
@@ -9,54 +7,23 @@ type UseMagentoConfiguration = () => {
   selectedCurrency: ComputedRef<string | undefined>;
   selectedLocale: ComputedRef<string | undefined>;
   selectedStore: ComputedRef<string | undefined>;
-  loadConfiguration: (params: { updateCookies: boolean; updateLocale: boolean; }) => Promise<void>;
 };
 
 export const useMagentoConfiguration: UseMagentoConfiguration = () => {
-  const { app: { i18n, $vsf: { $magento: { config } } } } = useContext();
+  const { app: { $vsf: { $magento: { config } } } } = useContext();
 
   const {
     config: storeConfig,
-    load: loadConfig,
   } = useConfig();
 
   const selectedCurrency = computed<string | undefined>(() => config.state.getCurrency());
   const selectedLocale = computed<string | undefined>(() => config.state.getLocale());
   const selectedStore = computed<string | undefined>(() => config.state.getStore());
 
-  const loadConfiguration: (params: { updateCookies: boolean; updateLocale: boolean; }) => Promise<void> = async (params = {
-    updateCookies: false,
-    updateLocale: false,
-  }) => {
-    const {
-      updateCookies,
-      updateLocale,
-    } = params;
-
-    await loadConfig();
-    if (config.state.getStore() || updateCookies) {
-      config.state.setStore(storeConfigGetters.getCode(storeConfig.value));
-    }
-
-    if (!config.state.getLocale() || updateCookies) {
-      config.state.setLocale(storeConfigGetters.getLocale(storeConfig.value));
-    }
-
-    if (!config.state.getCurrency() || updateCookies) {
-      config.state.setCurrency(storeConfigGetters.getCurrency(storeConfig.value));
-    }
-
-    // eslint-disable-next-line promise/always-return
-    if (updateLocale) {
-      i18n.setLocale(storeConfigGetters.getLocale(storeConfig.value));
-    }
-  };
-
   return {
     storeConfig,
     selectedCurrency,
     selectedLocale,
     selectedStore,
-    loadConfiguration,
   };
 };
