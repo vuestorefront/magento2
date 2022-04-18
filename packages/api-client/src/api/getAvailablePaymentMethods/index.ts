b8bd6ce224d8785971bcc404ff3d2db1855211df
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-04-18 16:20:53 -0300

    chore: convert getAvailablePaymentMethods to function declaration

--- packages/api-client/src/api/getAvailablePaymentMethods/index.ts
+++ packages/api-client/src/api/getAvailablePaymentMethods/index.ts
@@ -2,10 +2,7 @@ import { ApolloQueryResult } from '@apollo/client/core';
 import { CustomQuery } from '@vue-storefront/core';
 import { Context } from '../../types/context';
 import GuestAvailablePaymentMethods from './GuestAvailablePaymentMethods';
-import {
-  GuestAvailablePaymentMethodsQuery,
-  GuestAvailablePaymentMethodsQueryVariables,
-} from '../../types/GraphQL';
+import { GuestAvailablePaymentMethodsQuery, GuestAvailablePaymentMethodsQueryVariables } from '../../types/GraphQL';
 
 /**
  * Fetches the available payment methods for the received cart.
@@ -14,26 +11,23 @@ import {
  * @param params params containing the cart's ID
  * @param [customQuery] (optional) - custom GraphQL query that extends the default query
  */
-export default async (
+export default async function getAvailablePaymentMethods(
   context: Context,
   params: {
     cartId: string;
   },
   customQuery: CustomQuery = { paymentMethods: 'paymentMethods' },
-): Promise<ApolloQueryResult<GuestAvailablePaymentMethodsQuery>> => {
+): Promise<ApolloQueryResult<GuestAvailablePaymentMethodsQuery>> {
   const defaultVariables = {
     cartId: params.cartId || '',
   };
 
-  const { paymentMethods } = context.extendQuery(
-    customQuery,
-    {
-      paymentMethods: {
-        query: GuestAvailablePaymentMethods,
-        variables: defaultVariables,
-      },
+  const { paymentMethods } = context.extendQuery(customQuery, {
+    paymentMethods: {
+      query: GuestAvailablePaymentMethods,
+      variables: defaultVariables,
     },
-  );
+  });
 
   try {
     return await context.client.query<GuestAvailablePaymentMethodsQuery, GuestAvailablePaymentMethodsQueryVariables>({
@@ -43,4 +37,4 @@ export default async (
   } catch (error) {
     throw error.graphQLErrors?.[0].message || error.networkError?.result || error;
   }
-};
+}
