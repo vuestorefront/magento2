1d19dc8e296bd84e0c786e4b0fb7f97240fa5ba0
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-04-18 04:58:54 -0300

    chore: convert subscribeEmailToNewsletter to function declaration

--- packages/api-client/src/api/subscribeEmailToNewsletter/index.ts
+++ packages/api-client/src/api/subscribeEmailToNewsletter/index.ts
@@ -1,30 +1,25 @@
 import { FetchResult } from '@apollo/client/core';
 import { CustomQuery } from '@vue-storefront/core';
-import subscribeEmailToNewsletter from './subscribeEmailToNewsletter';
-import {
-  SubscribeEmailToNewsletterMutation, SubscribeEmailToNewsletterMutationVariables,
-} from '../../types/GraphQL';
+import subscribeEmailToNewsletterMutation from './subscribeEmailToNewsletter';
+import { SubscribeEmailToNewsletterMutation, SubscribeEmailToNewsletterMutationVariables } from '../../types/GraphQL';
 import { Context } from '../../types/context';
 
-export default async (
+export default async function subscribeEmailToNewsletter(
   context: Context,
   { email }: SubscribeEmailToNewsletterMutationVariables,
   customQuery: CustomQuery = { subscribeEmailToNewsletter: 'subscribeEmailToNewsletter' },
-): Promise<FetchResult<SubscribeEmailToNewsletterMutation>> => {
-  const { subscribeEmailToNewsletter: subscribeEmailToNewsletterGQL } = context.extendQuery(
-    customQuery,
-    {
-      subscribeEmailToNewsletter: {
-        query: subscribeEmailToNewsletter,
-        variables: {
-          email,
-        },
+): Promise<FetchResult<SubscribeEmailToNewsletterMutation>> {
+  const { subscribeEmailToNewsletter: subscribeEmailToNewsletterGQL } = context.extendQuery(customQuery, {
+    subscribeEmailToNewsletter: {
+      query: subscribeEmailToNewsletterMutation,
+      variables: {
+        email,
       },
     },
-  );
+  });
 
   return context.client.mutate<SubscribeEmailToNewsletterMutation, SubscribeEmailToNewsletterMutationVariables>({
     mutation: subscribeEmailToNewsletterGQL.query,
     variables: subscribeEmailToNewsletterGQL.variables,
   });
-};
+}
