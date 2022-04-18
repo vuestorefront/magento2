aca5637270b69164466730b956253f4ab9633cd4
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-04-18 16:24:50 -0300

    chore: convert setPaymentMethodOnCart to function declaration

--- packages/api-client/src/api/setPaymentMethodOnCart/index.ts
+++ packages/api-client/src/api/setPaymentMethodOnCart/index.ts
@@ -1,11 +1,7 @@
 import { FetchResult } from '@apollo/client/core';
 import { CustomQuery } from '@vue-storefront/core';
-import setPaymentMethodOnCart from './setPaymentMethodOnCart';
-import {
-  SetPaymentMethodOnCartInput,
-  SetPaymentMethodOnCartMutation,
-  SetPaymentMethodOnCartMutationVariables,
-} from '../../types/GraphQL';
+import setPaymentMethodOnCartMutation from './setPaymentMethodOnCart';
+import { SetPaymentMethodOnCartInput, SetPaymentMethodOnCartMutation, SetPaymentMethodOnCartMutationVariables } from '../../types/GraphQL';
 import { Context } from '../../types/context';
 
 export interface SetPaymentMethodOnCartInputs extends SetPaymentMethodOnCartInput {
@@ -19,23 +15,20 @@ export interface SetPaymentMethodOnCartInputs extends SetPaymentMethodOnCartInpu
  * @param input params containing the cart's ID and the payment method
  * @param [customQuery] (optional) - custom GraphQL query that extends the default query
  */
-export default async (
+export default async function setPaymentMethodOnCart(
   context: Context,
   input: SetPaymentMethodOnCartInputs,
   customQuery: CustomQuery = { setPaymentMethodOnCart: 'setPaymentMethodOnCart' },
-): Promise<FetchResult<SetPaymentMethodOnCartMutation>> => {
-  const { setPaymentMethodOnCart: setPaymentMethodOnCartGQL } = context.extendQuery(
-    customQuery,
-    {
-      setPaymentMethodOnCart: {
-        query: setPaymentMethodOnCart,
-        variables: { input },
-      },
+): Promise<FetchResult<SetPaymentMethodOnCartMutation>> {
+  const { setPaymentMethodOnCart: setPaymentMethodOnCartGQL } = context.extendQuery(customQuery, {
+    setPaymentMethodOnCart: {
+      query: setPaymentMethodOnCartMutation,
+      variables: { input },
     },
-  );
+  });
 
   return context.client.mutate<SetPaymentMethodOnCartMutation, SetPaymentMethodOnCartMutationVariables>({
     mutation: setPaymentMethodOnCartGQL.query,
     variables: setPaymentMethodOnCartGQL.variables,
   });
-};
+}
