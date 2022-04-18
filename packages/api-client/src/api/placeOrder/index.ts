6c8a23e896ad6714bc1b8265cece66606997673d
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-04-18 03:07:52 -0300

    chore: remove unused gql import in placeOrder API method

--- packages/api-client/src/api/placeOrder/index.ts
+++ packages/api-client/src/api/placeOrder/index.ts
@@ -1,5 +1,4 @@
 import { FetchResult } from '@apollo/client/core';
-import gql from 'graphql-tag';
 import { CustomQuery } from '@vue-storefront/core';
 import placeOrderQuery from './placeOrder';
 import {
