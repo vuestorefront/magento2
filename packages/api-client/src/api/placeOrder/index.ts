bee485516b6839177d41a34972cf34f2f05724e1
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-04-18 03:08:54 -0300

    chore: convert placeOrder to function declaration

--- packages/api-client/src/api/placeOrder/index.ts
+++ packages/api-client/src/api/placeOrder/index.ts
@@ -1,31 +1,27 @@
 import { FetchResult } from '@apollo/client/core';
 import { CustomQuery } from '@vue-storefront/core';
-import placeOrderQuery from './placeOrder';
-import {
-  PlaceOrderInput,
-  PlaceOrderMutation,
-  PlaceOrderMutationVariables,
-} from '../../types/GraphQL';
+import placeOrderMutation from './placeOrder';
+import { PlaceOrderInput, PlaceOrderMutation, PlaceOrderMutationVariables } from '../../types/GraphQL';
 import { Context } from '../../types/context';
 
-export default async (
+export default async function placeOrder(
   context: Context,
   input: PlaceOrderInput,
   customQuery: CustomQuery = { placeOrder: 'placeOrder' },
-): Promise<FetchResult<PlaceOrderMutation>> => {
-  const { placeOrder } = context.extendQuery(customQuery, {
+): Promise<FetchResult<PlaceOrderMutation>> {
+  const { placeOrder: placeOrderGQL } = context.extendQuery(customQuery, {
     placeOrder: {
-      query: placeOrderQuery,
+      query: placeOrderMutation,
       variables: { input },
     },
   });
 
   try {
     return await context.client.mutate<PlaceOrderMutation, PlaceOrderMutationVariables>({
-      mutation: placeOrder.query,
-      variables: placeOrder.variables,
+      mutation: placeOrderGQL.query,
+      variables: placeOrderGQL.variables,
     });
   } catch (error) {
     throw error.graphQLErrors?.[0].message || error.networkError?.result || error;
   }
-};
+}
