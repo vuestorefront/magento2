b134fa2b77d6a75b97f87d6e34b271a8bcaae986
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-04-21 00:07:47 -0300

    fix: rollback usePaymentProvider commands arguments to context

--- packages/theme/composables/usePaymentProvider/commands/getAvailablePaymentMethodsCommand.ts
+++ packages/theme/composables/usePaymentProvider/commands/getAvailablePaymentMethodsCommand.ts
@@ -1,9 +1,11 @@
-import type { NuxtAppOptions } from '@nuxt/types';
+import type { useContext } from '@nuxtjs/composition-api';
 import type { AvailablePaymentMethod } from '~/modules/GraphQL/types';
 
+type Context = ReturnType<typeof useContext>;
+
 export const getAvailablePaymentMethodsCommand = {
-  execute: async (app: NuxtAppOptions, cartId: string): Promise<AvailablePaymentMethod[]> => {
-    const { data } = await app.$vsf.$magento.api.getAvailablePaymentMethods({ cartId });
+  execute: async (context: Context, cartId: string): Promise<AvailablePaymentMethod[]> => {
+    const { data } = await context.app.$vsf.$magento.api.getAvailablePaymentMethods({ cartId });
 
     return data?.cart?.available_payment_methods ?? [];
   },
--- packages/theme/composables/usePaymentProvider/commands/setPaymentMethodOnCartCommand.ts
+++ packages/theme/composables/usePaymentProvider/commands/setPaymentMethodOnCartCommand.ts
@@ -1,10 +1,12 @@
-import type { NuxtAppOptions } from '@nuxt/types';
+import type { useContext } from '@nuxtjs/composition-api';
 import type { AvailablePaymentMethod } from '~/modules/GraphQL/types';
 import type PaymentMethodParams from '../PaymentMethodParams';
 
+type Context = ReturnType<typeof useContext>;
+
 export const setPaymentMethodOnCartCommand = {
-  execute: async (app: NuxtAppOptions, params: PaymentMethodParams): Promise<AvailablePaymentMethod[]> => {
-    const { data } = await app.$vsf.$magento.api.setPaymentMethodOnCart(params);
+  execute: async (context: Context, params: PaymentMethodParams): Promise<AvailablePaymentMethod[]> => {
+    const { data } = await context.app.$vsf.$magento.api.setPaymentMethodOnCart(params);
 
     return data?.setPaymentMethodOnCart?.cart.available_payment_methods ?? [];
   },
