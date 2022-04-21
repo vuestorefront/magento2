550d875861428967c73b8088ba782ad0dcd569d3
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-04-20 23:47:55 -0300

    fix: rollback useMakeOrder command argument to context

--- packages/theme/composables/useMakeOrder/commands/placeOrderCommand.ts
+++ packages/theme/composables/useMakeOrder/commands/placeOrderCommand.ts
@@ -1,9 +1,11 @@
-import type { NuxtAppOptions } from '@nuxt/types';
+import type { useContext } from '@nuxtjs/composition-api';
 import type { PlaceOrderOutput } from '~/modules/GraphQL/types';
 
+type Context = ReturnType<typeof useContext>;
+
 export const placeOrderCommand = {
-  execute: async (app: NuxtAppOptions, cartId: string): Promise<PlaceOrderOutput | null> => {
-    const { data } = await app.$vsf.$magento.api.placeOrder({ cart_id: cartId });
+  execute: async (context: Context, cartId: string): Promise<PlaceOrderOutput | null> => {
+    const { data } = await context.app.$vsf.$magento.api.placeOrder({ cart_id: cartId });
 
     return data?.placeOrder ?? null;
   },
