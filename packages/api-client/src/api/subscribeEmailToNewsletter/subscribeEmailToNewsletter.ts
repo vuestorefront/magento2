297fff9a91dc25c6cdcfd2bf5cdfc8761260db48
Vitor L Cavalcanti <vitorluizc@outlook.com>
2022-04-21 08:52:41 -0300

    feat!: make `error` and `loading` properties readonly on `useNewsletter` composable (#879)
    * feat!: rename UseNewsletter to UseNewsletterInterface
    
    * fix: fix error and loading types in useNewsletter
    
    * feat!: make error and loading properties readonly on useNewsletter
    
    * feat: add UseNewsletterUpdateSubscriptionParams type
    
    * chore: convert useNewsletter to function declaration
    
    * docs: write docs for useNewsletter
    
    * feat: export useNewsletter and its types in ~/composables
    
    * feat: use a real enum in SubscriptionStatusesEnum
    
    * chore: convert subscribeEmailToNewsletter to function declaration
    
    * docs: write docs for subscribeEmailToNewsletter
    
    * feat: remove repeated SubscriptionStatusesEnum
    
    * chore: use 'import type' on subscribeEmailToNewsletter
    
    Co-authored-by: Bartosz Herba <bartoszherba@gmail.com>
    
    * feat: define types for updateSubscriptionCommand
    
    * fix: rollback useNewsletter command argument to context
    
    Co-authored-by: Bartosz Herba <bartoszherba@gmail.com>
    Co-authored-by: Marcin Kwiatkowski <marcin.kwiatkowski@vuestorefront.io>

--- packages/api-client/src/api/subscribeEmailToNewsletter/subscribeEmailToNewsletter.ts
+++ packages/api-client/src/api/subscribeEmailToNewsletter/subscribeEmailToNewsletter.ts
@@ -1,5 +1,6 @@
 import gql from 'graphql-tag';
 
+/** GraphQL Mutation that subscribes an email in the newsletter. */
 export default gql`
   mutation subscribeEmailToNewsletter($email: String!){
     subscribeEmailToNewsletter(email: $email) {
