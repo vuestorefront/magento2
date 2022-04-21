82f2b0b4011953a200eda0e2e1aa460ab90b1994
Vitor L Cavalcanti <vitorluizc@outlook.com>
2022-04-21 08:52:41 -0300

    feat!: make `error` and `loading` properties readonly on `useNewsletter` composable (#879)
    * feat!: rename UseNewsletter to UseNewsletterInterface
    
    * fix: fix error and loading types in useNewsletter
    
    * feat!: make error and loading properties readonly on useNewsletter
    
    * feat: add UseNewsletterUpdateSubscriptionParams type
    
    * chore: convert useNewsletter to function declaration
    
    * docs: write docs for useNewsletter
    
    * feat: export useNewsletter and its types in ~/composables
    
    * feat: use a real enum in SubscriptionStatusesEnum
    
    * chore: convert subscribeEmailToNewsletter to function declaration
    
    * docs: write docs for subscribeEmailToNewsletter
    
    * feat: remove repeated SubscriptionStatusesEnum
    
    * chore: use 'import type' on subscribeEmailToNewsletter
    
    Co-authored-by: Bartosz Herba <bartoszherba@gmail.com>
    
    * feat: define types for updateSubscriptionCommand
    
    * fix: rollback useNewsletter command argument to context
    
    Co-authored-by: Bartosz Herba <bartoszherba@gmail.com>
    Co-authored-by: Marcin Kwiatkowski <marcin.kwiatkowski@vuestorefront.io>

--- packages/theme/composables/useNewsletter/enums/SubscriptionStatusesEnum.ts
+++
@@ -1,8 +0,0 @@
-const SubscriptionStatusesEnum = {
-  NOT_ACTIVE: 'NOT_ACTIVE',
-  SUBSCRIBED: 'SUBSCRIBED',
-  UNSUBSCRIBED: 'UNSUBSCRIBED',
-  UNCONFIRMED: 'UNCONFIRMED',
-};
-
-export default SubscriptionStatusesEnum;
