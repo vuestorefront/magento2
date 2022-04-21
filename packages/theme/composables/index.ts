e3f7908c727bb22583a8aef366614b3fd1aa8182
Filip Sobol <sobol.filip@gmail.com>
2022-04-21 13:30:05 +0200

    docs: `useUpsellProducts` composable (M2-398)

--- packages/theme/composables/index.ts
+++ packages/theme/composables/index.ts
@@ -32,7 +32,7 @@ export { default as useShippingProvider } from './useShippingProvider';
 export { default as useStore } from './useStore';
 export { default as useUiHelpers } from './useUiHelpers';
 export { default as useUiNotification } from './useUiNotification';
-export { default as useUiState } from './useUiState';
+export * from './useUiState';
 export * from './useUpsellProducts';
 export * from './useUser';
 export * from './useUserAddress';
