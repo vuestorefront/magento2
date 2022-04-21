0809633e2daf5f347da34ba43b512ba210d2efa7
Vitor L Cavalcanti <vitorluizc@outlook.com>
2022-04-21 06:58:36 -0300

    feat!: make `error` and `loading` properties readonly on `usePaymentProvider` composable (#881)
    * feat!: rename SetPaymentMethodOnCartInputs to PaymentMethodParams and simplify it
    
    * chore: move PaymentMethodParams to its own module
    
    * feat: define types for usePaymentProvider composable
    
    * fix: usePaymentProvider commands should use context.app and return an array
    
    * feat!: make error and loading properties readonly on usePaymentProvider
    
    * chore: convert usePaymentProvider to function declaration
    
    * docs: write docs for usePaymentProvider
    
    * docs: write docs for getAvailablePaymentMethods
    
    * chore: convert getAvailablePaymentMethods to function declaration
    
    * docs: write docs for setPaymentMethodOnCart
    
    * chore: convert setPaymentMethodOnCart to function declaration
    
    * docs: fix typo on usePaymentProvider docs
    
    * chore: use 'import type' in getAvailablePaymentMethods
    
    Co-authored-by: Bartosz Herba <bartoszherba@gmail.com>
    
    * chore: use 'import type' in setPaymentMethodOnCart
    
    Co-authored-by: Bartosz Herba <bartoszherba@gmail.com>
    
    * fix: use context.app instead of context in usePaymentProvider commands
    
    * fix: rollback usePaymentProvider commands arguments to context
    
    Co-authored-by: Bartosz Herba <bartoszherba@gmail.com>

--- packages/theme/composables/usePaymentProvider/commands/setPaymentMethodOnCartCommand.ts
+++ packages/theme/composables/usePaymentProvider/commands/setPaymentMethodOnCartCommand.ts
@@ -1,17 +1,13 @@
-import { AvailablePaymentMethod } from '~/modules/GraphQL/types';
-import { SetPaymentMethodOnCartInputs } from '../usePaymentProvider';
+import type { useContext } from '@nuxtjs/composition-api';
+import type { AvailablePaymentMethod } from '~/modules/GraphQL/types';
+import type PaymentMethodParams from '../PaymentMethodParams';
+
+type Context = ReturnType<typeof useContext>;
 
 export const setPaymentMethodOnCartCommand = {
-  execute: async (context, paymentMethodParams: SetPaymentMethodOnCartInputs): Promise<AvailablePaymentMethod> => {
-    const { data } = await context
-      .$vsf
-      .$magento
-      .api
-      .setPaymentMethodOnCart(paymentMethodParams);
+  execute: async (context: Context, params: PaymentMethodParams): Promise<AvailablePaymentMethod[]> => {
+    const { data } = await context.app.$vsf.$magento.api.setPaymentMethodOnCart(params);
 
-    return data
-      .setPaymentMethodOnCart
-      .cart
-      .available_payment_methods;
+    return data?.setPaymentMethodOnCart?.cart.available_payment_methods ?? [];
   },
 };
