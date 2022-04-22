05ccacb53e2c3ffc8238997ed5b56326411af290
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-04-22 16:28:24 -0300

    chore: convert setShippingAddressesOnCart to function declaration

--- packages/api-client/src/api/setShippingAddressesOnCart/index.ts
+++ packages/api-client/src/api/setShippingAddressesOnCart/index.ts
@@ -1,12 +1,12 @@
-import { FetchResult } from '@apollo/client/core';
-import { CustomQuery } from '@vue-storefront/core';
-import setShippingAddressesOnCart from './setShippingAddressesOnCart';
-import {
+import type { FetchResult } from '@apollo/client/core';
+import type { CustomQuery } from '@vue-storefront/core';
+import setShippingAddressesOnCartQuery from './setShippingAddressesOnCart';
+import type {
   SetShippingAddressesOnCartInput,
   SetShippingAddressesOnCartMutation,
   SetShippingAddressesOnCartMutationVariables,
 } from '../../types/GraphQL';
-import { Context } from '../../types/context';
+import type { Context } from '../../types/context';
 
 /**
  * Sets a shipping address on received cart.
@@ -15,23 +15,20 @@ import { Context } from '../../types/context';
  * @param input params with cart ID and shipping address.
  * @param [customQuery] (optional) - custom GraphQL query that extends the default one
  */
-export default async (
+export default async function setShippingAddressesOnCart(
   context: Context,
   input: SetShippingAddressesOnCartInput,
   customQuery: CustomQuery = { setShippingAddressesOnCart: 'setShippingAddressesOnCart' },
-): Promise<FetchResult<SetShippingAddressesOnCartMutation>> => {
-  const { setShippingAddressesOnCart: setShippingAddressesOnCartGQL } = context.extendQuery(
-    customQuery,
-    {
-      setShippingAddressesOnCart: {
-        query: setShippingAddressesOnCart,
-        variables: { input },
-      },
+): Promise<FetchResult<SetShippingAddressesOnCartMutation>> {
+  const { setShippingAddressesOnCart: setShippingAddressesOnCartGQL } = context.extendQuery(customQuery, {
+    setShippingAddressesOnCart: {
+      query: setShippingAddressesOnCartQuery,
+      variables: { input },
     },
-  );
+  });
 
   return context.client.mutate<SetShippingAddressesOnCartMutation, SetShippingAddressesOnCartMutationVariables>({
     mutation: setShippingAddressesOnCartGQL.query,
     variables: setShippingAddressesOnCartGQL.variables,
   });
-};
+}
