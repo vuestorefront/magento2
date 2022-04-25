9bf6356008deca17a15d6fbd1b2ceb7a5d4197e2
Bartosz Herba <bartoszherba@gmail.com>
2022-04-25 14:41:10 +0200

    fix: wishlist sidebar (#918)
    - fix issue with refreshig sidebar wishlist
    - fix issue with wishlist sidebar transition
    - fix issue with cart sidebar transition
    M2-539

--- packages/theme/components/AppHeader.vue
+++ packages/theme/components/AppHeader.vue
@@ -165,12 +165,12 @@ export default defineComponent({
     const { isAuthenticated } = useUser();
     const { loadTotalQty: loadCartTotalQty, cart } = useCart();
     const { loadItemsCount: loadWishlistItemsCount } = useWishlist();
-
     const { categories: categoryList, load: categoriesListLoad } = useCategory();
 
+    const customerStore = useCustomerStore();
     const isSearchOpen = ref(false);
     const result = ref(null);
-    const wishlistItemsQty = ref(null);
+    const wishlistItemsQty = computed(() => customerStore.wishlist?.items_count ?? 0);
 
     const wishlistHasProducts = computed(() => wishlistItemsQty.value > 0);
     const accountIcon = computed(() => (isAuthenticated.value ? 'profile_fill' : 'profile'));
@@ -191,16 +191,11 @@ export default defineComponent({
         .filter((category) => category.include_in_menu);
     });
 
-    onMounted(() => {
+    onMounted(async () => {
       if (app.$device.isDesktop) {
-        loadCartTotalQty();
+        await loadCartTotalQty();
         // eslint-disable-next-line promise/catch-or-return
-        loadWishlistItemsCount({})
-          .then((response) => {
-            wishlistItemsQty.value = response;
-
-            return response;
-          });
+        await loadWishlistItemsCount({});
       }
     });
 
