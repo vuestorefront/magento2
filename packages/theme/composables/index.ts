b30928fb6dc8f7aaa3e0ab22805288432d4482a6
Vitor L Cavalcanti <vitorluizc@outlook.com>
2022-04-26 06:51:18 -0300

    feat: define types and write docs for `useUiHelpers` composable (#923)
    * chore: convert top-level arrows to function declarations on useUiHelpers
    
    * feat: define types for reduceFilters
    
    * docs: write docs for useUiHelpers params interfaces and group them
    
    * feat: define UseUiHelpersInterface type and export it
    
    * docs: write the docs for useUiHelpers composable
    
    * feat: export useUiHelpers as named too
    
    * fix: replace broken params type in useUiHelpers
    
    * docs: fix broken reference in changeSearchTerm method

--- packages/theme/composables/index.ts
+++ packages/theme/composables/index.ts
@@ -30,7 +30,7 @@ export * from './useReview';
 export * from './useShippingProvider';
 export * from './useShipping';
 export * from './useStore';
-export { default as useUiHelpers } from './useUiHelpers';
+export * from './useUiHelpers';
 export * from './useUiNotification';
 export * from './useUiState';
 export * from './useUpsellProducts';
