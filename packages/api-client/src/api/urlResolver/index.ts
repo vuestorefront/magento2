5cbf2dfb1845e43bcea0981efc47498958cc29cd
Vitor L Cavalcanti <vitorluizc@outlook.com>
2022-04-26 06:52:55 -0300

    feat!: standardize `useUrlResolver` composable and make its properties readonly (#925)
    * feat!: move useUrlResolver to its own folder
    
    * feat: define types for useUrlResolver composable
    
    * feat!: make error and loading properties readonly on useUrlResolver
    
    * chore: convert useUrlResolver to function declaration
    
    * docs: write docs for urlResolver API method
    
    * docs: write the docs for useUrlResolver
    
    * feat: export useUrlResolver and its types from ~/composables
    
    * chore: convert urlResolver to function declaration and format it
    
    * docs: fix order of useUrlResolver in the composables menu

--- packages/api-client/src/api/urlResolver/index.ts
+++ packages/api-client/src/api/urlResolver/index.ts
@@ -1,26 +1,30 @@
-import { ApolloQueryResult } from '@apollo/client/core';
-import { CustomQuery } from '@vue-storefront/core';
-import { UrlResolverQuery, UrlResolverQueryVariables } from '../../types/GraphQL';
-import urlResolver from './urlResolver';
-import { Context } from '../../types/context';
+import type { ApolloQueryResult } from '@apollo/client/core';
+import type { CustomQuery } from '@vue-storefront/core';
+import type { UrlResolverQuery, UrlResolverQueryVariables } from '../../types/GraphQL';
+import urlResolverQuery from './urlResolver';
+import type { Context } from '../../types/context';
 
-export default async (
+/**
+ * Fetches the resolver for received URL.
+ *
+ * @param context VSF Context
+ * @param url the URL to be resolved
+ * @param [customQuery] (optional) - custom GraphQL query that extends the default one
+ */
+export default async function urlResolver(
   context: Context,
   url: string,
   customQuery: CustomQuery = { urlResolver: 'urlResolver' },
-): Promise<ApolloQueryResult<UrlResolverQuery>> => {
-  const { urlResolver: urlResolverGQL } = context.extendQuery(
-    customQuery,
-    {
-      urlResolver: {
-        query: urlResolver,
-        variables: { url },
-      },
+): Promise<ApolloQueryResult<UrlResolverQuery>> {
+  const { urlResolver: urlResolverGQL } = context.extendQuery(customQuery, {
+    urlResolver: {
+      query: urlResolverQuery,
+      variables: { url },
     },
-  );
+  });
 
   return context.client.query<UrlResolverQuery, UrlResolverQueryVariables>({
     query: urlResolverGQL.query,
     variables: urlResolverGQL.variables,
   });
-};
+}
