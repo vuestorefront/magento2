02bd87b6fe3fb2d548a8a70c8af7e67a8e3ac8bc
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-04-26 18:31:47 -0300

    docs: write usage example for useApi

--- packages/theme/composables/useApi/index.ts
+++ packages/theme/composables/useApi/index.ts
@@ -2,7 +2,64 @@ import { useContext } from '@nuxtjs/composition-api';
 import type { Variables } from 'graphql-request';
 import type { UseApiInterface } from './UseApi';
 
-/** The `useApi()` composable allows executing GraphQL queries and mutations. */
+/**
+ * The `useApi()` composable allows executing GraphQL queries and mutations.
+ *
+ * @example
+ *
+ * Execute a GraphQL Query and handle its error, loading and result:
+ *
+ * ```ts
+ * import { ref, readonly } from '@nuxtjs/composition-api';
+ * import { useApi } from '~/composables/useApi';
+ *
+ * const GET_CART_PRICE_QUERY = `
+ *   query GET_CART_PRICE_QUERY($cartId: String!) {
+ *     cart(cart_id: $cartId) {
+ *       prices {
+ *         subtotal_excluding_tax {
+ *           value
+ *         }
+ *       }
+ *     }
+ *   }
+ * `;
+ *
+ * export default {
+ *   setup() {
+ *     const { query } = useApi();
+ *
+ *     const price = ref<number | null>(null);
+ *     const loading = ref(false);
+ *     const error = ref<Error | null>(null);
+ *
+ *     const getCartPrice = async (cartId: string) => {
+ *       loading.value = false;
+ *       try {
+ *         const result = await query(GET_CART_PRICE_QUERY, { cartId }, {
+ *           'Accept': 'application/json',
+ *         });
+ *
+ *         price.value = result.data?.cart?.prices?.[0]?.subtotal_excluding_tax?.value ?? null;
+ *         error.value = null;
+ *       } catch (error) {
+ *         price.value = null;
+ *         error.value = error;
+ *       } finally {
+ *         loading.value = false;
+ *       }
+ *     };
+ *
+ *     return {
+ *       price: readonly(price),
+ *       error: readonly(error),
+ *       loading: readonly(loading),
+ *       getCartPrice,
+ *     };
+ *   },
+ * };
+ * ```
+ */
 export function useApi(): UseApiInterface {
   const context = useContext();
 
