f80299636bb30459aec26d7bfd1d23fea2bfe02a
Bartosz Herba <bartoszherba@gmail.com>
2022-04-28 10:08:26 +0200

    feat: header categories (#903)
    * feat: header categories
    - add multilevel categories navigation
    - add unit tests for new components
    - upgrade @testing-library/user-event @13.5 -> @14.1.1
    
    M2-424
    
    * refactor: fix CR issues

---
+++ packages/theme/components/Header/Navigation/__tests__/HeaderNavigationSubcategories.spec.js
@@ -0,0 +1,54 @@
+import userEvent from '@testing-library/user-event';
+import HeaderNavigationSubcategories from '../HeaderNavigationSubcategories.vue';
+import { render } from '~/test-utils';
+import CategoryTreeDataMock from '~/test-utils/mocks/categoryTreeDataMock';
+import { useUiHelpers } from '~/composables';
+import useUiHelpersMock from '~/test-utils/mocks/useUiHelpersMock';
+
+jest.mock('~/composables');
+useUiHelpers.mockReturnValue(useUiHelpersMock());
+
+// Women category selection
+const childrenProps = { props: { currentCategory: CategoryTreeDataMock[0].children[1] } };
+const noChildrenProps = { props: { currentCategory: CategoryTreeDataMock[0].children[0] } };
+
+describe('HeaderNavigationSubcategories', () => {
+  it('displays level 1 children category links', () => {
+    const { getByRole } = render(HeaderNavigationSubcategories, childrenProps);
+    getByRole('link', { name: /tops/i });
+    getByRole('link', { name: /bottoms/i });
+  });
+
+  it('displays level 2 children category links', () => {
+    const { getByRole } = render(HeaderNavigationSubcategories, childrenProps);
+    getByRole('link', { name: /jackets/i });
+    getByRole('link', { name: /hoodies & sweatshirts/i });
+    getByRole('link', { name: /tees/i });
+    getByRole('link', { name: /tanks/i });
+  });
+
+  it('do not display any links if root category has no children', () => {
+    const { queryByTestId } = render(HeaderNavigationSubcategories, noChildrenProps);
+    expect(queryByTestId(/navigation-subcategories/i)).toBeNull();
+  });
+
+  it('should emit "hideSubcategories" event when linkLv1 is clicked', async () => {
+    const user = userEvent.setup();
+    const { getByRole, emitted } = render(HeaderNavigationSubcategories, childrenProps);
+    const lvl1Link = getByRole('link', { name: /tops/i });
+
+    await user.click(lvl1Link);
+
+    expect(emitted()).toHaveProperty('hideSubcategories');
+  });
+
+  it('should emit "hideSubcategories" event when linkLv2 is clicked', async () => {
+    const user = userEvent.setup();
+    const { getByRole, emitted } = render(HeaderNavigationSubcategories, childrenProps);
+    const lvl2Link = getByRole('link', { name: /jackets/i });
+
+    await user.click(lvl2Link);
+
+    expect(emitted()).toHaveProperty('hideSubcategories');
+  });
+});
