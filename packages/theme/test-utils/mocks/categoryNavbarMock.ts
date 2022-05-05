42107a359e75f37330303c878283d24e7bc338af
Diego Alba <diegoalba15@icloud.com>
2022-05-05 19:19:59 +0000

    test: categorynavbar component

---
+++ packages/theme/test-utils/mocks/categoryNavbarMock.ts
@@ -0,0 +1,35 @@
+import { AgnosticPagination } from '~/composables/types';
+
+export const paginationData: AgnosticPagination = {
+  currentPage: 1,
+  totalPages: 2,
+  totalItems: 11,
+  itemsPerPage: 10,
+  pageOptions: [10, 50, 100],
+};
+
+export const sortByData = {
+  options: [
+    {
+      label: 'Sort: Default',
+      value: '',
+    },
+    {
+      label: 'Sort: Name A-Z',
+      value: 'name_ASC',
+    },
+    {
+      label: 'Sort: Name Z-A',
+      value: 'name_DESC',
+    },
+    {
+      label: 'Sort: Price from low to high',
+      value: 'price_ASC',
+    },
+    {
+      label: 'Sort: Price from high to low',
+      value: 'price_DESC',
+    },
+  ],
+  selected: 'name_DESC',
+};
