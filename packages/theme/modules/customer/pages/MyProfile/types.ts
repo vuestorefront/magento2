c57acf3ea7a734c439a7a22b31ed8a2f0929a733
Artur Tagisow <atagisow@vuestorefront.io>
2022-05-06 12:31:59 +0200

    refactor: better MyProfile typing
    M2-546
    
    refactor: add real myprofile form types
    
    refactor: improve typing

--- packages/theme/modules/customer/pages/MyProfile/types.ts
+++ packages/theme/modules/customer/pages/MyProfile/types.ts
@@ -1,11 +1,21 @@
-import type { Ref } from '@nuxtjs/composition-api';
-
-export type Form = Ref<Record<string, unknown>>;
 export type OnFormComplete = () => void;
 export type OnFormError = (message: string) => void;
 
-export type SubmitEventPayload = {
-  form: Form,
+export type SubmitEventPayload<TForm extends Record<string, unknown>> = {
+  form: TForm,
   onComplete: OnFormComplete,
   onError: OnFormError,
+};
+
+export type PasswordResetFormFields = {
+  currentPassword: string,
+  newPassword: string,
+  repeatPassword: string,
+};
+
+export type ProfileUpdateFormFields = {
+  firstname: string,
+  lastname: string,
+  email: string,
+  password?: string;
 };
