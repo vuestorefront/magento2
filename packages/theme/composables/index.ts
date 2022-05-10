e236748bd90d7b4f4543a7de19d2445fac715bc9
Filip Sobol <sobol.filip@gmail.com>
2022-05-10 12:42:20 +0200

    docs: fix build

--- packages/theme/composables/index.ts
+++ packages/theme/composables/index.ts
@@ -45,6 +45,4 @@ export * from './useMagentoConfiguration';
 
 export * from './types';
 export * from '../modules/GraphQL/types';
-export { default as InjectDOMPurify } from '../plugins/dompurify';
-export { default as InjectFormatCurrency } from '../plugins/fcPlugin';
 export { default as InjectGraphQLClient } from '../plugins/graphqlClient';
