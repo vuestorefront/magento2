ed340b4de0d73044d632612bfa9e14224d02e6ad
Artur Tagisow <atagisow@vuestorefront.io>
2022-05-16 15:22:45 +0200

    chore: fix import order

--- packages/theme/modules/catalog/category/composables/useFacet/index.ts
+++ packages/theme/modules/catalog/category/composables/useFacet/index.ts
@@ -2,16 +2,16 @@ import { readonly, ref } from '@nuxtjs/composition-api';
 import { Logger } from '~/helpers/logger';
 import type { ComposableFunctionArgs } from '~/composables/types';
 import type { GetProductSearchParams } from '~/modules/catalog/product/types';
-import type {
-  UseFacetInterface, UseFacetErrors, UseFacetSearchResult, FacetSearchParams,
-} from './useFacet';
 import useApi from '~/composables/useApi';
-import GetFacetDataQuery from './getFacetData.gql';
 import { SortingOptions } from '~/modules/catalog/category/composables/useFacet/SortingOptions';
 import { PerPageOptions } from '~/modules/catalog/category/composables/useFacet/PerPageOptions';
 import { createProductAttributeFilterInput } from '~/modules/catalog/category/composables/useFacet/input/createProductAttributeFilterInput';
 import { createProductAttributeSortInput } from '~/modules/catalog/category/composables/useFacet/input/createProductAttributeSortInput';
 import { Products } from '~/modules/GraphQL/types';
+import GetFacetDataQuery from './getFacetData.gql';
+import type {
+  UseFacetInterface, UseFacetErrors, UseFacetSearchResult, FacetSearchParams,
+} from './useFacet';
 
 /**
  * The `useFacet()` composable allows searching for products using facets.
