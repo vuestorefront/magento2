525b872a0d01f87e4c3e6c2332fa1648a9ceef9f
Artur Tagisow <atagisow@vuestorefront.io>
2022-05-16 12:34:30 +0200

    chore: autofix asyncLocalStorage
    This is ONLY autofixes, I did not make any manual changes.

--- packages/theme/helpers/__tests__/asyncLocalStorage.spec.js
+++ packages/theme/helpers/__tests__/asyncLocalStorage.spec.js
@@ -1,9 +1,13 @@
-import { setItem, getItem, mergeItem, removeItem, clear } from '~/helpers/asyncLocalStorage';
+import {
+  setItem, getItem, mergeItem, removeItem, clear,
+} from '~/helpers/asyncLocalStorage';
+
+/* eslint-disable no-underscore-dangle */
 
 describe('asyncLocalStorage :: Promised Based Localstorage Management', () => {
   test('setItem :: should store to localStorage by key', async () => {
-    const KEY = 'jest'
-    const VSF_KEY = `vsf-${KEY}`
+    const KEY = 'jest';
+    const VSF_KEY = `vsf-${KEY}`;
     const VALUE = 'jest-localstorage-value';
     const VSF_VALUE = JSON.stringify(VALUE);
 
@@ -11,12 +15,12 @@ describe('asyncLocalStorage :: Promised Based Localstorage Management', () => {
 
     expect(localStorage.setItem).toHaveBeenLastCalledWith(VSF_KEY, VSF_VALUE);
     expect(localStorage.__STORE__[VSF_KEY]).toBe(VSF_VALUE);
-    expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+    expect(Object.keys(localStorage.__STORE__)).toHaveLength(1);
   });
 
   test('getItem :: should get value from localStorage by key', async () => {
-    const KEY = 'jest'
-    const VSF_KEY = `vsf-${KEY}`
+    const KEY = 'jest';
+    const VSF_KEY = `vsf-${KEY}`;
     const VSF_VALUE = 'jest-localstorage-value';
 
     const VALUE = await getItem(KEY);
@@ -27,44 +31,43 @@ describe('asyncLocalStorage :: Promised Based Localstorage Management', () => {
   });
 
   test('merge :: should merge values to localStorage by key', async () => {
-    const KEY = 'jest'
-    const VSF_KEY = `vsf-${KEY}`
-    const VALUE = {row1: 'Lonely'};
+    const KEY = 'jest';
+    const VSF_KEY = `vsf-${KEY}`;
+    const VALUE = { row1: 'Lonely' };
     const VSF_VALUE = JSON.stringify(VALUE);
-    const MERGE_VALUE = {row2: 'Not anymore'};
+    const MERGE_VALUE = { row2: 'Not anymore' };
     const VSF_MERGE_VALUE = JSON.stringify({
       row1: 'Lonely',
-      row2: 'Not anymore'
-    })
+      row2: 'Not anymore',
+    });
 
     await setItem(KEY, VALUE);
 
     expect(localStorage.__STORE__[VSF_KEY]).toBe(VSF_VALUE);
-    expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+    expect(Object.keys(localStorage.__STORE__)).toHaveLength(1);
 
     await mergeItem(KEY, MERGE_VALUE);
 
     expect(localStorage.__STORE__[VSF_KEY]).toBe(VSF_MERGE_VALUE);
-    expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+    expect(Object.keys(localStorage.__STORE__)).toHaveLength(1);
   });
 
   test('remove :: should remove a key and value from localStorage', async () => {
-    const KEY = 'jest'
-    const VSF_KEY = `vsf-${KEY}`
+    const KEY = 'jest';
+    const VSF_KEY = `vsf-${KEY}`;
 
-    expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+    expect(Object.keys(localStorage.__STORE__)).toHaveLength(1);
 
     await removeItem(KEY);
 
     expect(localStorage.removeItem).toHaveBeenCalledWith(VSF_KEY);
-    expect(Object.keys(localStorage.__STORE__).length).toBe(0);
+    expect(Object.keys(localStorage.__STORE__)).toHaveLength(0);
   });
 
   test('clear :: should clear all values and keys from localStorage', async () => {
     await clear();
 
     expect(localStorage.clear).toHaveBeenCalledWith();
-    expect(Object.keys(localStorage.__STORE__).length).toBe(0);
+    expect(Object.keys(localStorage.__STORE__)).toHaveLength(0);
   });
 });
-
