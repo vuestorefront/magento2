b27b0578c450e4cb4febb3f55be576bc0b2ea692
Artur Tagisow <atagisow@vuestorefront.io>
2022-05-17 07:39:02 +0200

    refactor: improve asyncLocalStorage typing

--- packages/theme/helpers/asyncLocalStorage.ts
+++ packages/theme/helpers/asyncLocalStorage.ts
@@ -1,14 +1,22 @@
-const getVsfKey = (key) => `vsf-${key}`;
+const getVsfKey = (key: string) => `vsf-${key}`;
 
-const mergeLocalStorageItem = (key: string, value: string) => {
+type Callback<TValue> = (error: Error | null, value?: TValue) => void;
+
+const mergeLocalStorageItem = (key: string, value: Record<string, unknown>) : void => {
   const oldValue = window.localStorage.getItem(key);
   const oldObject = JSON.parse(oldValue);
   const newObject = value;
-  const nextValue = JSON.stringify({ ...JSON.parse(JSON.stringify(oldObject)), ...JSON.parse(JSON.stringify(newObject)) });
+  const nextValue = JSON.stringify({
+    ...JSON.parse(JSON.stringify(oldObject)),
+    ...JSON.parse(JSON.stringify(newObject)),
+  });
   window.localStorage.setItem(key, nextValue);
 };
 
-const createPromise = (getValue, callback): Promise<any> => new Promise((resolve, reject) => {
+const createPromise = <TValue>(
+  getValue: () => TValue,
+  callback: Callback<TValue>,
+) : Promise<TValue> => new Promise((resolve, reject) => {
   try {
     const value = getValue();
     if (callback) {
@@ -17,26 +25,36 @@ const createPromise = (getValue, callback): Promise<any> => new Promise((resolve
     resolve(value);
   } catch (err) {
     if (callback) {
-      callback(err);
+      callback(err as Error);
     }
     reject(err);
   }
 });
 
-// eslint-disable-next-line max-len
-export const getItem = (key: string, callback?: Function): Promise<any> => createPromise(() => JSON.parse(window.localStorage.getItem(getVsfKey(key))), callback);
+export const getItem = <T = unknown>(
+  key: string,
+  callback?: Callback<T>,
+): Promise<T> => createPromise(() => JSON.parse(window.localStorage.getItem(getVsfKey(key))), callback);
 
-// eslint-disable-next-line max-len
-export const setItem = (key: string, value: string, callback?: Function): Promise<any> => createPromise(() => (window.localStorage.setItem(getVsfKey(key), JSON.stringify(value))), callback);
+export const setItem = <T = unknown>(
+  key: string,
+  value: T,
+  callback?: Callback<void>,
+): Promise<void> => createPromise(() => (window.localStorage.setItem(getVsfKey(key), JSON.stringify(value))), callback);
 
-// eslint-disable-next-line max-len
-export const removeItem = (key: string, callback?: Function): Promise<any> => createPromise(() => {
+export const removeItem = (
+  key: string,
+  callback?: Callback<void>,
+): Promise<void> => createPromise(() => {
   window.localStorage.removeItem(getVsfKey(key));
 }, callback);
 
-// eslint-disable-next-line max-len
-export const mergeItem = (key: string, value: string, callback?: Function): Promise<any> => createPromise(() => mergeLocalStorageItem(getVsfKey(key), value), callback);
+export const mergeItem = (
+  key: string,
+  value: Record<string, unknown>,
+  callback?: Callback<void>,
+): Promise<void> => createPromise(() => mergeLocalStorageItem(getVsfKey(key), value), callback);
 
-export const clear = (callback?: Function): Promise<any> => createPromise(() => {
+export const clear = (callback?: Callback<void>): Promise<void> => createPromise(() => {
   window.localStorage.clear();
 }, callback);
