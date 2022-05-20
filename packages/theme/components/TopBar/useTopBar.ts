f84f84e7784aea2a712cfa58ee68122e5a050ab3
Artur Tagisow <atagisow@vuestorefront.io>
2022-05-20 12:01:57 +0200

    refactor: improve TopBar typing

--- packages/theme/components/TopBar/useTopBar.ts
+++ packages/theme/components/TopBar/useTopBar.ts
@@ -1,20 +1,25 @@
 import { ref, onMounted } from '@nuxtjs/composition-api';
+import type { Currency, StoreConfig } from '~/modules/GraphQL/types';
 
-import useApi from '~/composables/useApi';
+import { useApi } from '~/composables/useApi';
 import checkStoresAndCurrencyQuery from './checkStoresAndCurrency.gql';
+
+type StoresAndCurrencyQueryResponse = {
+  availableStores: Pick<StoreConfig, 'store_code'>[],
+  currency: Pick<Currency, 'available_currency_codes'>,
+};
 
 export const useTopBar = () => {
   const { query } = useApi();
-  const hasStoresToSelect = ref(null);
-  const hasCurrencyToSelect = ref(null);
+  const hasStoresToSelect = ref<boolean | null>(null);
+  const hasCurrencyToSelect = ref<boolean | null>(null);
 
   onMounted(() => {
-    query(checkStoresAndCurrencyQuery)
+    query<StoresAndCurrencyQueryResponse>(checkStoresAndCurrencyQuery)
+      // eslint-disable-next-line promise/always-return
       .then((data) => {
-        hasStoresToSelect.value = data?.availableStores?.length;
-        hasCurrencyToSelect.value = data?.currency?.available_currency_codes?.length > 1;
-
-        return data;
+        hasStoresToSelect.value = data.availableStores.length > 1;
+        hasCurrencyToSelect.value = data.currency.available_currency_codes.length > 1;
       })
       .catch(() => {
         hasStoresToSelect.value = false;
