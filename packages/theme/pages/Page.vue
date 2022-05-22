ea5112a8892baab69fb5addc469c477d1ac66e97
Artur Tagisow <atagisow@vuestorefront.io>
2022-05-22 12:15:06 +0200

    refactor: fix eslint errors in Page.vue

--- packages/theme/pages/Page.vue
+++ packages/theme/pages/Page.vue
@@ -14,17 +14,15 @@
     </div>
   </SfLoader>
 </template>
-<script>
-import {
-  SfLoader,
-  SfHeading,
-} from '@storefront-ui/vue';
+<script lang="ts">
+import { SfLoader, SfHeading } from '@storefront-ui/vue';
 import {
   defineComponent, ref, useContext, useFetch, useRoute,
 } from '@nuxtjs/composition-api';
 import { useCache, CacheTagPrefix } from '@vue-storefront/cache';
 import { useContent } from '~/composables';
-import HTMLContent from '~/components/HTMLContent';
+import type { CmsPage } from '~/modules/GraphQL/types';
+import HTMLContent from '~/components/HTMLContent.vue';
 
 export default defineComponent({
   name: 'CmsPage',
@@ -41,22 +39,19 @@ export default defineComponent({
   },
   setup(props) {
     const { addTags } = useCache();
-    const {
-      loadPage,
-      error,
-      loading,
-    } = useContent('cmsPage');
+    const { loadPage, error, loading } = useContent();
 
     const route = useRoute();
     const { error: nuxtError, app } = useContext();
     const { params } = route.value;
-    const page = ref({});
+    const page = ref<CmsPage | null>(null);
 
+    // eslint-disable-next-line consistent-return
     useFetch(async () => {
       page.value = await loadPage({ identifier: params.slug || props.identifier });
 
       if (error?.value?.page || !page.value) {
-        return nuxtError({ statusCode: 404, message: app.i18n.t('Page not found') });
+        return nuxtError({ statusCode: 404, message: app.i18n.t('Page not found') as string });
       }
 
       addTags([{ prefix: CacheTagPrefix.View, value: page.value.identifier }]);
@@ -67,17 +62,19 @@ export default defineComponent({
     };
   },
   head() {
-    if (!this.page) {
+    // eslint-disable-next-line prefer-destructuring
+    const page : CmsPage | null = this.page;
+    if (!page) {
       return null;
     }
 
-    const title = this.page?.meta_title ? this.page.meta_title : this.page.title;
+    const title = page?.meta_title ? page.meta_title : page.title;
     const meta = [];
-    if (this.page.meta_description) {
+    if (page.meta_description) {
       meta.push({
         hid: 'description',
         name: 'description',
-        content: this.page.meta_description,
+        content: page.meta_description,
       });
     }
     return {
