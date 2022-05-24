394799ebd553f77656f1e9f398d189b607656876
Alexander Devitsky <alexander.devitsky@gmail.com>
2022-05-24 17:32:13 +0300

    test: added tests for cmscontent component

---
+++ packages/theme/modules/catalog/category/components/cms/__tests__/CmsContent.spec.ts
@@ -0,0 +1,6 @@
+import { cmsContentTest } from '~/test-utils/tests/cmsContent';
+import CmsContent from '~/modules/catalog/category/components/cms/CmsContent.vue';
+
+describe('CmsContent.vue', () => {
+  cmsContentTest(CmsContent);
+});
