69ad107ca82d58747268c7622f62b42e23dd3b8a
Artur Tagisow <atagisow@vuestorefront.io>
2022-05-25 16:32:18 +0200

    refactor: createAddCustomerCommand context type

--- packages/theme/modules/customer/composables/useUserAddress/commands/createCustomerAddressCommand.ts
+++ packages/theme/modules/customer/composables/useUserAddress/commands/createCustomerAddressCommand.ts
@@ -1,8 +1,11 @@
+import type { useContext } from '@nuxtjs/composition-api';
 import { CustomerAddressInput } from '~/modules/GraphQL/types';
 
+type Context = ReturnType<typeof useContext>;
+
 export const createCustomerAddressCommand = {
-  execute: async (context, params: CustomerAddressInput) => {
-    const { data } = await context.$vsf.$magento.api.createCustomerAddress(params);
+  execute: async (context: Context, params: CustomerAddressInput) => {
+    const { data } = await context.app.$vsf.$magento.api.createCustomerAddress(params);
 
     return data?.createCustomerAddress ?? {};
   },
