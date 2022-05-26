ff774562374ed1b0dfc8e70c30917004b3f603e3
Artur Tagisow <atagisow@vuestorefront.io>
2022-05-26 09:47:11 +0200

    refactor: updatecustomeraddress useContext

--- packages/theme/modules/customer/composables/useUserAddress/commands/updateCustomerAddressCommand.ts
+++ packages/theme/modules/customer/composables/useUserAddress/commands/updateCustomerAddressCommand.ts
@@ -1,11 +1,14 @@
+import type { useContext } from '@nuxtjs/composition-api';
 import { CustomerAddressInput } from '~/modules/GraphQL/types';
 
+type Context = ReturnType<typeof useContext>;
+
 export const updateCustomerAddressCommand = {
-  execute: async (context, params: {
+  execute: async (context: Context, params: {
     addressId: number;
     input: CustomerAddressInput;
   }) => {
-    const { data } = await context.$vsf.$magento.api.updateCustomerAddress(params);
+    const { data } = await context.app.$vsf.$magento.api.updateCustomerAddress(params);
 
     return data?.updateCustomerAddress ?? {};
   },
