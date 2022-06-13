d741d00d74c58288238d552594d1f6f3e2fade5a
Artur Tagisow <5359825+sethidden@users.noreply.github.com>
2022-06-13 08:48:12 +0200

    fix: moving back in history after logout shouldn't 404 (#1118)
    M2-595

--- packages/theme/middleware/is-authenticated.ts
+++ packages/theme/middleware/is-authenticated.ts
@@ -2,7 +2,8 @@ import { Middleware } from '@nuxt/types';
 
 const middleware : Middleware = (context) => {
   if (!context.app.$vsf.$magento.config.state.getCustomerToken()) {
-    context.redirect('/');
+    const homepage = context.localeRoute({ name: 'home' });
+    context.redirect(homepage);
   }
 };
 export default middleware;
