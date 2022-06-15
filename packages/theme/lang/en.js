164098b96d50c60d25f36a94659d0cb4fe02ae20
Bartosz Herba <bherba@vuestorefront.io>
2022-06-15 12:52:54 +0200

    fix: m2-620. wishlist amount of displayed products is changed after visiting my account wishlist

--- packages/theme/lang/en.js
+++ packages/theme/lang/en.js
@@ -276,5 +276,6 @@ export default {
   "To": "To",
   "Your customization": "Your customization",
   "Passwords don't match":"Passwords don't match",
-  "The password must be at least 8 characters long and must contain at least: 1 uppercase or lowercase letter, 1 number, or one special character (E.g. , . _ & ? etc)":"The password must be at least 8 characters long and must contain at least: 1 uppercase or lowercase letter, 1 number, or one special character (E.g. , . _ & ? etc)"
+  "The password must be at least 8 characters long and must contain at least: 1 uppercase or lowercase letter, 1 number, or one special character (E.g. , . _ & ? etc)":"The password must be at least 8 characters long and must contain at least: 1 uppercase or lowercase letter, 1 number, or one special character (E.g. , . _ & ? etc)",
+  "Show all products":"Show all products",
 };
