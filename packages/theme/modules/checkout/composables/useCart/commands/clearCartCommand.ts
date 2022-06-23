a72af1e811a7635adaed59f86539da49cb03d9de
Artur Tagisow <atagisow@vuestorefront.io>
2022-06-23 12:29:53 +0200

    fix: cart should be cleared after logout finishes
    there's logic inside clear() that creates a fresh cart if there's no
    cart id
    
    before this commit there seems to have been a race condition between
    logout and clear

--- packages/theme/modules/checkout/composables/useCart/commands/clearCartCommand.ts
+++
@@ -1,7 +0,0 @@
-import { VsfContext } from '~/composables/context';
-
-export const clearCartCommand = {
-  execute: (context: VsfContext) => {
-    context.$magento.config.state.setCartId(null);
-  },
-};
