db56d0c5e9a3f902634fceee5bac6fe923e03cf5
Oleg Bratuniak <oleg.bratuniak@gmail.com>
2022-08-09 19:24:14 +0200

    fix: fix product detail components
    Replace a route query id with a route data sku after implementing #181
    
    fix #1329

--- packages/theme/modules/catalog/product/components/tabs/ProductTabs.vue
+++ packages/theme/modules/catalog/product/components/tabs/ProductTabs.vue
@@ -59,7 +59,7 @@
 </template>
 <script lang="ts">
 import {
-  computed, defineComponent, PropType, ref, useRoute,
+  computed, defineComponent, PropType, ref,
 } from '@nuxtjs/composition-api';
 import {
   SfReview,
@@ -78,6 +78,7 @@ import reviewGetters, {
 import { useReview, UseReviewAddReviewParams } from '~/modules/review/composables/useReview';
 import { Product } from '~/modules/catalog/product/types';
 import { TabsConfig } from '~/modules/catalog/product/composables/useProductTabs';
+import { usePageStore } from '~/stores/page';
 
 export default defineComponent({
   name: 'ProductTabs',
@@ -99,7 +100,7 @@ export default defineComponent({
     },
   },
   setup(props, { emit }) {
-    const route = useRoute();
+    const { routeData } = usePageStore();
     const reviews = ref(null);
 
     const {
@@ -108,11 +109,10 @@ export default defineComponent({
       addReview,
     } = useReview();
 
-    const { params: { id } } = route.value;
     const getSearchQuery = () => ({
       filter: {
         sku: {
-          eq: id,
+          eq: routeData.sku,
         },
       },
     });
