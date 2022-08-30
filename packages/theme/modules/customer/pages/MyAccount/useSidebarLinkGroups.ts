7c01dd78998b89a026d89ecb4e09324f3c72ac00
Bartosz Herba <bherba@vuestorefront.io>
2022-08-30 15:43:40 +0200

    fix: m2-1017 logout doesn't work after reload of the MyAccount page

--- packages/theme/modules/customer/pages/MyAccount/useSidebarLinkGroups.ts
+++ packages/theme/modules/customer/pages/MyAccount/useSidebarLinkGroups.ts
@@ -1,34 +1,39 @@
 import type { RawLocation } from 'vue-router';
+
 import { useRouter, useContext } from '@nuxtjs/composition-api';
 import { useUser } from '~/modules/customer/composables/useUser';
 import { useCart } from '~/modules/checkout/composables/useCart';
 
 type LinkGroup = { title: string, items: LinkGroupItem[] };
-type LinkGroupItem = { label: string, link?: RawLocation, listeners?: Record<string, () => (Promise<void> | void)> };
+type LinkGroupItem = { id: string, label: string, link?: RawLocation };
 
 export const useSidebarLinkGroups = () => {
   const { localeRoute } = useContext();
   const { logout } = useUser();
   const { clear } = useCart();
-
   const router = useRouter();
+
   const sidebarLinkGroups : LinkGroup[] = [
     {
       title: 'Personal details',
       items: [
         {
+          id: 'my-profile',
           label: 'My profile',
           link: { name: 'customer-my-profile' },
         },
         {
+          id: 'address-details',
           label: 'Addresses details',
           link: { name: 'customer-addresses-details' },
         },
         {
+          id: 'my-newsletter',
           label: 'My newsletter',
           link: { name: 'customer-my-newsletter' },
         },
         {
+          id: 'my-wishlist',
           label: 'My wishlist',
           link: { name: 'customer-my-wishlist' },
         },
@@ -38,26 +43,28 @@ export const useSidebarLinkGroups = () => {
       title: 'Order details',
       items: [
         {
+          id: 'order-history',
           label: 'Order history',
           link: { name: 'customer-order-history' },
         },
         {
+          id: 'my-reviews',
           label: 'My reviews',
           link: { name: 'customer-my-reviews' },
         },
         {
+          id: 'log-out',
           label: 'Log out',
-          listeners: {
-            click: async () => {
-              await logout({});
-              await clear({});
-              await router.push(localeRoute({ name: 'home' }));
-            },
-          },
         },
       ],
     },
   ];
 
-  return { sidebarLinkGroups };
+  const logoutUser = async () => {
+    await logout({});
+    await clear({});
+    await router.push(localeRoute({ name: 'home' }));
+  };
+
+  return { sidebarLinkGroups, logoutUser };
 };
