395378b4f19ac978b0bc7c3840cec42081d2bf84
Bartosz Herba <bherba@vuestorefront.io>
2022-09-06 10:00:50 +0200

    fix!: displaying proper price and currency in the customer dashboard - rework deprecated grand_total from the customerOrders query - make prices and currency constant in the customer dashboard

--- packages/theme/plugins/fcPlugin.ts
+++ packages/theme/plugins/fcPlugin.ts
@@ -20,7 +20,7 @@ declare module '@nuxt/types' {
 const plugin : Plugin = (context, inject) => {
   inject('fc', (value: number | string, locale?: string, options = {}): string => {
     // eslint-disable-next-line no-param-reassign
-    locale = locale || context.i18n?.localeProperties?.iso.replace('_', '-');
+    locale = (locale || context.i18n?.localeProperties?.iso || '').replace('_', '-');
     // eslint-disable-next-line no-param-reassign
     options = { currency: context.app.$vsf.$magento.config.state.getCurrency() || context.i18n?.localeProperties?.defaultCurrency, ...options };
     return formatCurrency(value, locale, options);
