c700817b4f684765f8cb0c3c46eb707e83a41e3b
Artur <5359825+sethidden@users.noreply.github.com>
2023-04-24 16:20:59 +0200

    refactor: move WSH12 sku to variable

--- packages/sdk/__tests__/integration/__config__/jest.const.ts
+++ packages/sdk/__tests__/integration/__config__/jest.const.ts
@@ -3,6 +3,7 @@ export const TEST_USER_EMAIL = 'vsf.magento.integration.test.user@gmail.com';
 export const TEST_USER_PASSWORD = '9uvPv!Vvn2!Uaz.yNy4a';
 export const TEST_CART_ID = 'pCS0ykep1l3wGlPKSyWLJq5fb1DxIQcp';
 export const TEST_COUPON_CODE = 'integration-tests';
+export const TEST_PRODUCT_SKU = 'WSH12';
 export const TEST_ADDRESS = {
   firstname: 'John',
   lastname: 'Doe',
@@ -14,7 +15,6 @@ export const TEST_ADDRESS = {
   postcode: '10001',
   save_in_address_book: false
 };
-export const TEST_PRODUCT_SKU = 'WSH12';
 
 export const NOCK_FIXTURES_CATALOG_NAME = '__nock-fixtures__';
 export const NOCK_MODE = 'record';
