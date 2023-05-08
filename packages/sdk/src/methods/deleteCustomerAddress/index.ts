9b44e65d65c3d868521b988e93b8e3825b591acf
Fifciuu <filip.jdrasik@gmail.com>
2023-05-08 14:31:40 +0200

    chore: working on deleteCustomerAddress

---
+++ packages/sdk/src/methods/deleteCustomerAddress/index.ts
@@ -0,0 +1,52 @@
+import { CustomQuery, MethodOptions } from '../../types';
+import { DeleteCustomerAddressMutation, DeleteCustomerAddressMutationVariables } from '@vsf-enterprise/magento-api-types';
+import { client } from '../../client';
+import { DeepPartial } from 'ts-essentials';
+import { ApolloQueryResult } from '@apollo/client';
+
+/**
+ * deleteCustomerAddress response type
+ */
+export type DeleteCustomerAddressResponse<T extends DeepPartial<DeleteCustomerAddressMutation> = DeleteCustomerAddressMutation> = ApolloQueryResult<T>
+
+/**
+ * Method to delete a customer address.
+ *
+ * @remarks
+ * This method communicates with the
+ * {@link @vsf-enterprise/magento-api#ApiMethods.deleteCustomerAddress | deleteCustomerAddress } endpoint
+ * of the Vue Storefront API Middleware.
+ * The default GraphQL query used by this method can be found
+ * {@link @vsf-enterprise/magento-api#deleteCustomerAddress | here}.
+ *
+ * @param params -
+ * Parameter object which can be used with this method.
+ * Refer to its type definition to learn about possible properties.
+ *
+ * @param options -
+ * Options that can be passed to additionally configure the request
+ * or customize the logic in a plugin.
+ *
+ * @typeParam Res - Customizable response interface to be used with custom queries.
+ *
+ * @returns
+ * Returns a representation of the {@link @vsf-enterprise/magento2-sdk#DeleteCustomerAddressResponse | DeleteCustomerAddressResponse}.
+ *
+ * @example
+ * Simple usage:
+ * ```ts
+ * import { sdk } from '~/sdk.config.ts';
+ *
+ * const addressId = 12;
+ * await sdk.magento.deleteCustomerAddress({ id: addressId });
+ * ```
+ */
+export async function deleteCustomerAddress<RES extends DeleteCustomerAddressResponse>(params: DeleteCustomerAddressMutationVariables, options?: MethodOptions<CustomQuery<'deleteCustomerAddress'>>) {
+  const { data } = await client.post<RES>(
+    'deleteCustomerAddress',
+    [params.id, options?.customQuery, options?.customHeaders],
+    options?.clientConfig
+  );
+
+  return data;
+}
