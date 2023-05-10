d8c27a84156f6eda51453b52b0ece14e7790d8ed
Fifciuu <filip.jdrasik@gmail.com>
2023-05-10 13:02:49 +0200

    feat: wishlistItemCount method

--- packages/api-client/src/api/wishlistItemsCount/index.ts
+++ packages/api-client/src/api/wishlistItemsCount/index.ts
@@ -1,32 +1,19 @@
-import { ApolloQueryResult } from '@apollo/client/core';
+import { ApolloQueryResult, gql } from '@apollo/client/core';
 import {
-  CustomQuery,
   WishlistQuery,
 } from '@vsf-enterprise/magento-api-types';
 
 import type { CustomHeaders } from '@vsf-enterprise/magento-api-types';
-import query from './wishlistItemsCount';
+import wishlistItemsCountQuery from './wishlistItemsCount';
 import { Context } from '../../types/context';
 import getHeaders from '../getHeaders';
 
 export default async (
   context: Context,
-  customQuery: CustomQuery = { wishlistItemsCount: 'wishlistItemsCount' },
   customHeaders: CustomHeaders = {},
-): Promise<ApolloQueryResult<WishlistQuery>> => {
-  const { wishlistItemsCount } = context.extendQuery(customQuery, {
-    wishlistItemsCount: {
-      query,
-    },
-  });
-  try {
-    return await context.client.query<WishlistQuery>({
-      query: wishlistItemsCount.query,
-      context: {
-        headers: getHeaders(context, customHeaders),
-      },
-    });
-  } catch (error) {
-    throw error.graphQLErrors?.[0].message || error.networkError?.result || error;
-  }
-};
+): Promise<ApolloQueryResult<WishlistQuery>> => context.client.query<WishlistQuery>({
+  query: gql`${wishlistItemsCountQuery}`,
+  context: {
+    headers: getHeaders(context, customHeaders),
+  },
+});
