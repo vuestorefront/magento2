1240dfcc83113738889722bbab02d995fa0240ca
Artur Tagisow <atagisow@vuestorefront.io>
2023-07-05 12:15:04 +0200

    fix rollup issue

--- packages/api-client/rollup.config.js
+++ packages/api-client/rollup.config.js
@@ -2,6 +2,6 @@ import {
   generateBaseConfig,
   generateServerConfig,
 } from "@vue-storefront/rollup-config";
-import package_ from "../dev/magento2/packages/api-client/package.json";
+import package_ from "./package.json";
 
 export default [generateBaseConfig(package_), generateServerConfig(package_)];
