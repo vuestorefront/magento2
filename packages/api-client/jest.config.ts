25c300ecec3bf6769b06f67b4fc08fe00a5eb0b9
Artur Tagisow <atagisow@vuestorefront.io>
2023-07-06 13:47:53 +0200

    re-add broken files

--- packages/api-client/jest.config.ts
+++ packages/api-client/jest.config.ts
@@ -1,53 +1,5 @@
-module.exports = {
-  globals: {
-    __DEV__: true,
-  },
-  coverageReporters: ['lcov'],
-  coverageThreshold: {
-    global: {},
-  },
-  coveragePathIgnorePatterns: ['/node_modules/', '.d.ts$', '/__mocks__/'],
-  coverageDirectory: './coverage/',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-    'api-client(.*)$': '<rootDir>$1',
-  },
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(m)js$': 'babel-jest',
-    '^.+\\.ts$': 'ts-jest',
-  },
-  transformIgnorePatterns: ['node_modules', '<rootDir>/node_modules'],
-  testMatch: ['<rootDir>/**/__tests__/**/*spec.[jt]s?(x)'],
-  watchPlugins: [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
-    [
-      'jest-watch-toggle-config',
-      {
-        setting: 'verbose',
-      },
-    ],
-    [
-      'jest-watch-toggle-config',
-      {
-        setting: 'collectCoverage',
-      },
-    ],
-    [
-      'jest-watch-toggle-config',
-      {
-        setting: 'notify',
-      },
-    ],
-    [
-      'jest-watch-toggle-config',
-      {
-        setting: 'bail',
-      },
-    ],
-  ],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  moduleFileExtensions: ['js', 'json', 'ts'],
+import { baseConfig } from '@vue-storefront/jest-config';
+
+export default {
+  ...baseConfig,
 };
