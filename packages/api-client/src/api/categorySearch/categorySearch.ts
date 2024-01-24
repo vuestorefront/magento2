fce1d87062f7c450e8a681dbe5c2ef5dbf76ccf4
Andrzej Kurek <32792205+Razz21@users.noreply.github.com>
2024-01-24 09:51:33 +0100

    Revert "feat(IN-3927): UDM enhancements (#1483)" (#1485)
    This reverts commit e3a461c93fa55500e7172675888429c0c1fc9597.

--- packages/api-client/src/api/categorySearch/categorySearch.ts
+++ packages/api-client/src/api/categorySearch/categorySearch.ts
@@ -9,21 +9,8 @@ export default `
       position
       product_count
       uid
-      url_key
       url_path
       url_suffix
-      children {
-        include_in_menu
-        is_anchor
-        level
-        name
-        position
-        product_count
-        uid
-        url_key
-        url_path
-        url_suffix
-      }
     }
   }
 `;
