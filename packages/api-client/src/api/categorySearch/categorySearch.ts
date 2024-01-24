e3a461c93fa55500e7172675888429c0c1fc9597
Andrzej Kurek <32792205+Razz21@users.noreply.github.com>
2024-01-24 07:30:43 +0100

    feat(IN-3927): UDM enhancements (#1483)
    * feat: categorylistquery
    
    * feat: categorysearchquery
    
    * feat: productlistquery
    
    * chore: regenerate fixtures
    
    * chore: changeset
    
    * chore: udpate changelog
    
    * chore: update changelog

--- packages/api-client/src/api/categorySearch/categorySearch.ts
+++ packages/api-client/src/api/categorySearch/categorySearch.ts
@@ -9,8 +9,21 @@ export default `
       position
       product_count
       uid
+      url_key
       url_path
       url_suffix
+      children {
+        include_in_menu
+        is_anchor
+        level
+        name
+        position
+        product_count
+        uid
+        url_key
+        url_path
+        url_suffix
+      }
     }
   }
 `;
