26b1c4920fcce8f71c4c1974907261d9a71c0317
Wojciech Sikora <wsikora@vuestorefront.io>
2024-04-10 12:43:32 +0200

    fix build issues

--- packages/api-client/rollup.config.js
+++ packages/api-client/rollup.config.js
@@ -1,12 +1,7 @@
-import json from "@rollup/plugin-json";
-
 import { generateBaseConfig, generateServerConfig } from "@vue-storefront/rollup-config";
 import package_ from "./package.json";
 
 const baseConfig = generateBaseConfig(package_);
 const serverConfig = generateServerConfig(package_);
-
-baseConfig.plugins.push(json());
-serverConfig.plugins.push(json());
 
 export default [baseConfig, serverConfig];
