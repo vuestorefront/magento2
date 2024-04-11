f721636268b7cda6556eec97e3379c62f6e5352a
Bartosz Herba <bherba@vuestorefront.io>
2024-04-11 10:15:24 +0200

    fix: type inference for the module build

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -2,6 +2,10 @@ import type { Module } from '@vue-storefront/sdk';
 import { connector } from './connector';
 import { ModuleOptions } from './types';
 
+/**
+ * @deprecated This type is deprecated and will be removed in the next major version.
+ * It is no longer necessary to use this type. Please, check documentation of `magentoModule`.
+ */
 export interface MagentoModuleType extends Module {
   connector: ReturnType<typeof connector>;
 }
@@ -9,9 +13,7 @@ export interface MagentoModuleType extends Module {
 /**
  * Magento module.
  *
- * @example
- *
- * Initialization of the Magento module.
+ * @example Initialization of the Magento module.
  *
  * ```js
  * import { initSDK, buildModule } from '@vue-storefront/sdk';
@@ -19,7 +21,7 @@ export interface MagentoModuleType extends Module {
  *
  * const sdkConfig = {
  *   magento:
- *     buildModule<MagentoModuleType>(
+ *     buildModule(
  *       magentoModule,
  *       {
  *         apiUrl: 'http://localhost:8181/magento',
@@ -30,7 +32,7 @@ export interface MagentoModuleType extends Module {
  * export const sdk = initSDK<typeof sdkConfig>(sdkConfig);
  * ```
  */
-export const magentoModule = (options: ModuleOptions): MagentoModuleType => {
+export const magentoModule = (options: ModuleOptions) => {
   return {
     connector: connector(options),
   };
